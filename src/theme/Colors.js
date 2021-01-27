9afae519d07720076c62e4d32ec28c40ea958a95
dianavile <diana.vile@gmail.com>
2021-01-27 15:22:05 +0100

    fix error

--- src/theme/Colors.js
+++ src/theme/Colors.js
@@ -1,9 +1,10 @@
 const Colors = {
-  white: '#FFFFFF',
-  black: '#000000',
-  mainColor: 'orange',
-  redColor: 'rgba(193, 12, 12, 1)',
-  lightBlue: '#46C1F7',
-  darkBlue: '#0073E6'
+	white: "#FFFFFF",
+	black: "#000000",
+	mainColor: "orange",
+	redColor: "rgba(193, 12, 12, 1)",
+	lightBlue: "#46C1F7",
+	darkBlue: "#0073E6",
+};
 
-export default Colors
+export default Colors;
