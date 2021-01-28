87ff2572850c69c8862f43c56914e5c90e6c95c0
dianavile <diana.vile@gmail.com>
2021-01-28 14:41:30 +0100

    divide into small tasks

--- src/components/units/Input/styles.js
+++ src/components/units/Input/styles.js
@@ -1,24 +1,32 @@
-import styled from 'styled-components'
-import Colors from 'theme/Colors'
+import styled from "styled-components";
+import Colors from "theme/Colors";
 
 const StyledInput = styled.input`
-    top: 1rem;
-    width: 18.6rem;
-    height: 2.6rem;
-    border-radius: 6px;
-    margin: 0.4rem 0 0.4rem 0;
-    opacity: 1;
-    outline: none;
-    border: 1px solid #dddddd;
-    text-align: left;
-    font: normal normal normal 16px/32px Helvetica Neue;
-    letter-spacing: 0px;
-    color: #393939;
-    opacity: 1;
-    &.error {
-        border: 1px solid ${Colors.redColor};
-        color: #7d868b;
-    }
-`
+	top: 1rem;
+	width: 18.6rem;
+	height: 2.6rem;
+	border-radius: 6px;
+	margin: 0.4rem 0 0.4rem 0;
+	opacity: 1;
+	outline: none;
+	border: 1px solid #dddddd;
+	text-align: left;
+	font: normal normal normal 16px/32px Helvetica Neue;
+	letter-spacing: 0px;
+	color: #393939;
+	opacity: 1;
+	&.error {
+		border: 1px solid ${Colors.redColor};
+		color: #7d868b;
+	}
+`;
 
-export default StyledInput
+// add input state messages: error, disabled, success
+// check input con el diseño
+// top: 301px;
+// left: 492px;
+// width: 100%;
+// height: 40px;
+// outline: none;
+
+export default StyledInput;
