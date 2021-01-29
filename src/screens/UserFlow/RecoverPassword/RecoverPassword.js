e547583b2412593ba52fd3e83c24615b402c18e6
dianavile <diana.vile@gmail.com>
2021-01-29 13:34:10 +0100

    divide tasks

--- src/screens/UserFlow/RecoverPassword/RecoverPassword.js
+++ src/screens/UserFlow/RecoverPassword/RecoverPassword.js
@@ -1,13 +1,41 @@
+//import
 import React, {useState} from "react";
 import {Link} from "react-router-dom";
 import Body from "components/layout/Body/Body";
 
+// 0 SETUP
+//- create component
+
+//1 INPUT
+// - importar archivos
+// - declarar variables
+
+//2 LOGICA CON EL INPUT- DATA PROCESSION
+// - set useState
+//- add methods();
+// -add conditional rendering: if---else/swithc statement
+//- add try-catch for error logic
+
+//3 OUTPUT
+//return form
+
+//create COMPONENT
 const RecoverPassword = (props) => {
+	// - declarar variables
+
+	//2 LOGICA CON EL INPUT- DATA PROCESSION
+	// - set useState
 	const [email, setEmail] = useState();
+
+	//- add methods();
+	// -add conditional rendering: if---else/swithc statement
 
 	const changeEmail = () => {
 		setEmail("");
 	};
+
+	//- add try-catch for error logic
+
 	return (
 		//add function to body to change password
 		<Body title="Cambiar contraseña">
@@ -17,7 +45,7 @@ const RecoverPassword = (props) => {
 					<strong>¿Has olvidado tu contraseña?</strong> Para recuperarla introduce tu
 					email y te enviaremos una nueva por correo.
 				</Label>
-				<input>Email </input>
+				<input>Email</input>
 				<button>Enviar</button>
 			</StyledForm>
 			<Footer></Footer>
