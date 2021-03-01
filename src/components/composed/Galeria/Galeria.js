cdea3233db549555f9a699bc736cf1d9c95ec9f5
Kevin Mamaqi <kevinmamaqi@gmail.com>
2021-03-01 10:24:09 +0100

    first commit

---
+++ src/components/composed/Galeria/Galeria.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Galeria = () => {
+	return <div>Hola</div>;
+};
+
+export default Galeria;
---
+++ src/screens/Anuncio/Anuncio.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import Galeria from "components/composed/Galeria/Galeria";
+
+const Anuncio = () => {
+	return <div></div>;
+};
+
+export default Anuncio;
