854bdcf34a78f59f4c20cac900edaa1e6d5e2722
Maria Tibau <maria.ta.89@gmail.com>
2021-03-01 20:06:46 +0100

    override images rendering logic on gallery

--- src/components/composed/Gallery/Gallery.js
+++ src/components/composed/Gallery/Gallery.js
@@ -1,24 +1,42 @@
 import React from "react";
 import ImageGallery from 'react-image-gallery';
+import { adImage1, adImage2, adImage3 } from "assets/images"
+import {StyledImage} from "./Gallery.styles.js"
+import "react-image-gallery/styles/css/image-gallery.css";
 
+const renderImage = (isThumbnail, src) => {
+  return (
+    <StyledImage src={src} thumbnail={isThumbnail}/>
+  )
+}
 const images = [
   {
-    original: 'https://picsum.photos/id/1018/1000/600/',
-    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+    original: adImage1,
+    renderThumbInner: () => renderImage(true, adImage1),
+    renderItem: () => renderImage(false, adImage1)
   },
   {
-    original: 'https://picsum.photos/id/1015/1000/600/',
-    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+    original: adImage2,
+    renderThumbInner: () => renderImage(true, adImage2),
+    renderItem: () => renderImage(false, adImage2)
   },
   {
-    original: 'https://picsum.photos/id/1019/1000/600/',
-    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    original: adImage3,
+    renderThumbInner: () => renderImage(true, adImage3),
+    renderItem: () => renderImage(false, adImage3)
   },
 ];
 
 const Gallery = () => {
-	 
-		return <ImageGallery items={images} />;
+		return (
+      <ImageGallery 
+        items={images} 
+        showFullscreenButton={false} 
+        showBullets={false}
+        showPlayButton={false}
+        showNav={false}
+      />
+    );
 	  
 };
 
