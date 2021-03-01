185d26f5cb7d10dc7e4f714520e27f0fda740219
Kevin Mamaqi <kevinmamaqi@gmail.com>
2021-03-01 12:29:10 +0100

    old huskys

--- src/components/layout/Body/Body.js
+++ src/components/layout/Body/Body.js
@@ -3,12 +3,10 @@ import Header from "components/layout/Header/Header";
 import Footer from "components/layout/Footer/Footer";
 import {Childrens, StyledBody} from "./styles";
 
-const wessro = sssa;
-
 const Body = ({children, title, isLoggedIn}) => {
 	return (
 		<StyledBody>
-			<Header title={titlsades} isLoggedIn={isLoggedIn} />
+			<Header title={title} isLoggedIn={isLoggedIn} />
 			<Childrens>{children}</Childrens>
 			<Footer />
 		</StyledBody>
