1692f69550c9c6f0a20da733a58e494924b72725
Kevin Mamaqi <kevinmamaqi@gmail.com>
2021-03-01 09:13:14 +0100

    second commit

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -18,10 +18,14 @@ const profilePicture =
 
 const Header = ({isLoggedIn, title}) => {
 	const [dropdownVisible, setDropdownVisible] = useState(false);
+    const [testing, setTesting] = useState(false)
+    useEffect(() => {
+        const tryThis = true;
+    }, [])
 	const handleClick = () => {
 		console.log("profile clicked");
 		setDropdownVisible(!dropdownVisible);
-	};
+	
 	if (isLoggedIn) {
 		return (
 			<header>
