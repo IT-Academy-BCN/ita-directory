8dabe3b97adfdcf7be247c7ddc2968bb75c009e4
silviapio <silvia.piovesan@gmail.com>
2021-03-01 14:57:55 +0100

    create InputNumber file

---
+++ src/components/units/Input/InputNumber.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const InputNumber = () => {
+    return(
+        <div>
+            input type number
+        </div>
+    );
+}
+
+//include propTypes
+
+export default InputNumber;
