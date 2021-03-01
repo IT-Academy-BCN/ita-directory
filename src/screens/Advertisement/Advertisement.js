ba02e506376bcfae03730c7c8d57c9cf5b944f95
Cristina M <mcristinamotoc@gmail.com>
2021-03-01 16:59:45 +0100

    add advertisement

---
+++ src/screens/Advertisement/Advertisement.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Gallery from "components/composed/Gallery/Gallery"
+
+const Advertisement = () => {
+	return <div><Gallery/></div>;
+};
+
+export default Advertisement;
--- src/screens/Anuncio/Anuncio.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-// import Galeria from "components/composed/Galeria/Galeria";
-
-const Anuncio = () => {
-	return <div></div>;
-};
-
-export default Anuncio;
