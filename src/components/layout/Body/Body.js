d8a65b01bf52699f2d12cd8f303f5f0154ba8f1c
Kevin Mamaqi <kevinmamaqi@gmail.com>
2021-03-01 12:16:42 +0100

    old huskys

--- src/components/layout/Body/Body.js
+++ src/components/layout/Body/Body.js
@@ -3,7 +3,7 @@ import Header from "components/layout/Header/Header";
 import Footer from "components/layout/Footer/Footer";
 import {Childrens, StyledBody} from "./styles";
 
-const wesro = sssa;
+const wessro = sssa;
 
 const Body = ({children, title, isLoggedIn}) => {
 	return (
