d917e63e4785db89a27d9716a548baa1834b716e
Cristina M <mcristinamotoc@gmail.com>
2021-03-01 16:58:01 +0100

    add gallery

--- src/components/composed/Galeria/Galeria.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Galeria = () => {
-	return <div>Hola</div>;
-};
-
-export default Galeria;
---
+++ src/components/composed/Gallery/Gallery.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ImageGallery from 'react-image-gallery';
+
+const images = [
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+  },
+];
+
+const Gallery = () => {
+	 
+		return <ImageGallery items={images} />;
+	  
+};
+
+export default Gallery;
