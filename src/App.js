3780101878d749fd7f447e3f75bf72a21ecd02bd
Kevin Mamaqi <kevinmamaqi@gmail.com>
2021-03-01 08:41:16 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React from "react";
 import {Route, Switch} from "react-router-dom";
 import ProtectedRoute from "components/composed/ProtectedRoute";
 
-// import {faEye} from "@fortawesome/free-solid-svg-icons";
-// 1. FOOTER DEJAR HUECO ENTRE LINEA Y FOOTER
-// 2. FOOTER ALIENAR ELEMENTOS AL COMIENZO Y AL FINAL
-
 import Home from "screens/Home/Home";
 
 import Login from "screens/UserFlow/Login/Login";
@@ -22,7 +18,6 @@ const App = () => {
 				<Route exact path="/register" component={Registration} />
 				<Route exact path="/profile" component={Profile} />
 				<Route exact path="/recover-password/:hash" component={RecoverPassword} />
-				{/* <ProtectedRoute component={Page404} /> */}
 			</Switch>
 		</>
 	);
