aae5116ca2c8987e5b3086b272cf3c9f4f1514ae
Kevin Mamaqi <kevinmamaqi@gmail.com>
2021-03-01 11:41:46 +0100

    new commit

--- src/components/layout/Body/Body.js
+++ src/components/layout/Body/Body.js
@@ -3,10 +3,12 @@ import Header from "components/layout/Header/Header";
 import Footer from "components/layout/Footer/Footer";
 import {Childrens, StyledBody} from "./styles";
 
+dasdas;
+
 const Body = ({children, title, isLoggedIn}) => {
 	return (
 		<StyledBody>
-			<Header title={titles} isLoggedIn={isLoggedIn} />
+			<Header title={titlsades} isLoggedIn={isLoggedIn} />
 			<Childrens>{children}</Childrens>
 			<Footer />
 		</StyledBody>
