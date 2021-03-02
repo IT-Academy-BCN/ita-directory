5b62cef2ccc739e2bf3d20c800311162b894e52a
Maria Tibau <maria.ta.89@gmail.com>
2021-03-02 11:12:07 +0100

    extract images from gallery

--- src/components/composed/Gallery/Gallery.js
+++ src/components/composed/Gallery/Gallery.js
@@ -1,24 +1,9 @@
 import React from "react";
 import ImageGallery from 'react-image-gallery';
-import { adImage1, adImage2, adImage3, adThumbnail1, adThumbnail2, adThumbnail3 } from "assets/images";
 import "react-image-gallery/styles/css/image-gallery.css";
 
-const images = [
-  {
-    original: adImage1,
-    thumbnail: adThumbnail1
-  },
-  {
-    original: adImage2,
-    thumbnail: adThumbnail2
-  },
-  {
-    original: adImage3,
-    thumbnail: adThumbnail3
-  },
-];
 
-const Gallery = () => {
+const Gallery = ({images}) => {
 		return (
       <ImageGallery 
         items={images} 
--- src/screens/Advertisement/Advertisement.js
+++ src/screens/Advertisement/Advertisement.js
@@ -1,8 +1,27 @@
 import React from "react";
 import Gallery from "components/composed/Gallery/Gallery"
+import Body from "components/layout/Body/Body"
+import { adImage1, adImage2, adImage3, adThumbnail1, adThumbnail2, adThumbnail3 } from "assets/images";
 
 const Advertisement = () => {
-	return <div><Gallery/></div>;
+
+	const images = [
+		{
+		  original: adImage1,
+		  thumbnail: adThumbnail1
+		},
+		{
+		  original: adImage2,
+		  thumbnail: adThumbnail2
+		},
+		{
+		  original: adImage3,
+		  thumbnail: adThumbnail3
+		},
+	  ];
+
+	  
+	return <Body title="Anuncio"><Gallery images={images}/></Body>;
 };
 
 export default Advertisement;
