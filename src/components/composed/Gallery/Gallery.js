57f4bdc0b17cb516e328e6645ad46dc7786d1d72
Maria Tibau <maria.ta.89@gmail.com>
2021-03-02 11:52:06 +0100

    wrap image gallery into styled container

--- src/components/composed/Gallery/Gallery.js
+++ src/components/composed/Gallery/Gallery.js
@@ -1,17 +1,20 @@
 import React from "react";
 import ImageGallery from 'react-image-gallery';
 import "react-image-gallery/styles/css/image-gallery.css";
+import {Container} from "./Gallery.styles.js"
 
-
-const Gallery = ({images}) => {
+const Gallery = ({images, width=800, height=256}) => {
 		return (
-      <ImageGallery 
+      <Container width={width} height={height}>
+        <ImageGallery 
         items={images} 
         showFullscreenButton={false} 
         showBullets={false}
         showPlayButton={false}
         showNav={false}
       />
+      </Container>
+      
     );
 	  
 };
