d3755652dfacdf9c550da8476ab0fb4d364737e5
Maria Tibau <maria.ta.89@gmail.com>
2021-03-02 12:56:58 +0100

    set default options for image gallery

--- src/components/composed/Gallery/Gallery.js
+++ src/components/composed/Gallery/Gallery.js
@@ -3,18 +3,41 @@ import ImageGallery from 'react-image-gallery';
 import "react-image-gallery/styles/css/image-gallery.css";
 import {Container} from "./Gallery.styles.js"
 
-const Gallery = ({images, width=800, height=256}) => {
+const Gallery = ({
+  images, 
+  width=800, 
+  height=256, 
+  showFullscreenButton=false, 
+  showBullets=false,
+  showPlayButton=false,
+  showNav=false,
+  showIndex=false,
+  autoPlay=false,
+  showThumbnails=true,
+  infinite=false,
+  lazyLoad=false,
+  slideDuration=450,
+  slideInterval=3000,
+  startIndex=0
+}) => {
 		return (
       <Container width={width} height={height}>
         <ImageGallery 
-        items={images} 
-        showFullscreenButton={false} 
-        showBullets={false}
-        showPlayButton={false}
-        showNav={false}
-      />
+          items={images} 
+          showFullscreenButton={showFullscreenButton} 
+          showBullets={showBullets}
+          showPlayButton={showPlayButton}
+          showNav={showNav}
+          showIndex={showIndex}
+          autoPlay={autoPlay}
+          showThumbnails={showThumbnails}
+          infinite={infinite}
+          lazyLoad={lazyLoad}
+          slideDuration={slideDuration}
+          slideInterval={slideInterval}
+          startIndex={startIndex}
+        />
       </Container>
-      
     );
 	  
 };
