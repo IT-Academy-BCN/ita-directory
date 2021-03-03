d99ac542d16527e46f697a607153059a5b42a378
Cristina M <mcristinamotoc@gmail.com>
2021-03-03 13:44:38 +0100

    fix styles footer

--- src/components/layout/Footer/styles.js
+++ src/components/layout/Footer/styles.js
@@ -22,110 +22,87 @@ export const StyledFooter = styled.footer`
 	display: flex;
 	flex-direction: row;
 	justify-content: space-between;
-	margin-top: 1.5rem;
-	border-top: 1px solid ${Colors.lightGrey};
+	border-top: 1px solid ${Colors.grey};
 	width: 100%;
+	margin: 1rem 3rem;
+	padding-top: 1rem;
 
 	@media only screen and (max-width: 468px) {
-		display: flex;
 		flex-wrap: wrap;
 		justify-content: center;
-		margin: 1rem;
+		margin: 1rem 0.5rem;
+	}
+
+	@media only screen and (max-width: 600px) {
+		margin: 2rem;
 	}
 `;
 
 export const Logo = styled.div`
-	@media only screen and (min-width: 1020px) {
-		text-align: left;
-		font: normal normal normal 20px/20px Korb-Bold;
-		letter-spacing: 0px;
-		color: ${Colors.black};
-		opacity: 1;
-		font-weight: bold;
-	}
+	text-align: left;
+	font: normal normal normal 15px/15px Korb-Bold;
+	letter-spacing: 0px;
+	color: ${Colors.black};
+	opacity: 1;
+	font-weight: bold;
+	text-transform: uppercase;
+	padding-top: 3px;
+	padding-bottom: 1rem;
+
 	@media only screen and (max-width: 1019px) {
-		text-align: left;
-		font: normal normal normal 20px/20px Korb-Bold;
-		letter-spacing: 0px;
-		color: ${Colors.black};
-		opacity: 1;
-		font-weight: bold;
 		min-width: 10rem;
 		flex: 2;
 	}
+
 	@media only screen and (max-width: 468px) {
 		text-align: center;
-		font: normal normal normal 20px/20px Korb-Bold;
-		letter-spacing: 0px;
-		color: ${Colors.black};
-		opacity: 1;
-		font-weight: bold;
-		padding: 0 25% 0 25%;
 		flex: 4;
 	}
 `;
 
 export const Information = styled.div`
 	text-align: right;
+	letter-spacing: 0;
+	opacity: 1;
+	list-style-type: none;
+	font: normal normal normal 0.9rem Helvetica Neue;
+	margin: 0px;
+	padding-left: 0;
+	color: ${Colors.grey};
+
 	@media only screen and (min-width: 1020px) {
-		text-align: right;
-		font: normal normal normal 16px Helvetica Neue;
-		letterspacing: 0;
-		color: #4a4a4a;
-		opacity: 1;
-		list-style-type: none;
-		margin: 0px;
-		padding-left: 0;
 		min-width: 10rem;
 		flex: 3;
 	}
 	@media only screen and (min-width: 1019px) {
-		text-align: right;
-		font: normal normal normal 16px Helvetica Neue;
-		letterspacing: 0;
-		color: #4a4a4a;
-		opacity: 1;
-		list-style-type: none;
 		margin: 0px;
-		padding-left: 0;
 		min-width: 10rem;
 		flex: 3;
 	}
 	@media only screen and (max-width: 468px) {
 		text-align: center;
-		font: normal normal normal 16px Helvetica Neue;
-		letterspacing: 0;
-		color: #4a4a4a;
-		opacity: 1;
-		list-style-type: none;
 		margin-top: 0.5rem;
-		padding-left: 0;
 		flex: 4;
 	}
 `;
 export const Copyright = styled.div`
-	@media only screen and (min-width: 1020px && max-width:468px) {
-		margin: 3px;
-	}
+	margin: 3px;
 `;
 export const Rights = styled.div`
-	@media only screen and (min-width: 1020px && max-width:468px) {
-		margin: 3px;
-	}
+	margin: 3px;
 `;
 
 export const Legal = styled.div`
-	@media only screen and (min-width: 1020px && max-width:468px) {
-		margin: 3px;
-	}
+	margin: 3px;
+	white-space: nowrap;
 `;
 
 export const Anchor = styled.a`
-	color: #4a4a4a;
+	color: ${Colors.grey};
 	text-decoration: none;
 	list-style-type: none;
 	padding: 2px;
 	&:hover {
-		color: #0578e7;
+		color: ${Colors.lightGrey};
 	}
 `;
