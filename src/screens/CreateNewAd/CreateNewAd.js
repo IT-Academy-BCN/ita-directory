a362e57a8f3effb22b291b846192854aa543b5dd
silviapio <silvia.piovesan@gmail.com>
2021-03-04 10:45:11 +0100

    initial commit

---
+++ src/screens/CreateNewAd/CreateNewAd.js
@@ -0,0 +1,21 @@
+import Header from "components/layout/Header/Header";
+import Body from "components/layout/Body/Body";
+import Footer from "components/layout/Footer/Footer";
+
+const CreateNewAd = () => {
+
+    return(
+        <>
+            <Header />
+            <Body>
+                <p>form goes here</p>
+            </Body>
+
+            <Footer />
+
+        </>
+
+    );
+}
+
+export default CreateNewAd;
