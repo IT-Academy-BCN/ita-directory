f750a19f799c9b83c71b783b0e6808c17d8bdd75
Maria Tibau <maria.ta.89@gmail.com>
2021-03-05 12:13:39 +0100

    add onClick to Button

--- src/components/units/Button/Button.js
+++ src/components/units/Button/Button.js
@@ -17,6 +17,7 @@ const Button = ({
 	textStyles,
 	iconStyles,
 	animated,
+	onClick,
 }) => {
 	return (
 		<StyledButton
@@ -24,6 +25,7 @@ const Button = ({
 			disabled={disabled}
 			className={`${className} ${animated ? "animated" : ""} ${disabled ? "disabled" : ""}`}
 			style={{...buttonStyles}}
+			onClick={onClick}
 		>
 			{iconPosition === "left" &&
 				(isLoading ? (
@@ -49,11 +51,12 @@ Button.propTypes = {
 	isLoading: PropTypes.bool,
 	iconPosition: PropTypes.string,
 	className: PropTypes.string,
-	buttonStyles: PropTypes.string,
+	buttonStyles: PropTypes.object,
 	textStyles: PropTypes.object,
 	iconStyles: PropTypes.string,
 	animated: PropTypes.bool,
 	disabled: PropTypes.bool,
+	onClick: PropTypes.func,
 };
 
 export default Button;
