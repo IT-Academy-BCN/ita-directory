23616f27fd85a24632d0db42c24e5c43ea84b569
silviapio <silvia.piovesan@gmail.com>
2021-03-05 12:26:35 +0100

    refactor CreateNewAd render

--- src/screens/CreateNewAd/CreateNewAd.js
+++ src/screens/CreateNewAd/CreateNewAd.js
@@ -12,55 +12,65 @@ const CreateNewAd = () => {
 
 	const handleChangeNumeric = (e) => setNumericValue(e.target.value);
 
+	const inputComponentData = [
+		{
+			label: "Título",
+			inputClassName: "styleInputCreateNewAd",
+		},
+		{
+			label: "Descripción",
+			inputClassName: "styleInput styleInputCreateNewAd",
+		},
+		{
+			label: "Ciudad",
+			inputClassName: "styleInputCreateNewAd2",
+			icon: faMapMarkerAlt,
+		},
+	];
+	const inputNumberComponentData = [
+		{
+			label: "Habitaciones",
+			icon: faBed,
+		},
+		{
+			label: "Precio",
+			icon: faEuroSign,
+		},
+		{
+			label: "M\u00B2",
+			icon: faHome,
+		},
+		{
+			label: "Baños",
+			icon: faBath,
+		},
+	];
+
 	return (
 		<>
 			<Body title="Publicar anuncio" isLoggedIn={true}>
 				<Wrapper>
 					<form>
-						<Input
-							label={"Título"}
-							inputContainerClassName="createNewAd"
-							className="styleInputCreateNewAd"
-						/>
-						<Input
-							label={"Descripción"}
-							className="styleInput styleInputCreateNewAd"
-							inputContainerClassName="createNewAd"
-						/>
-						<Input
-							label={"Ciudad"}
-							icon={faMapMarkerAlt}
-							inputContainerClassName="createNewAd"
-							className="styleInputCreateNewAd2"
-						/>
-						<InputNumber
-							label={"Habitaciones"}
-							icon={faBed}
-							onChange={handleChangeNumeric}
-							value={numericValue}
-							className="styleInputCreateNewAd"
-						/>
-						<InputNumber
-							label={"Precio"}
-							icon={faEuroSign}
-							onChange={handleChangeNumeric}
-							value={numericValue}
-							className="styleInputCreateNewAd"
-						/>
-						<InputNumber
-							label={"M2"}
-							icon={faHome}
-							onChange={handleChangeNumeric}
-							value={numericValue}
-							className="styleInputCreateNewAd"
-						/>
-						<InputNumber
-							label={"Baños"}
-							icon={faBath}
-							onChange={handleChangeNumeric}
-							value={numericValue}
-							className="styleInputCreateNewAd"
-						/>
+						{inputComponentData.map((el, i) => (
+							<Input
+								key={i}
+								label={el.label}
+								className={el.inputClassName}
+								icon={el.icon}
+								inputContainerClassName="createNewAd"
+							/>
+						))}
+						{inputNumberComponentData.map((el, i) => (
+							<InputNumber
+								key={i}
+								label={el.label}
+								icon={el.icon}
+								className="styleInputCreateNewAd"
+								onChange={handleChangeNumeric}
+								value={numericValue}
+							/>
+						))}
+
 						<Button
 							buttonStyles={{width: "7.25rem", height: "2.125rem"}}
 							text="Enviar"
