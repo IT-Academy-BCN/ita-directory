67ee6bcc9e1dd5d22a6b6c96113c0bfa45ce8fec
Maria Tibau <maria.ta.89@gmail.com>
2021-03-05 09:58:19 +0100

    add PropTypes

--- src/components/composed/Modal/Modal.js
+++ src/components/composed/Modal/Modal.js
@@ -9,6 +9,7 @@ import {
 	ModalOverlay,
 	ModalTitle,
 } from "./Modal.styles";
+import PropTypes from "prop-types";
 
 const Modal = ({title, footer, children, active, hideModal}) => {
 	return (
@@ -29,4 +30,21 @@ const Modal = ({title, footer, children, active, hideModal}) => {
 		</Fragment>
 	);
 };
+
+Modal.propTypes = {
+	title: PropTypes.string,
+	footer: PropTypes.oneOfType([
+		PropTypes.arrayOf(PropTypes.node),
+		PropTypes.node,
+		PropTypes.string,
+	]),
+	children: PropTypes.oneOfType([
+		PropTypes.arrayOf(PropTypes.node),
+		PropTypes.node,
+		PropTypes.string,
+	]).isRequired,
+	active: PropTypes.bool.isRequired,
+	hideModal: PropTypes.func.isRequired,
+};
+
 export default Modal;
