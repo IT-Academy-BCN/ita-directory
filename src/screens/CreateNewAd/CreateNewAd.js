6536a11ab8f2798d6c0a7c5b876dbbd82d9e2cef
silviapio <silvia.piovesan@gmail.com>
2021-03-06 10:17:22 +0100

    refactor CreateNewAd.js, added Button type

--- src/screens/CreateNewAd/CreateNewAd.js
+++ src/screens/CreateNewAd/CreateNewAd.js
@@ -7,7 +7,7 @@ import {Wrapper} from "./CreateNewAd.styles";
 import {faMapMarkerAlt, faBed, faEuroSign, faHome, faBath} from "@fortawesome/free-solid-svg-icons";
 
 const CreateNewAd = () => {
-	const [formInput, setFormInput] = useState({
+	const [form, setForm] = useState({
 		title: "",
 		description: "",
 		city: "",
@@ -19,50 +19,61 @@ const CreateNewAd = () => {
 
 	const handleChange = (e) => {
 		const {name, value} = e.target;
-		setFormInput({
-			...formInput,
+		setForm({
+			...form,
 			[name]: value,
 		});
 	};
 
 	const inputComponentData = [
 		{
+			Component: Input,
+			type: "text",
 			label: "Título",
 			name: "title",
 			inputClassName: "styleInputCreateNewAd",
+			inputContainerClassName: "createNewAd",
 		},
 		{
+			Component: Input,
+			type: "text",
 			label: "Descripción",
 			name: "description",
 			inputClassName: "styleInput styleInputCreateNewAd",
+			inputContainerClassName: "createNewAd",
 		},
 		{
+			Component: Input,
+			type: "text",
 			label: "Ciudad",
 			name: "city",
 			inputClassName: "styleInputCreateNewAd2",
+			inputContainerClassName: "createNewAd",
 			icon: faMapMarkerAlt,
 		},
-	];
-	const inputNumberComponentData = [
 		{
+			Component: InputNumber,
 			label: "Habitaciones",
 			name: "rooms",
 			inputClassName: "styleInputCreateNewAd",
 			icon: faBed,
 		},
 		{
+			Component: InputNumber,
 			label: "Precio",
 			name: "price",
 			inputClassName: "styleInputCreateNewAd",
 			icon: faEuroSign,
 		},
 		{
+			Component: InputNumber,
 			label: "M\u00B2",
 			name: "squareM",
 			inputClassName: "styleInputCreateNewAd",
 			icon: faHome,
 		},
 		{
+			Component: InputNumber,
 			label: "Baños",
 			name: "bathrooms",
 			inputClassName: "styleInputCreateNewAd",
@@ -70,36 +81,32 @@ const CreateNewAd = () => {
 		},
 	];
 
-	const mapInput = (data, Component, type, inputContainerClassName) => {
-		let inputs = data.map((el, i) => {
-			const inputName = el.name;
-			return (
-				<Component
-					key={i}
-					type={type}
-					label={el.label}
-					name={inputName}
-					value={formInput.inputName}
-					onChange={handleChange}
-					className={el.inputClassName}
-					icon={el.icon}
-					inputContainerClassName={inputContainerClassName}
-				/>
-			);
-		});
-		return inputs;
-	};
-
 	return (
 		<>
 			<Body title="Publicar anuncio" isLoggedIn={true}>
 				<Wrapper>
 					<form>
-						{mapInput(inputComponentData, Input, "text", "createNewAd")}
-						{mapInput(inputNumberComponentData, InputNumber)}
+						{inputComponentData.map((el, i) => {
+							const inputName = el.name;
+							const Component = el.Component;
+							return (
+								<Component
+									key={i}
+									type={el.type}
+									label={el.label}
+									name={inputName}
+									value={form.inputName}
+									onChange={handleChange}
+									className={el.inputClassName}
+									icon={el.icon}
+									inputContainerClassName={el.inputContainerClassName}
+								/>
+							);
+						})}
 						<Button
 							buttonStyles={{width: "7.25rem", height: "2.125rem"}}
 							text="Enviar"
+							type="submit"
 							className="blueGradient"
 						/>
 					</form>
