13a5016925233ec3f90f0d270c0b5efff9fe1b69
Cristina M <mcristinamotoc@gmail.com>
2021-03-09 17:13:38 +0100

    change route AdList

---
+++ src/screens/AdList/AdList/AdList.js
@@ -0,0 +1,60 @@
+import React from "react";
+import AdCard from "screens/AdList/AdCard/AdCard";
+import Body from "components/layout/Body/Body";
+import {adCardImage} from "assets/images";
+import {
+	StyledTitle,
+	StyledWrapper,
+	RowWrapper,
+	StyledTreeSearch,
+	StyledCard,
+	StyledAdList,
+} from "./AdList.style.js";
+import {faMapMarkerAlt} from "@fortawesome/free-solid-svg-icons";
+import IconWithLabel from "components/units/IconWithLabel/IconWithLabel";
+import {Container} from "theme/GlobalStyles.js";
+const AdList = () => {
+	const adList = [
+		{
+			image: {src: {adCardImage}, alt: "Casa Piscina"},
+			title: "Piso en calle Ángel Puech, Valdeacederas, Madrid ",
+			price: "990 €/mes",
+			rooms: "3 habitaciones",
+			surface: "95m2",
+			includedExpenses: true,
+			description:
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+		},
+	];
+	const [ad] = adList;
+	return (
+		<Body title="Pisos en Alquiler en Madrid">
+			<Container row>
+				<StyledAdList>
+					<StyledTreeSearch>
+						<label>Madrid</label> > <label>Alquiler</label>
+					</StyledTreeSearch>
+					<RowWrapper>
+						<StyledTitle>Listado de pisos</StyledTitle>
+						<IconWithLabel text="Vista de mapa" icon={faMapMarkerAlt} />
+					</RowWrapper>
+					<StyledWrapper>
+						<StyledCard>
+							{" "}
+							<AdCard {...ad} />
+						</StyledCard>
+						<StyledCard>
+							{" "}
+							<AdCard {...ad} />
+						</StyledCard>
+						<StyledCard>
+							{" "}
+							<AdCard {...ad} />
+						</StyledCard>
+					</StyledWrapper>
+				</StyledAdList>
+			</Container>
+		</Body>
+	);
+};
+export default AdList;
