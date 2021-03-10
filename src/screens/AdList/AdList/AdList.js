54f15a1c4e42b16a9f5fe1607b3a9e6547865d4a
Maria Tibau <maria.ta.89@gmail.com>
2021-03-10 18:00:56 +0100

    add scripts to start server

--- src/screens/AdList/AdList/AdList.js
+++ src/screens/AdList/AdList/AdList.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import AdCard from "screens/AdList/AdCard/AdCard";
 import Body from "components/layout/Body/Body";
-import {adCardImage} from "assets/images";
+import {getAds} from "api/ads";
 import {
 	StyledTitle,
 	StyledWrapper,
@@ -13,44 +13,38 @@ import {
 import {faMapMarkerAlt} from "@fortawesome/free-solid-svg-icons";
 import IconWithLabel from "components/units/IconWithLabel/IconWithLabel";
 import {Container} from "theme/GlobalStyles.js";
+
 const AdList = () => {
-	const adList = [
-		{
-			image: {src: {adCardImage}, alt: "Casa Piscina"},
-			title: "Piso en calle Ángel Puech, Valdeacederas, Madrid ",
-			price: "990 €/mes",
-			rooms: "3 habitaciones",
-			surface: "95m2",
-			includedExpenses: true,
-			description:
-				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-		},
-	];
-	const [ad] = adList;
+	const [ads, setAds] = useState([]);
+
+	useEffect(() => {
+		try {
+			getAds().then((ads) => setAds(ads));
+		} catch (e) {
+			console.log(e);
+		}
+	}, []);
 	return (
 		<Body title="Pisos en Alquiler en Madrid">
 			<Container row>
 				<StyledAdList>
 					<StyledTreeSearch>
-						<label>Madrid</label> > <label>Alquiler</label>
+						<label>Madrid</label>
+						{">"}
+						<label>Alquiler</label>
 					</StyledTreeSearch>
 					<RowWrapper>
 						<StyledTitle>Listado de pisos</StyledTitle>
 						<IconWithLabel text="Vista de mapa" icon={faMapMarkerAlt} />
 					</RowWrapper>
 					<StyledWrapper>
-						<StyledCard>
-							{" "}
-							<AdCard {...ad} />
-						</StyledCard>
-						<StyledCard>
-							{" "}
-							<AdCard {...ad} />
-						</StyledCard>
-						<StyledCard>
-							{" "}
-							<AdCard {...ad} />
-						</StyledCard>
+						{ads.map((ad) => {
+							return (
+								<StyledCard>
+									<AdCard {...ad} />
+								</StyledCard>
+							);
+						})}
 					</StyledWrapper>
 				</StyledAdList>
 			</Container>
