5d145f32ab920b1745ee33d290b2d7580fe351c5
Maria Tibau <maria.ta.89@gmail.com>
2021-03-11 17:08:45 +0100

    create routes

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Registration from "screens/UserFlow/Registration/Registration";
 import RecoverPassword from "screens/UserFlow/RecoverPassword/RecoverPassword";
 import Profile from "screens/UserFlow/Profile/Profile";
 import AdList from "screens/AdList/AdList/AdList";
+import UserAds from "screens/UserAds/UserAds";
 
 import GoToMap from "components/composed/Map/GoToMap";
 import MapView from "components/composed/Map/MapView";
@@ -19,8 +20,9 @@ const App = () => {
 		<>
 			<Switch>
 				<Route exact path="/" component={Home} />
-				<Route exact path="/ad" component={Ad} />
+				<Route exact path="/ad/:id" component={Ad} />
 				<Route exact path="/new-ad" component={CreateNewAd} />
+				<Route exact path="/user-ads" component={UserAds} />
 				<Route exact path="/login" component={Login} />
 				<Route exact path="/register" component={Registration} />
 				<Route exact path="/profile" component={Profile} />
