1d84abe6e8173cc054b151b20e3f050cb995771d
sarah <sschmidt0@uoc.edu>
2021-04-07 13:00:56 +0200

    implemented echarts line

--- src/screens/LinealGraphic/LinealGraphic.js
+++ src/screens/LinealGraphic/LinealGraphic.js
@@ -1,10 +1,28 @@
 import Body from "components/layout/Body/Body";
 import React from "react";
-import Card from "../../components/composed/Card/Card";
+import ReactEcharts from "echarts-for-react";
+
 function LinealGraphic() {
 	return (
-		<Body title="Lineal Graphic">
-			<Card />
+		<Body title="Lineal Graphic" isLoggedIn={true}>
+			<ReactEcharts
+				style={{height: "40vh", left: 50, top: 50, width: "60vw"}}
+				option={{
+					xAxis: {
+						type: "category",
+						data: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+					},
+					yAxis: {
+						type: "value",
+					},
+					series: [
+						{
+							data: [150, 230, 224, 218, 135, 147, 260],
+							type: "line",
+						},
+					],
+				}}
+			/>
 		</Body>
 	);
 }
