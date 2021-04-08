ab099eefc47cb4d5e33e457aaf03e675c71de52e
sarah <sschmidt0@uoc.edu>
2021-04-08 10:40:41 +0200

    echarts-for-react deinstalled

--- src/screens/LinealGraphic/LinealGraphic.js
+++ src/screens/LinealGraphic/LinealGraphic.js
@@ -1,6 +1,5 @@
 import Body from "components/layout/Body/Body";
 import React from "react";
-import ReactEcharts from "echarts-for-react";
 
 // import styles
 import {CardChart, CardHeader, CardHeaderTitle, CardHeaderSelect} from "./LinealGraphic.styles";
@@ -18,24 +17,6 @@ function LinealGraphic() {
 						<option value="audi">Audi</option>
 					</CardHeaderSelect>
 				</CardHeader>
-				<ReactEcharts
-					style={{height: "30vh", left: 50, top: 50, width: "40vw"}}
-					option={{
-						xAxis: {
-							type: "category",
-							data: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
-						},
-						yAxis: {
-							type: "value",
-						},
-						series: [
-							{
-								data: [150, 230, 224, 218, 135, 147, 260],
-								type: "line",
-							},
-						],
-					}}
-				/>
 			</CardChart>
 		</Body>
 	);
