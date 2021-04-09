91562d8f6c6fe59468cfd580f0b5383b19e4397d
sarah <sschmidt0@uoc.edu>
2021-04-09 10:14:09 +0200

    generateData changed and 4 categories added

--- src/utils/generateData.js
+++ src/utils/generateData.js
@@ -1,23 +1,26 @@
 const dayInMiliseconds = 24 * 60 * 60 * 1000;
-const initialDate = new Date(2020, 4, 8);
-const values = [30, 80];
+//const initialDate = new Date(2020, 4, 8);
+//const values = [30, 80];
 const randomValue = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
-const generateData = (startDay, days, rangeValues) => {
+export const generateData = (startDay, days, rangeValues) => {
 	const data = [];
 	const [min, max] = rangeValues;
 
 	for (let i = 0; i < days; i++) {
 		let day = new Date(startDay.getTime() + dayInMiliseconds * i);
-		let value = randomValue(min, max);
+		//let value = randomValue(min, max);
 		let option = {
 			day,
-			value,
+			pisos: randomValue(min, max),
+			garajes: randomValue(min, max),
+			locales: randomValue(min, max),
+			chalets: randomValue(min, max),
 		};
 		data.push(option);
 	}
 	return data;
 };
 
-const options = generateData(initialDate, 30, values);
-console.log("options", options);
+//const options = generateData(initialDate, 30, values);
+//console.log("options", options);
