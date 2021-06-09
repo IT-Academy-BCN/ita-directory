12d1701c828e7eb314be4369d644ab494d451c6a
monCecilia <mcmoneta@gmail.com>
2021-06-09 10:59:53 -0300

    maquetar pantalla grande al 60%

--- src/components/composed/Gallery/Gallery.styles.js
+++ src/components/composed/Gallery/Gallery.styles.js
@@ -1,7 +1,10 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    width: 100%; 
-    padding-right: 3rem;
-    padding-left: 3rem;
+	width: 60%;
+	padding-right: 3rem;
+	padding-left: 3rem;
+	@media screen and (max-width: 768px) {
+		width: 90%;
+	}
 `;
