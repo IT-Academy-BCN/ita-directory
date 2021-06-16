0b391123be7b96a37ea62b71377ae6aade4ccaed
monCecilia <mcmoneta@gmail.com>
2021-06-16 16:02:31 -0300

    avance tarea en map. pop up map

--- src/components/composed/Map/MapMarkers.js
+++ src/components/composed/Map/MapMarkers.js
@@ -4,8 +4,8 @@ import {icon} from "./MapIcon";
 import MapPopup from "./MapPopup";
 
 const MapMarkers = ({apartments}) => {
-	const markers = apartments.map((apartment, i) => (
-		<Marker key={i} position={apartment.geometry} icon={icon}>
+	const markers = apartments.slice(0, 20).map((apartment, i) => (
+		<Marker key={i} position={[apartment.lat, apartment.long]} icon={icon}>
 			<MapPopup data={apartment} />
 		</Marker>
 	));
