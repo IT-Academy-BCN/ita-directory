382546c4a95e32b908340a1524e34346a1e4881e
monCecilia <mcmoneta@gmail.com>
2021-06-17 12:11:00 -0300

    avance view map

--- src/components/composed/Map/MapView.js
+++ src/components/composed/Map/MapView.js
@@ -1,33 +1,66 @@
-import React, {useState} from "react";
+import React from "react";
 import {MapContainer, TileLayer} from "react-leaflet";
 import MapMarkers from "./MapMarkers";
 import "leaflet/dist/leaflet.css";
 import "./MapView.css";
 
 const MapView = ({filteredAds}) => {
-	const outer = [
-		[43.26544319441563, -5], //bilbao
-		[39.48299617133865, 5], //valencia
-	];
+	const dataMarkers = filteredAds;
+	console.log(dataMarkers);
 
-	const [bounds, setBounds] = useState(outer);
+	function getMax(dataMarkers, prop) {
+		let max;
+		for (let i = 0; i < dataMarkers.length; i++) {
+			if (max == null || parseFloat(dataMarkers[i][prop]) > parseFloat(max[prop]))
+				max = dataMarkers[i];
+		}
+		return max;
+	}
 
-	// eslint-disable-next-line no-unused-vars
-	const onClickOuter = () => {
-		setBounds({bounds: outer});
-	};
-	// eslint-disable-next-line
-	const [state, setState] = useState({
-		currentLocation: {lat: 40.34572785994146, lng: -1.106286485224387}, //teruel
-		zoom: 6,
-	});
+	let maxLat = getMax(dataMarkers, "lat");
+	console.log(maxLat);
+	let maxLong = getMax(dataMarkers, "long");
+	console.log(maxLong);
+
+	function getMin(dataMarkers, prop) {
+		let min;
+		for (let i = 0; i < dataMarkers.length; i++) {
+			if (min == null || parseFloat(dataMarkers[i][prop]) < parseFloat(min[prop]))
+				min = dataMarkers[i];
+		}
+		return min;
+	}
+	let minLat = getMin(dataMarkers, "lat");
+	console.log(minLat);
+	let minLong = getMin(dataMarkers, "long");
+	console.log(minLong);
+
+	const outer = [maxLat, minLat, maxLong, minLong];
+	console.log(outer);
+
+	//AQUI COMENZAMOS A PLANTEAR EL BOUND, NO ESTÁ DESARROLLADO
+	// function getBoundView(outer){
+	// const [bounds, setBounds] = useState(outer);
+	//   const map = useMap();
+	//   setBounds(innerBounds)
+	//   map.fitBounds(innerBounds)
+
+	// }
+
+	// const [bounds, setBounds] = useState(outer);
+
+	// // eslint-disable-next-line no-unused-vars
+	// const onClickOuter = () => {
+	// 	setBounds({bounds: outer});
+	// };
+	// // eslint-disable-next-line
 
 	return (
 		<MapContainer
 			className="Container-View"
-			center={state.currentLocation}
-			zoom={state.zoom}
-			bound={bounds}
+			// center={state.currentLocation}
+			// zoom={state.zoom}
+			// bound={bounds}
 		>
 			<TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 			<MapMarkers apartments={filteredAds} />
