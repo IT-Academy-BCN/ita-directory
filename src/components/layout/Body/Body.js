9957bedcbd58ca7914641f608ed05517ab3354ef
Kevin Mamaqi <kevinmamaqi@gmail.com>
2021-06-28 09:39:01 +0200

    actualizar repositorio

--- src/components/layout/Body/Body.js
+++ src/components/layout/Body/Body.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "components/layout/Header/Header";
 import Footer from "components/layout/Footer/Footer";
-import {Childrens, StyledBody} from "./styles";
+import {Childrens, StyledBody} from "./Body.styles";
 
 const Body = ({
 	children,
