4e4d869645cb4625cbd59646c4149954e35dc3fc
felipovicc <felipovicc@gmail.com>
2021-09-07 13:35:24 +0200

    Modificar estilo desplegable

--- src/components/composed/Map/IconSelector/IconsSelector.styles.js
+++ src/components/composed/Map/IconSelector/IconsSelector.styles.js
@@ -2,12 +2,14 @@ import styled from "styled-components";
 
 export const IconContainer = styled.div`
 	height: 50px;
-	width: 450px;
-	background: rgba(255, 255, 255, 0.5);
+	width: auto;
+	background: rgba(255, 255, 255, 0.8);
+	border-radius: 5px;
 	z-index: 999;
 	position: absolute;
 	transform: translate(10%, -120%);
 	display: felex;
+	align-items: center;
 	flex-direction: row;
 	flex-wrap: wrap;
 `;
@@ -16,4 +18,5 @@ export const IconImg = styled.div`
 	width: auto;
 	height: auto;
 	background: transparent;
+	padding: 2px;
 `;
