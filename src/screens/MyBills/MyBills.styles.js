61da378f296137a7f93c4f413cf642fe2ed16b70
Ainhoa <ainhoaactic@gmail.com>
2021-09-09 09:43:08 +0200

    when there's no bill, show icon on the right

--- src/screens/MyBills/MyBills.styles.js
+++ src/screens/MyBills/MyBills.styles.js
@@ -21,7 +21,7 @@ export const StyledWrapper = styled.div`
 	.actions-column {
 		display: flex;
 		align-items: center;
-		justify-content: space-between;
+		justify-content: end;
 		width: 40px;
 
 		button {
