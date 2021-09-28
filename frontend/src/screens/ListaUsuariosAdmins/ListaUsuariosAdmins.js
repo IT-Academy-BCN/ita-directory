cb7570a847ef86a46bac09a555be9a8dd3eaadb4
maze02 <71835134+maze02@users.noreply.github.com>
2021-09-28 11:45:35 +0200

    userlistadmin- about to modify photo problem

--- frontend/src/screens/ListaUsuariosAdmins/ListaUsuariosAdmins.js
+++ frontend/src/screens/ListaUsuariosAdmins/ListaUsuariosAdmins.js
@@ -254,11 +254,11 @@ export default ListaUsuariosAdmins;
 
 /*import React, {useState} from "react";
 import {
-	faUserClock,
-	faUserCheck,
-	faUserAltSlash,
-	faTrash,
-	faEye,
+    faUserClock,
+    faUserCheck,
+    faUserAltSlash,
+    faTrash,
+    faEye,
 } from "@fortawesome/free-solid-svg-icons";
 import Body from "components/layout/Body/Body";
 import {Container} from "theme/GlobalStyles.js";
@@ -275,180 +275,180 @@ import EditProfile from "components/composed/EditProfileModal/EditProfile.js";
 import {StyledWrapper, StyledImage, StyledDiv} from "./ListaUsuariosAdmins.style";
 
 function ListaUsuariosAdmins() {
-	const images = [people1b, people4b, people13b];
-	const [active, setActive] = useState(false);
-	const [dataUsers, setDataUsers] = useState(usuarios);
+    const images = [people1b, people4b, people13b];
+    const [active, setActive] = useState(false);
+    const [dataUsers, setDataUsers] = useState(usuarios);
 
-	//Delete user
-	const [eliminar, setEliminar] = useState(false);
-	const [currentColum, setCurrentColum] = useState("");
+    //Delete user
+    const [eliminar, setEliminar] = useState(false);
+    const [currentColum, setCurrentColum] = useState("");
 
-	// Current user
-	const [currentName, setCurrentName] = useState("");
-	const [editar, setEditar] = useState(false);
-	const [currentUserState, setCurrentUserState] = useState("pending");
+    // Current user
+    const [currentName, setCurrentName] = useState("");
+    const [editar, setEditar] = useState(false);
+    const [currentUserState, setCurrentUserState] = useState("pending");
 
-	//Edit Profile
-	const [currentEmail, setCurrentEmail] = useState("");
+    //Edit Profile
+    const [currentEmail, setCurrentEmail] = useState("");
 
-	const handleModalStatus = (name, state) => {
-		setCurrentName(name);
-		setCurrentUserState(state);
-		setActive(true);
-	};
+    const handleModalStatus = (name, state) => {
+        setCurrentName(name);
+        setCurrentUserState(state);
+        setActive(true);
+    };
 
-	const handleModalDelete = (row) => {
-		setCurrentColum(row);
-		setEliminar(true);
-	};
+    const handleModalDelete = (row) => {
+        setCurrentColum(row);
+        setEliminar(true);
+    };
 
-	const handleModalEdit = (name, email) => {
-		setCurrentName(name);
-		setCurrentEmail(email);
-		setEditar(true);
-	};
+    const handleModalEdit = (name, email) => {
+        setCurrentName(name);
+        setCurrentEmail(email);
+        setEditar(true);
+    };
 
-	const updateDelete = (user) => {
-		const newUsers = dataUsers.filter(function (user) {
-			return user !== currentColum;
-		});
+    const updateDelete = (user) => {
+        const newUsers = dataUsers.filter(function (user) {
+            return user !== currentColum;
+        });
 
-		setDataUsers(newUsers);
-	};
+        setDataUsers(newUsers);
+    };
 
-	const updateUserData = (newName, newEmail) => {
-		setDataUsers(
-			dataUsers.map((item) =>
-				item.nombre === currentName || item.email === currentEmail
-					? {...item, nombre: newName, email: newEmail}
-					: item
-			)
-		);
-	};
+    const updateUserData = (newName, newEmail) => {
+        setDataUsers(
+            dataUsers.map((item) =>
+                item.nombre === currentName || item.email === currentEmail
+                    ? {...item, nombre: newName, email: newEmail}
+                    : item
+            )
+        );
+    };
 
-	const updateUserStatus = (val, nombreUsuario) => {
-		setDataUsers(
-			dataUsers.map((t) => (t.nombre === nombreUsuario ? {...t, acciones: val} : t))
-		);
-	};
+    const updateUserStatus = (val, nombreUsuario) => {
+        setDataUsers(
+            dataUsers.map((t) => (t.nombre === nombreUsuario ? {...t, acciones: val} : t))
+        );
+    };
 
-	const columns = [
-		{
-			name: (
-				<StyledDiv color={Colors.frenchBlue} paddingL="0px">
-					Foto
-				</StyledDiv>
-			),
-			selector: "foto",
-			cell: (row) => (
-				<StyledDiv>
-					{<StyledImage src={images[row.id]} alt="foto" width="30px" height="30px" />}
-				</StyledDiv>
-			),
-			sortable: true,
-			compact: true,
-			minWidth: "32px",
-			hide: 600,
-		},
-		{
-			name: (
-				<StyledDiv color={Colors.frenchBlue} padding="0">
-					Nombre
-				</StyledDiv>
-			),
-			selector: "nombre",
-			cell: (row) => <StyledDiv color={Colors.frenchBlue}>{row.nombre}</StyledDiv>,
-			sortable: true,
-			compact: true,
-			minWidth: "40px",
-		},
-		{
-			name: <StyledDiv color={Colors.frenchBlue}>Email</StyledDiv>,
-			selector: "email",
-			cell: (row) => <StyledDiv color={Colors.extraDarkBlue}>{row.email}</StyledDiv>,
-			sortable: true,
-			compact: true,
-			minWidth: "60px",
-		},
-		{
-			name: <StyledDiv color={Colors.frenchBlue}>Acciones</StyledDiv>,
-			selector: "acciones",
-			sortable: true,
-			compact: true,
-			right: true,
-			minWidth: "140px",
-			cell: (row) => (
-				<div className="actions-column">
-					<button onClick={() => handleModalStatus(row.nombre, row.acciones)}>
-						<FontAwesomeIcon
-							icon={
-								row.acciones === "rejected"
-									? faUserAltSlash
-									: row.acciones === "aprobado"
-									? faUserCheck
-									: faUserClock
-							}
-							color={
-								row.acciones === "rejected"
-									? Colors.redColor
-									: row.acciones === "aprobado"
-									? Colors.darkGreen
-									: Colors.grey
-							}
-						></FontAwesomeIcon>
-					</button>
-					<button onClick={() => handleModalEdit(row.nombre, row.email, row.id)}>
-						<FontAwesomeIcon icon={faEye} color={Colors.extraDarkBlue} />
-					</button>
-					<button onClick={() => handleModalDelete(row)}>
-						<FontAwesomeIcon icon={faTrash} color={Colors.redColor} />
-					</button>
-				</div>
-			),
-		},
-	];
+    const columns = [
+        {
+            name: (
+                <StyledDiv color={Colors.frenchBlue} paddingL="0px">
+                    Foto
+                </StyledDiv>
+            ),
+            selector: "foto",
+            cell: (row) => (
+                <StyledDiv>
+                    {<StyledImage src={images[row.id]} alt="foto" width="30px" height="30px" />}
+                </StyledDiv>
+            ),
+            sortable: true,
+            compact: true,
+            minWidth: "32px",
+            hide: 600,
+        },
+        {
+            name: (
+                <StyledDiv color={Colors.frenchBlue} padding="0">
+                    Nombre
+                </StyledDiv>
+            ),
+            selector: "nombre",
+            cell: (row) => <StyledDiv color={Colors.frenchBlue}>{row.nombre}</StyledDiv>,
+            sortable: true,
+            compact: true,
+            minWidth: "40px",
+        },
+        {
+            name: <StyledDiv color={Colors.frenchBlue}>Email</StyledDiv>,
+            selector: "email",
+            cell: (row) => <StyledDiv color={Colors.extraDarkBlue}>{row.email}</StyledDiv>,
+            sortable: true,
+            compact: true,
+            minWidth: "60px",
+        },
+        {
+            name: <StyledDiv color={Colors.frenchBlue}>Acciones</StyledDiv>,
+            selector: "acciones",
+            sortable: true,
+            compact: true,
+            right: true,
+            minWidth: "140px",
+            cell: (row) => (
+                <div className="actions-column">
+                    <button onClick={() => handleModalStatus(row.nombre, row.acciones)}>
+                        <FontAwesomeIcon
+                            icon={
+                                row.acciones === "rejected"
+                                    ? faUserAltSlash
+                                    : row.acciones === "aprobado"
+                                    ? faUserCheck
+                                    : faUserClock
+                            }
+                            color={
+                                row.acciones === "rejected"
+                                    ? Colors.redColor
+                                    : row.acciones === "aprobado"
+                                    ? Colors.darkGreen
+                                    : Colors.grey
+                            }
+                        ></FontAwesomeIcon>
+                    </button>
+                    <button onClick={() => handleModalEdit(row.nombre, row.email, row.id)}>
+                        <FontAwesomeIcon icon={faEye} color={Colors.extraDarkBlue} />
+                    </button>
+                    <button onClick={() => handleModalDelete(row)}>
+                        <FontAwesomeIcon icon={faTrash} color={Colors.redColor} />
+                    </button>
+                </div>
+            ),
+        },
+    ];
 
-	return (
-		<Body
-			title="Usuarios registrados"
-			color_logo={Colors.extraDarkBlue}
-			color_header={Colors.extraDarkBlue}
-			color_letra={Colors.white}
-			justifyTitle="flex-start"
-			paddingTitle="0px"
-			paddingTitle2="73px"
-			isLoggedIn="true"
-		>
-			<Container row>
-				<StyledWrapper>
-					<DataTable columns={columns} data={dataUsers} noHeader={true} />
-				</StyledWrapper>
-			</Container>
-			<UserModal
-				nombreUsuario={currentName}
-				currentUserState={currentUserState}
-				active={active}
-				hideModal={() => setActive(false)}
-				updateUserStatus={updateUserStatus}
-			/>
-			<DeleteModal
-				columnSelect={currentColum}
-				currentUser={eliminar}
-				active={eliminar}
-				hideModal={() => setEliminar(false)}
-				updateDelete={updateDelete}
-			/>
-			<EditProfile
-				currentNombre={currentName}
-				currentEmail={currentEmail}
-				active={editar}
-				hideModal={() => setEditar(false)}
-				updateUserData={updateUserData}
-				setCurrentNombre={setCurrentName}
-				// updateCerrar={updateCerrar}
-			/>
-		</Body>
-	);
+    return (
+        <Body
+            title="Usuarios registrados"
+            color_logo={Colors.extraDarkBlue}
+            color_header={Colors.extraDarkBlue}
+            color_letra={Colors.white}
+            justifyTitle="flex-start"
+            paddingTitle="0px"
+            paddingTitle2="73px"
+            isLoggedIn="true"
+        >
+            <Container row>
+                <StyledWrapper>
+                    <DataTable columns={columns} data={dataUsers} noHeader={true} />
+                </StyledWrapper>
+            </Container>
+            <UserModal
+                nombreUsuario={currentName}
+                currentUserState={currentUserState}
+                active={active}
+                hideModal={() => setActive(false)}
+                updateUserStatus={updateUserStatus}
+            />
+            <DeleteModal
+                columnSelect={currentColum}
+                currentUser={eliminar}
+                active={eliminar}
+                hideModal={() => setEliminar(false)}
+                updateDelete={updateDelete}
+            />
+            <EditProfile
+                currentNombre={currentName}
+                currentEmail={currentEmail}
+                active={editar}
+                hideModal={() => setEditar(false)}
+                updateUserData={updateUserData}
+                setCurrentNombre={setCurrentName}
+                // updateCerrar={updateCerrar}
+            />
+        </Body>
+    );
 }
 export default ListaUsuariosAdmins;
 */
