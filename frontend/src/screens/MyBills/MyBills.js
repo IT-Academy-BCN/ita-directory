357ef74edd1f24abd1b36c61d5110bf5c508f795
Carlos <cperezwuait@gmail.com>
2021-09-29 11:23:09 +0200

    revision de style y /my-bills

--- frontend/src/screens/MyBills/MyBills.js
+++ frontend/src/screens/MyBills/MyBills.js
@@ -4,12 +4,10 @@ import React, {useState, useMemo} from "react";
 import Body from "components/layout/Body/Body";
 import {faEye} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {Container} from "theme/GlobalStyles.js";
 import Colors from "theme/Colors";
-// import DataTable from "react-data-table-component";
 import bills from "./billsData.json";
 import {Link} from "react-router-dom";
-import {StyledWrapper, StyledDiv} from "./MyBills.styles";
+import {MyTableStyle, RowTableStyle, HeaderTableStyle} from "./MyBills.styles";
 import DownloadPDF from "./DocumentComponent";
 import modelBill from "./modelBillData.json";
 
@@ -31,103 +29,100 @@ const MyBills = () => {
 	const columns = useMemo(() => [
 		{
 			Header: (
-				<StyledDiv color={Colors.frenchBlue} style={{textAlign: "left"}}>
+				<HeaderTableStyle color={Colors.frenchBlue} style={{textAlign: "left"}}>
 					N.
-				</StyledDiv>
+				</HeaderTableStyle>
 			),
 			accessor: "id",
 			Cell: ({row}) => (
-				<StyledDiv color={Colors.lightGrey} style={{textAlign: "left"}}>
+				<RowTableStyle color={Colors.lightGrey} style={{textAlign: "left"}}>
 					{row.values.id}
-				</StyledDiv>
+				</RowTableStyle>
 			),
 			left: true,
 		},
 		{
 			Header: (
-				<StyledDiv color={Colors.frenchBlue} style={{textAlign: "left"}}>
+				<HeaderTableStyle color={Colors.frenchBlue} style={{textAlign: "left"}}>
 					Fecha
-				</StyledDiv>
+				</HeaderTableStyle>
 			),
 			accessor: "fecha",
 			Cell: ({row}) => (
-				<StyledDiv color={Colors.grey} style={{textAlign: "left"}}>
+				<RowTableStyle color={Colors.grey} style={{textAlign: "left"}}>
 					{row.values.fecha}
-				</StyledDiv>
+				</RowTableStyle>
 			),
 		},
 		{
 			Header: (
-				<StyledDiv color={Colors.frenchBlue} padding="0">
+				<HeaderTableStyle color={Colors.frenchBlue} padding="0">
 					Coste sin IVA
-				</StyledDiv>
+				</HeaderTableStyle>
 			),
 			accessor: "costeSinIVA",
 			Cell: ({row}) => (
-				<StyledDiv color={Colors.grey}>
+				<RowTableStyle color={Colors.grey}>
 					{row.values.costeSinIVA}
 					<span>€</span>
-				</StyledDiv>
+				</RowTableStyle>
 			),
 		},
 		{
 			Header: (
-				<StyledDiv color={Colors.frenchBlue} padding="0">
+				<HeaderTableStyle color={Colors.frenchBlue} padding="0">
 					IVA
-				</StyledDiv>
+				</HeaderTableStyle>
 			),
 			accessor: "IVA",
 			Cell: ({row}) => (
-				<StyledDiv color={Colors.grey}>
+				<RowTableStyle color={Colors.grey}>
 					{" "}
 					{row.values.IVA} <span>%</span>
-				</StyledDiv>
+				</RowTableStyle>
 			),
 		},
 		{
 			Header: (
-				<StyledDiv color={Colors.frenchBlue} padding="0">
+				<HeaderTableStyle color={Colors.frenchBlue} padding="0">
 					Descuento
-				</StyledDiv>
+				</HeaderTableStyle>
 			),
 			accessor: "descuento",
 			Cell: ({row}) => (
-				<StyledDiv color={Colors.grey}>
+				<RowTableStyle color={Colors.grey}>
 					{" "}
 					{row.values.descuento} {row.values.descuento ? <span>%</span> : <span>-</span>}
-				</StyledDiv>
+				</RowTableStyle>
 			),
 		},
 		{
 			Header: (
-				<StyledDiv color={Colors.frenchBlue} paddingL="6px">
+				<HeaderTableStyle color={Colors.frenchBlue} paddingL="6px">
 					Total
-				</StyledDiv>
+				</HeaderTableStyle>
 			),
 			accessor: "total",
 			Cell: ({row}) => (
-				<StyledDiv color={Colors.grey}>
+				<RowTableStyle color={Colors.grey}>
 					{row.values.descuento
 						? row.values.costeSinIVA -
 						  (row.values.costeSinIVA * row.values.descuento) / 100 +
 						  (row.values.costeSinIVA * row.values.IVA) / 100
 						: row.values.costeSinIVA + (row.values.costeSinIVA * row.values.IVA) / 100}
 					<span>€</span>
-				</StyledDiv>
+				</RowTableStyle>
 			),
 		},
 		{
 			Header: (
-				<StyledDiv color={Colors.frenchBlue} style={{textAlign: "right"}}>
+				<HeaderTableStyle color={Colors.frenchBlue} style={{textAlign: "right"}}>
 					Acciones
-				</StyledDiv>
+				</HeaderTableStyle>
 			),
 			accessor: "acciones",
-			//sortable: true,
-			//compact: true,
-			//right: true,
 			Cell: ({row}) => (
-				<StyledDiv style={{textAlign: "right"}}>
+				<RowTableStyle style={{textAlign: "right"}}>
 					<Link to={`/my-bills/${row.values.id}`} title="Ver factura">
 						<FontAwesomeIcon icon={faEye} color={Colors.grey} />
 					</Link>
@@ -138,7 +133,7 @@ const MyBills = () => {
 								<DownloadPDF key={invoice.id} data={[invoice]} type={"icon"} />
 							)
 					)}
-				</StyledDiv>
+				</RowTableStyle>
 			),
 		},
 	]);
@@ -157,192 +152,35 @@ const MyBills = () => {
 			justifyTitle="flex-start"
 			isLoggedIn="true"
 		>
-			<Container row>
-				<StyledWrapper>
-					<table {...getTableProps()}>
-						<thead>
-							{headerGroups.map((headerGroup) => (
-								<tr {...headerGroup.getHeaderGroupProps()}>
-									{headerGroup.headers.map((column) => (
-										<th {...column.getHeaderProps()}>
-											{column.render("Header")}
-										</th>
-									))}
+			<MyTableStyle>
+				<table {...getTableProps()}>
+					<thead>
+						{headerGroups.map((headerGroup) => (
+							<tr {...headerGroup.getHeaderGroupProps()}>
+								{headerGroup.headers.map((column) => (
+									<th {...column.getHeaderProps()}>{column.render("Header")}</th>
+								))}
+							</tr>
+						))}
+					</thead>
+					<tbody {...getTableBodyProps()}>
+						{rows.map((row) => {
+							prepareRow(row);
+							return (
+								<tr {...row.getRowProps()} style={{borderTop: "solid 1px grey"}}>
+									{row.cells.map((cell) => {
+										return (
+											<td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+										);
+									})}
 								</tr>
-							))}
-						</thead>
-						<tbody {...getTableBodyProps()}>
-							{rows.map((row) => {
-								prepareRow(row);
-								return (
-									<tr
-										{...row.getRowProps()}
-										style={{borderTop: "solid 1px grey"}}
-									>
-										{row.cells.map((cell) => {
-											return (
-												<td {...cell.getCellProps()}>
-													{cell.render("Cell")}
-												</td>
-											);
-										})}
-									</tr>
-								);
-							})}
-						</tbody>
-					</table>
-				</StyledWrapper>
-			</Container>
+							);
+						})}
+					</tbody>
+				</table>
+			</MyTableStyle>
 		</Body>
 	);
 };
-
-// Build the columns table
-// 	const columns = [
-// 		{
-// 			name: (
-// 				<StyledDiv color={Colors.frenchBlue} paddingL="0px">
-// 					N.
-// 				</StyledDiv>
-// 			),
-// 			selector: "id",
-// 			cell: (row) => <StyledDiv color={Colors.lightGrey}>{row.id}</StyledDiv>,
-// 			sortable: true,
-// 			compact: true,
-// 			width: "100px",
-// 			left: true,
-// 			hide: 600,
-// 		},
-// 		{
-// 			name: (
-// 				<StyledDiv color={Colors.frenchBlue} paddingL="0px">
-// 					Fecha
-// 				</StyledDiv>
-// 			),
-// 			selector: "fecha",
-// 			cell: (row) => <div color={Colors.grey}>{row.fecha}</div>,
-// 			sortable: true,
-// 			compact: true,
-// 			width: "100px",
-// 			left: true,
-// 		},
-
-// 		{
-// 			name: (
-// 				<StyledDiv color={Colors.frenchBlue} paddingL="6px">
-// 					Coste sin IVA
-// 				</StyledDiv>
-// 			),
-// 			selector: "costeSinIVA",
-// 			cell: (row) => (
-// 				<div color={Colors.grey}>
-// 					{row.costeSinIVA}
-// 					<span>€</span>
-// 				</div>
-// 			),
-// 			sortable: true,
-// 			compact: false,
-// 			width: "150px",
-// 			center: true,
-// 		},
-// 		{
-// 			name: (
-// 				<StyledDiv color={Colors.frenchBlue} paddingL="6px">
-// 					IVA
-// 				</StyledDiv>
-// 			),
-// 			selector: "IVA",
-// 			cell: (row) => (
-// 				<div color={Colors.grey}>
-// 					{row.IVA}
-// 					<span>%</span>
-// 				</div>
-// 			),
-// 			sortable: true,
-// 			compact: true,
-// 			width: "100px",
-// 			center: true,
-// 		},
-// 		{
-// 			name: (
-// 				<StyledDiv color={Colors.frenchBlue} paddingL="6px">
-// 					Descuento
-// 				</StyledDiv>
-// 			),
-// 			selector: "descuento",
-// 			cell: (row) => (
-// 				<div color={Colors.grey}>
-// 					{row.descuento}
-// 					{row.descuento ? <span>%</span> : <span>-</span>}
-// 				</div>
-// 			),
-// 			sortable: true,
-// 			compact: true,
-// 			width: "100px",
-// 			center: true,
-// 		},
-// 		{
-// 			name: (
-// 				<StyledDiv color={Colors.frenchBlue} paddingL="6px">
-// 					Total
-// 				</StyledDiv>
-// 			),
-// 			selector: "total",
-// 			cell: (row) => (
-// 				<div color={Colors.grey}>
-// 					{row.descuento
-// 						? row.costeSinIVA -
-// 						  (row.costeSinIVA * row.descuento) / 100 +
-// 						  (row.costeSinIVA * row.IVA) / 100
-// 						: row.costeSinIVA + (row.costeSinIVA * row.IVA) / 100}
-// 					<span>€</span>
-// 				</div>
-// 			),
-// 			sortable: true,
-// 			compact: true,
-// 			width: "100px",
-// 			center: true,
-// 		},
-// 		{
-// 			name: <StyledDiv color={Colors.frenchBlue}>Acciones</StyledDiv>,
-// 			selector: "acciones",
-// 			sortable: true,
-// 			compact: true,
-// 			right: true,
-// 			minWidth: "140px",
-// 			cell: (row) => (
-// 				<div className="actions-column">
-// 					<Link to={`/my-bills/${row.id}`} title="Ver factura">
-// 						<FontAwesomeIcon icon={faEye} color={Colors.grey} />
-// 					</Link>
-
-// 					{modelBill.map(
-// 						(invoice) =>
-// 							invoice.id === row.id && (
-// 								<DownloadPDF key={invoice.id} data={[invoice]} type={"icon"} />
-// 							)
-// 					)}
-// 				</div>
-// 			),
-// 		},
-// 	];
-
-// 	return (
-// 		<Body
-// 			title="Mis facturas"
-// 			color_logo={Colors.darkRed}
-// 			color_header={Colors.lightBlue}
-// 			color_letra={Colors.grey}
-// 			justifyTitle="flex-start"
-// 			isLoggedIn="true"
-// 		>
-// 			<Container row>
-// 				<StyledWrapper>
-// 					<DataTable columns={columns} noHeader={true} data={billsData} />
-// 				</StyledWrapper>
-// 			</Container>
-// 		</Body>
-// 	);
-// };
 
 export default MyBills;
