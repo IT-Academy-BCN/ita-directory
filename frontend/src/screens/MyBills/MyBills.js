175620e55b6b5344a955624cbe375d99181fc1d0
maze02 <71835134+maze02@users.noreply.github.com>
2021-10-04 12:34:31 +0200

    refactored colors to Tailwind Colors

--- frontend/src/screens/MyBills/MyBills.js
+++ frontend/src/screens/MyBills/MyBills.js
@@ -142,12 +142,12 @@ const MyBills = () => {
 		columns,
 		data,
 	});
-
+//#e6f2f2
 	return (
 		<Body
 			title="Mis facturas"
 			color_logo={Colors.darkRed}
-			color_header="#e6f2f2"
+			color_header= {Colors.lightblue} 
 			color_letra={Colors.grey}
 			justifyTitle="flex-start"
 			isLoggedIn="true"
@@ -167,7 +167,7 @@ const MyBills = () => {
 						{rows.map((row) => {
 							prepareRow(row);
 							return (
-								<tr {...row.getRowProps()} style={{borderTop: "solid 1px grey"}}>
+								<tr {...row.getRowProps()} style={{borderTop: `solid 1px ${Colors.grey}`}}>
 									{row.cells.map((cell) => {
 										return (
 											<td {...cell.getCellProps()}>{cell.render("Cell")}</td>
