a8d3e23ac709aa9494321496c94b9ad36a0a0be9
Moncho <monchoforcout@gmail.com>
2021-10-04 12:23:11 +0200

    Revision login

--- backend/app/controllers/users.js
+++ backend/app/controllers/users.js
@@ -3,12 +3,19 @@ const JWT = require("jsonwebtoken");
 const argon2 = require("argon2");
 const {getRedisClient} = require("../utils/initRedis");
 const Hashids = require("hashids");
-const {apiResponse, signToken, signRefreshToken, registerSchema, hashPassword} = require("../utils/utils");
+const {
+	apiResponse,
+	signToken,
+	signRefreshToken,
+	registerSchema,
+	hashPassword,
+} = require("../utils/utils");
 const prisma = require("../../prisma/indexPrisma");
 
 // Refresh token
 exports.getRefreshToken = (req, res) => {
-	let {refreshToken} = req.body;
+	let refreshToken = req.headers.refresh;
+
 	if (!refreshToken) {
 		return res.status(400).json(apiResponse({message: "refresh token missing"}));
 	}
@@ -31,12 +38,11 @@ exports.getRefreshToken = (req, res) => {
 					return res.sendStatus(401);
 				}
 				const accessToken = signToken(userId);
-				refreshToken = await signRefreshToken(userId);
+
 				res.status(200).json(
 					apiResponse({
 						data: {
 							accessToken: accessToken,
-							refreshToken: refreshToken,
 						},
 					})
 				);
@@ -81,7 +87,7 @@ exports.getUser = async (req, res) => {
 		res.status(400).send("Request is empty.");
 	}
 	try {
-		const USER = await prisma.user.findUnique({where: {id: parseInt(req.body.id),},});
+		const USER = await prisma.user.findUnique({where: {id: parseInt(req.body.id)}});
 		console.log("user", USER);
 		if (USER === null) {
 			res.status(204).json({
@@ -111,8 +117,8 @@ exports.registerUser = async (req, res) => {
 		const {...userDTO} = req.body;
 		const validFields = await registerSchema.validateAsync(userDTO);
 
-		const doesExist = await prisma.user.findUnique({where: {email: req.body.email,},});
-		
+		const doesExist = await prisma.user.findUnique({where: {email: req.body.email}});
+
 		if (doesExist !== null) {
 			res.status(400).json(
 				apiResponse({
@@ -124,15 +130,15 @@ exports.registerUser = async (req, res) => {
 		const {privacy, ...userDTO2} = req.body;
 		//Creating user without name or lastnames
 		const passwordHashed = await hashPassword(req.body.password);
-		const newUser = await prisma.user.create(
-			{data: {
-					email: req.body.email,
-					password: passwordHashed,
-					user_status_id: 1,
-					user_role_id: 3,
-					refresh_token: "20"
-				},
-			});
+		const newUser = await prisma.user.create({
+			data: {
+				email: req.body.email,
+				password: passwordHashed,
+				user_status_id: 1,
+				user_role_id: 3,
+				refresh_token: "20",
+			},
+		});
 		res.status(200).json(
 			apiResponse({
 				message: "User registered correctly.",
@@ -172,11 +178,10 @@ exports.getAllUsers = async (req, res) => {
 
 // Login
 exports.login = async (req, res) => {
-	const name = req.body.name;
-	const email = req.body.username;
-	const password = req.body.password;
+	const {body = {}} = req;
 	// Check that the request isn't empty
-	if (!email || !password) {
+
+	if (!body.email || !body.password) {
 		res.status(400).send({
 			code: "error",
 			message: "Content can not be empty!",
@@ -185,12 +190,8 @@ exports.login = async (req, res) => {
 	}
 
 	try {
-		const USER = await prisma.mec_user.findOne({
-			attributes: ["id", "mec_pwd"],
-			where: prisma.sequelize.where(
-				prisma.sequelize.fn("lower", prisma.sequelize.col("mec_un")),
-				prisma.sequelize.fn("lower", email)
-			),
+		const USER = await prisma.user.findUnique({
+			where: {email: body.email},
 		});
 
 		if (!USER) {
@@ -202,9 +203,9 @@ exports.login = async (req, res) => {
 			return;
 		}
 
-		let value = await USER.validatePassword(password, USER.mec_pwd);
+		let value = await argon2.verify(USER.password, body.password);
 
-		if (!value) {
+		if (value == false) {
 			res.status(200).send({
 				code: "error",
 				header: "Wrong password",
@@ -213,11 +214,13 @@ exports.login = async (req, res) => {
 			});
 		} else {
 			const token = signToken(USER.id);
+			const refreshToken = signRefreshToken(USER.id);
 			res.status(200).send({
 				code: "success",
 				header: "Welcome back",
 				message: "We are redirecting you to your account.",
 				token,
+				refreshToken,
 			});
 		}
 	} catch (err) {
@@ -228,7 +231,6 @@ exports.login = async (req, res) => {
 		});
 	}
 };
-
 //Update role to user with id_user & id_role (FOR TESTING PURPOSE)
 exports.updateUserRole = async (req, res) => {
 	if (!req.body) {
@@ -395,9 +397,9 @@ exports.forgetPassword = async (req, res) => {
 
 exports.receiveEmailGetToken = async (req, res) => {
 	try {
-		const {user} = req.body;
+		const user = req.body.email;
 
-		const passUser = await prisma.user.findOne({
+		const passUser = await prisma.user.findUnique({
 			where: {
 				email: user,
 			},
@@ -442,7 +444,7 @@ exports.recoverPassword = async (req, res) => {
 			);
 		}
 
-		JWT.verify(token, process.env.JWT_SECRET, (err, authData) => {
+		JWT.verify(token, process.env.JWT_SECRET, (err) => {
 			if (err) {
 				res.status(401).json(
 					apiResponse({
@@ -471,7 +473,7 @@ exports.recoverPassword = async (req, res) => {
 
 exports.changePassword = async (req, res) => {
 	try {
-		const {password, user} = req.body;
+		const {password, email} = req.body;
 
 		// Create hook for update password?
 		const hashedPassword = await argon2.hash(password, {
@@ -481,14 +483,14 @@ exports.changePassword = async (req, res) => {
 			parallelism: 1,
 		});
 
-		const passUser = await prisma.user.findOne({
+		await prisma.user.update({
 			where: {
-				email: user,
+				email: email,
+			},
+			data: {
+				password: hashedPassword,
 			},
 		});
-
-		passUser.password = hashedPassword;
-		passUser.save();
 
 		res.status(200).json(
 			apiResponse({
