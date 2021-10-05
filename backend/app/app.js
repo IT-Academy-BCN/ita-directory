3458b50bb94fac91e5d3bf69b0f4505fec04fce6
Moncho <monchoforcout@gmail.com>
2021-10-05 12:22:32 +0200

    descomentar rutas

--- backend/app/app.js
+++ backend/app/app.js
@@ -8,8 +8,8 @@ const options = require("./utils/swaggerOptions");
 const userRoutes = require("./routes/users");
 
 // const constantsRoute = require("./routes/constants");
-// const adsRoutes = require("./routes/ads");
-// const authenticateToken = require("./middleware/verifyToken");
+ const adsRoutes = require("./routes/ads");
+ const authenticateToken = require("./middleware/verifyToken");
 // const UsersController = require("./controllers/users");
 // const {loadConstants} = require("./utils/CONSTANTS");
 const {loadConstants} = require("./utils/CONSTANTS");
@@ -51,7 +51,7 @@ app.get("/", (req, res) => {
 
 // Routes
 // app.use("/", constantsRoute);
-// app.use("/ads", adsRoutes);
+ app.use("/ads", adsRoutes);
 app.use("/users", userRoutes);
 
 // app.get("/get-token", UsersController.getToken);
