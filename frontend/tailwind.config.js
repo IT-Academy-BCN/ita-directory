b6d8b02857e86b9ffa5c20a875443e49798866cb
Maria Santaeularia <71835134+maze02@users.noreply.github.com>
2021-10-06 09:20:59 +0200

    258 fix invoice UI (#279)
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * working on alignment of bottom calc columns
    
    * Payment and signature section - mobile first refactoring and semantic tags added, responsive layout fixed
    
    * grouped address and users
    
    * new styled components -> InvoiceRecipientStyled & InvoiceSenderStyled to break down large BillStyled component
    
    * refactored styled components so that they are not so big and full of classnames
    
    * added mobile first responsive fixes to address-name-wrapper
    
    * Bill.js - added flex properties to address-name-wrapper
    
    * changed line colour and amended mobile responsive layout for CalcTableStyled component
    
    * fixed disappearing header
    
    * improve alignment of the PaySignStyled Section
    
    * added further changes to improve alignment to PaySignStyled
    
    * Bill.js & Bill.styles :lightGrey0 -> converted to lightGrey1
    
    * Bill.js /Bill.styles.js -> converted headers of ReactTable columns to uppercase using css
    
    * removed lightGrey from Colors.js
    
    * changed many styled component properties to tailwind classnames
    
    * missing bracket added
    
    * refactored into tw  as much as I could within the time available
    
    * Delete Bill.stylesBackUp.js
    
    Co-authored-by: Kevin <kevinmamaqi@users.noreply.github.com>

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,6 +8,38 @@ module.exports = {
 		boxShadow: {
 			"button-shadow": "0px 2px 4px #00000029",
 		},
+		fontSize: {
+			xs: ".75rem",
+			sm: ".875rem",
+			tiny: ".875rem",
+			base: "1rem",
+			lg: "1.125rem",
+			xl: "1.25rem",
+			"2xl": "1.5rem",
+			"3xl": "1.875rem",
+			"4xl": "2.25rem",
+			"5xl": "3rem",
+			"6xl": "4rem",
+			"7xl": "5rem",
+		},
+		screens: {
+			xsm: "480px",
+
+			sm: "640px",
+			// => @media (min-width: 640px) { ... }
+
+			md: "768px",
+			// => @media (min-width: 768px) { ... }
+
+			lg: "1024px",
+			// => @media (min-width: 1024px) { ... }
+
+			xl: "1280px",
+			// => @media (min-width: 1280px) { ... }
+
+			"2xl": "1536px",
+			// => @media (min-width: 1536px) { ... }
+		},
 
 		extend: {
 			colors: {
@@ -15,6 +47,8 @@ module.exports = {
 				black: "#000000",
 				mainColor: "#FFA500	",
 				redColor: "#E12D2D",
+				bloodRed: "#DC2626",
+				paleRed: "#FECACA",
 				lightBlue: "#46C1F7",
 				darkBlue: "#0073E6",
 				frenchBlue: "#0077B3",
@@ -25,6 +59,8 @@ module.exports = {
 				darkRed: "#823434",
 				grey: "#707070",
 				lightGrey: "#999999",
+				lightGrey1: "#ffffff",
+				lightGrey2: "#eeeeee",
 				lightGray: "#B0B0B0",
 				strongBlue: "#006BB9",
 				extraLightGrey: "D8D8D8",
