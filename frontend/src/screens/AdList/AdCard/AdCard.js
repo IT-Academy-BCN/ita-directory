aa11618615ba1ebcc3879ea70e8b9b591cc7e0d9
IMGEORGEMOR <imgeorgemore@icloud.com>
2021-10-21 09:13:22 +0200

    Update issue 298

--- frontend/src/screens/AdList/AdCard/AdCard.js
+++ frontend/src/screens/AdList/AdCard/AdCard.js
@@ -21,13 +21,7 @@ const AdCard = ({
 	n_bathrooms,
 }) => {
 	const [active, setActive] = useState(false);
-	/*
 
-const AdCard = ({image, name, price, m2, gastosIncluidos, desc, habitaciones}) => {
-
-<img src={`${process.env.REACT_APP_STATIC_FILES_URL}/${image}`} alt={name} />
-
-*/
 	const gastosIncluidos = true;
 
 	return (
@@ -43,80 +37,34 @@ const AdCard = ({image, name, price, m2, gastosIncluidos, desc, habitaciones}) =
 						<span>Gastos {gastosIncluidos ? " incluidos" : " no incluidos"}</span>
 					</div>
 					<div className="description">"{description}"</div>
+					<Button
+						buttonStyles={{
+							display: "flex",
+							alignItems: "center",
+							width: "7.5rem",
+							height: "2.2rem",
+							marginTop: "auto",
+							fontSize: "1.125rem",
+							fontFamily: "Arial",
+							color: Colors.strongBlue,
+							background: "transparent",
+							boxShadow: "none",
+							paddingLeft: "0",
+						}}
+						text="Contactar"
+						type="button"
+						icon={faComments}
+						iconPosition="left"
+						iconStyles={{
+							marginRight: 5,
+							paddingLeft: 0,
+						}}
+						onClick={() => setActive(true)}
+					/>
 				</div>
-				<Button
-					buttonStyles={{
-						display: "flex",
-						alignItems: "center",
-						width: "7.5rem",
-						height: "2.2rem",
-						marginTop: "auto",
-						fontSize: "1.125rem",
-						fontFamily: "Arial",
-						color: Colors.strongBlue,
-						background: "transparent",
-						boxShadow: "none",
-						paddingLeft: "0",
-					}}
-					text="Contactar"
-					type="button"
-					icon={faComments}
-					iconPosition="left"
-					iconStyles={{
-						marginRight: 5,
-						paddingLeft: 0,
-					}}
-					onClick={() => setActive(true)}
-				/>
 			</div>
 			<ContactModal active={active} hideModal={() => setActive(false)} />
 		</AdCardStyled>
 	);
 };
 export default AdCard;
-
-/*
-    return (
-        <AdCardStyled>
-            <img src={url} alt={name} />
-            <div className="content">
-                <p className="address">Address here (missing from API).</p>
-                <div className="property-data">
-                    <span className="price">{price} €</span>
-                    <span>{habitaciones} habitaciones</span>
-                    <span>{m2}</span>
-                    <span>Gastos {gastosIncluidos ? " incluidos" : " no incluidos"}</span>
-                </div>
-                <div className="description">{desc}</div>
-                <Button
-                    buttonStyles={{
-                        display: "flex",
-                        alignItems: "center",
-                        width: "7.5rem",
-                        height: "2.2rem",
-                        marginTop: "auto",
-                        fontSize: "1.125rem",
-                        fontFamily: "Arial",
-                        color: Colors.strongBlue,
-                        background: "transparent",
-                        boxShadow: "none",
-                        paddingLeft: "0",
-                    }}
-                    text="Contactar"
-                    type="button"
-                    icon={faComments}
-                    iconPosition="left"
-                    iconStyles={{
-                        marginRight: 5,
-                        paddingLeft: 0,
-                    }}
-                    onClick={() => setActive(true)}
-                />
-            </div>
-            <ContactModal active={active} hideModal={() => setActive(false)} />
-        </AdCardStyled>
-    );
-};
-export default AdCard;
-
-*/
--- frontend/src/screens/AdList/AdList.js
+++ frontend/src/screens/AdList/AdList.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React, {useEffect, useState} from "react";
 import AdCard from "screens/AdList/AdCard/AdCard";
 import Body from "components/layout/Body/Body";
@@ -13,27 +12,24 @@ import AdListFilter from "screens/AdList/AdListFilter/AdListFilter";
 // Styles
 import {AdListStyled} from "./AdList.style.js";
 import {Container} from "theme/GlobalStyles.js";
-import {map} from "leaflet";
 
-/*
 const buttonStyle = {
-    display: "flex",
-    alignItems: "center",
-    width: "auto",
-    height: "auto",
-    fontSize: "0.95rem",
-    fontFamily: "Arial",
-    color: Colors.lightGray,
-    background: "transparent",
-    boxShadow: "none",
-    outline: "none",
-    paddingRight: 0,
+	display: "flex",
+	alignItems: "center",
+	width: "auto",
+	height: "auto",
+	fontSize: "0.95rem",
+	fontFamily: "Arial",
+	color: Colors.lightGray,
+	background: "transparent",
+	boxShadow: "none",
+	outline: "none",
+	paddingRight: 0,
 };
-*/
 
 const AdList = () => {
 	const [filtro, setFiltro] = useState();
-	//const [mapView, setMapView] = useState(true);
+	const [mapView, setMapView] = useState(false);
 	const [filteredAdList, setFilteredAdlist] = useState([]);
 	const [adList, setAdList] = useState([]);
 	const [loading, setLoading] = useState(true);
@@ -44,19 +40,11 @@ const AdList = () => {
 
 	useEffect(() => {
 		const fetchAds = async () => {
-			/*
-            const result = await axios("https://api-casas.kevinmamaqi.com/api-casas", {
-                params: {_limit: 10}, //parece no estar implemententado en el api de casas
-            });*/
-
 			const result = await axios("http://localhost:5000/ads/v1/ads");
-			console.log(result.data.data);
 			setAdList(result.data.data);
 			setLoading(false);
 		};
 		fetchAds();
-		console.log(adList);
-		// eslint-disable-next-line
 	}, []);
 
 	useEffect(() => {
@@ -96,33 +84,14 @@ const AdList = () => {
 		setFilteredAdlist(_filteredAds);
 	}, [filtro, adList]);
 
-	//const renderList = filteredAdList.map((e, index) => <AdCard {...e} key={index}  />);
-
-	const renderList = filteredAdList.map((e, index) => {
-		return (
-			<AdCard
-				key={index}
-				city={e.city}
-				description={e.description}
-				map_lat={e.map_lat}
-				map_lon={e.map_lon}
-				n_bathrooms={e.n_bathrooms}
-				n_rooms={e.n_rooms}
-				price={e.price}
-				square_meters={e.square_meters}
-				title={e.title}
-				user_id={e.user_id}
-			/>
-		);
-	});
+	const renderList = filteredAdList.map((e, index) => <AdCard {...e} key={index} />);
 
 	useEffect(() => {
 		if (loading === false) {
 			let priceValue = Array.from(renderList, (o) => o.props.price);
 			let maxPV = Math.max(...priceValue);
 			let minPV = Math.min(...priceValue);
-			//let sizeValue = Array.from(renderList, (o) => o.props.m2);
-			let sizeValue = Array.from(renderList, (o) => o.props.square_meters);
+			let sizeValue = Array.from(renderList, (o) => o.props.m2);
 			let mxM2 = Math.max(...sizeValue);
 			let mnM2 = Math.min(...sizeValue);
 
@@ -134,9 +103,9 @@ const AdList = () => {
 	}, [renderList, loading]);
 
 	return (
-		<Body title="Pisos en Alquiler en Madrid" isLoggedIn="true" justifyTitle="flex-end">
+		<Body title="Pisos en Alquiler en Madrid" isLoggedIn="true" justifyTitle="flex-start">
 			<AdListStyled>
-				<Container row>
+				<Container row className="probando">
 					{!loading ? (
 						<>
 							<AdListFilter
@@ -147,10 +116,36 @@ const AdList = () => {
 								minM2={minM2}
 							/>
 							<div className="ads">
-								<div className="tree-search">Madrid Alquiler</div>
-								<div className="h3">Listado de pisos</div>
-								<div className="rowWrapper"></div>
-								<div>{renderList}</div>
+								<div className="tree-search">Madrid - Alquiler</div>
+								<div className="h3">Mapa de pisos</div>
+								<div className="rowWrapper">
+									{mapView ? (
+										<Button
+											text="Vista de detalles"
+											icon={faBars}
+											iconPosition="left"
+											iconStyles={{
+												marginRight: 5,
+												paddingLeft: 0,
+											}}
+											onClick={() => setMapView(!mapView)}
+											buttonStyles={buttonStyle}
+										/>
+									) : (
+										<Button
+											text="Vista de mapa"
+											icon={faMapMarkerAlt}
+											iconPosition="left"
+											iconStyles={{
+												marginRight: 5,
+												paddingLeft: 0,
+											}}
+											onClick={() => setMapView(!mapView)}
+											buttonStyles={buttonStyle}
+										/>
+									)}
+								</div>
+								{mapView ? <MapView filteredAds={filteredAdList} /> : renderList}
 							</div>
 						</>
 					) : (
@@ -162,42 +157,3 @@ const AdList = () => {
 	);
 };
 export default AdList;
-
-/*
-<div className="ads">
-                                <div className="tree-search">Madrid - Alquiler</div>
-                                <div className="h3">Mapa de pisos</div>
-                                <div className="rowWrapper">
-                                    {mapView ? (
-                                        <Button
-                                            text="Vista de detalles"
-                                            icon={faBars}
-                                            iconPosition="left"
-                                            iconStyles={{
-                                                marginRight: 5,
-                                                paddingLeft: 0,
-                                            }}
-                                            onClick={() => setMapView(!mapView)}
-                                            buttonStyles={buttonStyle}
-                                        />
-                                    ) : (
-                                        <Button
-                                            text="Vista de mapa"
-                                            icon={faMapMarkerAlt}
-                                            iconPosition="left"
-                                            iconStyles={{
-                                                marginRight: 5,
-                                                paddingLeft: 0,
-                                            }}
-                                            onClick={() => setMapView(!mapView)}
-                                            buttonStyles={buttonStyle}
-                                        />
-                                    )}
-                                </div>
-                                    </div>
-*/
-
-/*
-    {mapView ? <MapView filteredAds={filteredAdList} /> : renderList}
-
-*/
