3a0c9f1950a0b36f2fedaa831117bd229766c925
Arnau Calduch <65604168+ArnauCalduch@users.noreply.github.com>
2021-10-21 09:02:52 +0200

    set jest and supertest (#315)

---
+++ backend/app/_tests_/app.test.js
@@ -0,0 +1,10 @@
+const request = require("supertest");
+
+describe("Post Image", () => {
+	it("should post the image", async () => {
+		/*const res = await request(app.server).post("/media/v1/upload");
+
+		expect(res.statusCode).toEqual(200);
+		expect(res.body).toStrictEqual([]);*/
+	});
+});
