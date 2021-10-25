19eec589aa1115e6be23235e3851f11bf3f3add3
ArnauCalduch <arnaucalduch@gmail.com>
2021-10-25 09:05:06 +0200

    test jest

--- backend/app/_tests_/app.test.js
+++ backend/app/_tests_/app.test.js
@@ -1,10 +1,23 @@
 const request = require("supertest");
+const prisma = require("../../prisma/indexPrisma");
 
 describe("Post Image", () => {
-	it("should post the image", async () => {
-		/*const res = await request(app.server).post("/media/v1/upload");
 
-		expect(res.statusCode).toEqual(200);
-		expect(res.body).toStrictEqual([]);*/
+	const testMedia = {
+		data: {
+			path: `public/2021/10`,
+			mime_type: "jpeg",
+			file_size: "290",
+			user: {
+				connect: {
+					id: 1,
+				},
+			},
+		}
+	};
+
+	it("should post the image", async () => {
+		const success = prisma.media.create({testMedia});
+		expect(success).toBe(true);
 	});
 });
