8a4fc738daf4ec127e95bebd2c6839b41a4cddeb
rubennp <rubennp@users.noreply.github.com>
2021-10-25 15:01:54 +0200

    Added List of page's links to Home (#330)
    * Added List of page's links to Home
    
    * Fix User's Ads route

---
+++ frontend/src/screens/Home/Home.styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import {Container} from "theme/GlobalStyles";
+
+export const HomeContainer = styled(Container)`
+	height: 60vh;
+
+	ul {
+		text-align: center;
+
+		li {
+			margin: 0.5rem;
+		}
+	}
+
+	a {
+		&:hover {
+			text-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+			color: blue;
+		}
+	}
+`;
