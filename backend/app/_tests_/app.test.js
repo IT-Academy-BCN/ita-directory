8c57ffc7ec833a06636755e55d9f7d6543c7430e
ArnauCalduch <arnaucalduch@gmail.com>
2021-10-25 13:32:52 +0200

    try test upload

--- backend/app/_tests_/app.test.js
+++ backend/app/_tests_/app.test.js
@@ -1,23 +1,16 @@
 const request = require("supertest");
-const prisma = require("../../prisma/indexPrisma");
+const fs = require("fs");
+//const prisma = require("../../prisma/indexPrisma");
+const app = require("../app");
 
 describe("Post Image", () => {
 
-	const testMedia = {
-		data: {
-			path: `public/2021/10`,
-			mime_type: "jpeg",
-			file_size: "290",
-			user: {
-				connect: {
-					id: 1,
-				},
-			},
-		}
-	};
-
-	it("should post the image", async () => {
-		const success = prisma.media.create({testMedia});
-		expect(success).toBe(true);
+	it('should upload image', async () => {
+		const res = await request(app.server)
+			.post('media/v1/upload')
+			.set('content-type', 'multipart/form-data')
+			.field('userId', 1)
+			.attach('image', fs.readFileSync(`public/2021/8/jam.jpg`))
+		expect(res.status).toEqual(200);
 	});
 });
