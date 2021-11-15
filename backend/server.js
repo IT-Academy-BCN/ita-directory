45d399abae2eb05d4b141edc2cd1e8bb3a6fb58c
Kevin Mamaqi <kevinmamaqi@gmail.com>
2021-11-15 09:37:31 +0100

    update port close #357

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,10 @@ const handlerError = require("./app/middleware/handler-errors")
 // Create IO server
 const server = http.createServer(app);
 const io = socketio(server, {
-	cors: {
-		origin: process.env.CORS,
-		methods: ["GET", "POST"],
-	},
+    cors: {
+        origin: process.env.CORS,
+        methods: ["GET", "POST"],
+    },
 });
 require("./app/config/sockets")(io);
 
@@ -20,10 +20,7 @@ app.use(handlerError.routeFoundHandler);
 //Error handler
 app.use(handlerError.errorHandler);
 
-
-
-
 server.listen(process.env.PORT, () => {
-	console.log(`Server is running on port and working ${process.env.PORT}.`);
-	console.log(`Visit: http://localhost:${process.env.PORT}/`);
+    console.log(`Server is running on port and working ${process.env.PORT}.`);
+    console.log(`Visit: http://localhost:${process.env.PORT}/`);
 });
