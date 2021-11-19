57da00e4f03bf1e0bccb00d7ebe3ed54d8c5fab1
Marco <marco.zappani@gmail.com>
2021-11-19 12:41:33 +0100

    added GET parent/child route and Swagger documentation, fixed district seeds parent_id

--- backend/app/controllers/location.js
+++ backend/app/controllers/location.js
@@ -24,7 +24,7 @@ async function getRegion(req, res) {
 
         data.push(location[0])
 
-        for (let i = location[0].level_type_id; i >= 2; i--) {
+        for (let i = location[0].level_type_id; i >= 3; i--) {
             let id = location[0].parent_id
             const parent = await prisma.level.findUnique({
                 where: {
@@ -72,4 +72,79 @@ async function getRegion(req, res) {
 
 }
 
-module.exports = {getRegion}
+
+async function getParentChild(req, res) {
+    try {
+        const {name} = req.body
+        await getRegionByLocationSchema.validateAsync(name)
+        const formattedName = formatLocation(name)
+        const data = []
+        const location = await prisma.level.findMany({
+            where: {
+                name: {
+                    contains: formattedName
+                }
+            },
+            orderBy: [
+                {
+                    level_type_id: 'asc',
+                },
+            ]
+        });
+
+
+        for (let i = 0; i < location.length; i++) {
+
+            data.push(location[i])
+            const parent = await prisma.level.findMany({
+                where: {
+                    id: location[i].parent_id
+                }
+            })
+
+            data[i] = {...data[i], parent: parent}
+
+            const children = await prisma.level.findMany({
+                where: {
+                    parent_id: location[i].id
+                },
+            })
+
+            data[i] = {...data[i], children: children}
+        }
+
+
+        if (location.length == 0) {
+            res.status(404).json(
+                apiResponse({
+                    message: "The location is not in the database or you spelled it wrong",
+                })
+            )
+        }
+
+        res.status(200).json({
+            message: "Location fetched",
+            data
+        })
+
+    } catch (err) {
+        if (err.name === "ValidationError") {
+            res.status(400).json(
+                apiResponse({
+                    message: "Name must be a string.",
+                    err: err.message,
+                })
+            );
+        } else {
+            res.status(500).json(
+                apiResponse({
+                    message: "Something wrong occurred with your query.",
+                    err: err.message,
+                })
+            );
+        }
+    }
+
+}
+
+module.exports = {getRegion, getParentChild}
--- backend/app/routes/location.js
+++ backend/app/routes/location.js
@@ -33,4 +33,79 @@ const router = require("express").Router();
 router.get("/v1/region", locationController.getRegion);
 
 
+/**
+ * GET /location/v1/relative
+ * @summary Gets a searched location's parent and children.
+ * @tags Location
+ * @return {object} 200 - Success response - application/json
+ * @example request - Correct location payload
+ * {"name": "montcada"}
+ * @example response - 200 - Example success response
+ * {
+    "message": "Location fetched",
+    "data": [
+        {
+            "id": 28,
+            "name": "Barcelona",
+            "parent_id": 10,
+            "level_type_id": 3,
+            "parent": [
+                {
+                    "id": 10,
+                    "name": "Cataluña",
+                    "parent_id": 1,
+                    "level_type_id": 2
+                }
+            ],
+            "children": [
+                {
+                    "id": 935,
+                    "name": "Abrera",
+                    "parent_id": 28,
+                    "level_type_id": 4
+                },
+                {
+                    "id": 100000000,
+                    "name": "and many more towns",
+                    "parent_id": 28,
+                    "level_type_id": 4
+                }
+            ]
+        },
+        {
+            "id": 955,
+            "name": "Barcelona",
+            "parent_id": 28,
+            "level_type_id": 4,
+            "parent": [
+                {
+                    "id": 28,
+                    "name": "Barcelona",
+                    "parent_id": 10,
+                    "level_type_id": 3
+                }
+            ],
+            "children": [
+                {
+                    "id": 8214,
+                    "name": "Eixample",
+                    "parent_id": 955,
+                    "level_type_id": 5
+                },
+                {
+                    "id": 100000000,
+                    "name": "and many other districts",
+                    "parent_id": 955,
+                    "level_type_id": 5
+                }
+            ]
+        }
+    ]
+}
+ */
+
+
+router.get("/v1/relative", locationController.getParentChild);
+
+
 module.exports = router;
