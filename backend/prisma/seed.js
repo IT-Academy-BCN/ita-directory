bfc73f50f8cfde7a8b8c8679c3c68b053d8a6635
Marco Zappani <77837543+marcostaimusic@users.noreply.github.com>
2021-11-22 14:18:48 +0100

    Added ad_type table and seeds, modified 20210623161027_init migration to solve Prisma issue with seeding (#380)
    Gracias @marcostaimusic

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -110,6 +110,7 @@ const ads = [
 		n_bathrooms: 1,
 		map_lat: 41.385063,
 		map_lon: 2.173404,
+		ad_type_id: 1
 	},
 	{
 		id: 2,
@@ -124,6 +125,7 @@ const ads = [
 		n_bathrooms: 2,
 		map_lat: 52.520008,
 		map_lon: 13.404954,
+		ad_type_id: 1
 	},
 	{
 		id: 3,
@@ -138,6 +140,7 @@ const ads = [
 		n_bathrooms: 1,
 		map_lat: 55.864239,
 		map_lon: -4.251806,
+		ad_type_id: 1
 	},
 	{
 		id: 4,
@@ -152,6 +155,7 @@ const ads = [
 		n_bathrooms: 2,
 		map_lat: 51.924419,
 		map_lon: 4.477733,
+		ad_type_id: 1
 	},
 	{
 		id: 5,
@@ -166,6 +170,7 @@ const ads = [
 		n_bathrooms: 1,
 		map_lat: 39.695263,
 		map_lon: 3.017571,
+		ad_type_id: 1
 	},
 	{
 		id: 6,
@@ -180,6 +185,7 @@ const ads = [
 		n_bathrooms: 2,
 		map_lat: 45.75667,
 		map_lon: 4.831811,
+		ad_type_id: 1
 	},
 	{
 		id: 7,
@@ -194,6 +200,7 @@ const ads = [
 		n_bathrooms: 1,
 		map_lat: 41.55013,
 		map_lon: -8.427734,
+		ad_type_id: 1
 	},
 	{
 		id: 8,
@@ -208,6 +215,7 @@ const ads = [
 		n_bathrooms: 1,
 		map_lat: 40.83607,
 		map_lon: 14.249339,
+		ad_type_id: 1
 	},
 	{
 		id: 9,
@@ -222,6 +230,7 @@ const ads = [
 		n_bathrooms: 2,
 		map_lat: 48.834668,
 		map_lon: 2.350532,
+		ad_type_id: 1
 	},
 	{
 		id: 10,
@@ -236,8 +245,226 @@ const ads = [
 		n_bathrooms: 1,
 		map_lat: 51.499927,
 		map_lon: -0.118429,
+		ad_type_id: 1
+	},
+	{
+		id: 11,
+
+		user_id: 1,
+		title: "ad11",
+		description: "ad room 1",
+		city: "Tampa",
+		n_rooms: 1,
+		price: 300,
+		square_meters: 20,
+		n_bathrooms: 1,
+		map_lat: 27.950575,
+		map_lon: -82.457176,
+		ad_type_id: 2
+	},
+	{
+		id: 12,
+
+		user_id: 1,
+		title: "ad12",
+		description: "ad garage 1",
+		city: "Tulsa",
+		n_rooms: 1,
+		price: 1200,
+		square_meters: 20,
+		n_bathrooms: 2,
+		map_lat: 36.155330,
+		map_lon: -95.992080,
+		ad_type_id: 3
+	},
+	{
+		id: 13,
+
+		user_id: 1,
+		title: "ad13",
+		description: "ad storage 1",
+		city: "Cleveland",
+		n_rooms: 1,
+		price: 400,
+		square_meters: 40,
+		n_bathrooms: 1,
+		map_lat: 41.499321,
+		map_lon: -81.694359,
+		ad_type_id: 4
+	},
+	{
+		id: 14,
+
+		user_id: 1,
+		title: "ad14",
+		description: "ad office 1",
+		city: "Carcassonne",
+		n_rooms: 6,
+		price: 1800,
+		square_meters: 170,
+		n_bathrooms: 1,
+		map_lat: 43.212601,
+		map_lon: 2.355970,
+		ad_type_id: 5
+	},
+	{
+		id: 15,
+
+		user_id: 1,
+		title: "ad15",
+		description: "ad warehouse 1",
+		city: "Tokyo",
+		n_rooms: 4,
+		price: 2000,
+		square_meters: 250,
+		n_bathrooms: 2,
+		map_lat: 35.689487,
+		map_lon: 139.691711,
+		ad_type_id: 6
+	},
+	{
+		id: 16,
+
+		user_id: 1,
+		title: "ad16",
+		description: "ad building 1",
+		city: "Melbourne",
+		n_rooms: 50,
+		price: 20000,
+		square_meters: 5000,
+		n_bathrooms: 20,
+		map_lat: -37.813629,
+		map_lon: 144.963058,
+		ad_type_id: 7
+	},
+	{
+		id: 17,
+
+		user_id: 1,
+		title: "ad17",
+		description: "ad new_building 1",
+		city: "Shanghai",
+		n_rooms: 60,
+		price: 30000,
+		square_meters: 6000,
+		n_bathrooms: 30,
+		map_lat: 31.230391,
+		map_lon: 121.473701,
+		ad_type_id: 8
 	},
 ];
+
+
+const ad_types = [
+	{
+		id: 1,
+		name: "house",
+		house: 1,
+		room: 0,
+		garage: 0,
+		storage: 0,
+		office: 0,
+		warehouse: 0,
+		building: 0,
+		new_building: 0
+	},
+	{
+		id: 2,
+		name: "room",
+		house: 0,
+		room: 1,
+		garage: 0,
+		storage: 0,
+		office: 0,
+		warehouse: 0,
+		building: 0,
+		new_building: 0
+	},
+	{
+		id: 3,
+		name: "garage",
+		house: 0,
+		room: 0,
+		garage: 1,
+		storage: 0,
+		office: 0,
+		warehouse: 0,
+		building: 0,
+		new_building: 0
+	},
+	{
+		id: 4,
+		name: "storage",
+		house: 0,
+		room: 0,
+		garage: 0,
+		storage: 1,
+		office: 0,
+		warehouse: 0,
+		building: 0,
+		new_building: 0
+	},
+	{
+		id: 5,
+		name: "office",
+		house: 0,
+		room: 0,
+		garage: 0,
+		storage: 0,
+		office: 1,
+		warehouse: 0,
+		building: 0,
+		new_building: 0
+	},
+	{
+		id: 6,
+		name: "warehouse",
+		house: 0,
+		room: 0,
+		garage: 0,
+		storage: 0,
+		office: 0,
+		warehouse: 1,
+		building: 0,
+		new_building: 0
+	},
+	{
+		id: 7,
+		name: "building",
+		house: 0,
+		room: 0,
+		garage: 0,
+		storage: 0,
+		office: 0,
+		warehouse: 0,
+		building: 1,
+		new_building: 0
+	},
+	{
+		id: 8,
+		name: "new_building",
+		house: 0,
+		room: 0,
+		garage: 0,
+		storage: 0,
+		office: 0,
+		warehouse: 0,
+		building: 0,
+		new_building: 1
+	},
+	{
+		id: 1,
+		name: "house",
+		house: 1,
+		room: 0,
+		garage: 0,
+		storage: 0,
+		office: 0,
+		warehouse: 0,
+		building: 0,
+		new_building: 0
+	}
+]
 
 const medias = [
 	{
@@ -49718,6 +49945,16 @@ async function main() {
 			create: {
 				...user,
 				password: await hashPassword(user.password),
+			},
+		});
+	}
+	for (let i = 0; i < ad_types.length; i++) {
+		const ad_type = ad_types[i];
+		await prisma.ad_type.upsert({
+			where: {id: ad_type.id},
+			update: {},
+			create: {
+				...ad_type,
 			},
 		});
 	}
