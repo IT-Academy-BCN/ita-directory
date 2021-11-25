abfa33015f35d53fc03cf93fbdc8db43f05331ac
Katy-dotcom <76691416+Katy-dotcom@users.noreply.github.com>
2021-11-25 09:04:00 +0100

    search+load more functionality working (#394)

--- frontend/src/components/composed/AdCardList/AdCardListLoadMoreBtn.js
+++ frontend/src/components/composed/AdCardList/AdCardListLoadMoreBtn.js
@@ -1,30 +1,14 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import {AdCardListStyled} from "./AdCardList.styles";
 import Button from "components/units/Button/Button";
-import axios from "axios";
 import AdCardItem from "./AdCardItem";
 
-const AdCardListLoadMore = () => {
-	const [ads, setAds] = useState([]);
+const AdCardListLoadMore = ({ads}) => {
 	const [currentPage, setCurrentPage] = useState(1);
 
 	const itemsPerPage = 9;
 
 	const adsToShow = [...ads].slice(0, currentPage * itemsPerPage);
-
-	const getAds = async () => {
-		try {
-			const response = await axios.get(`${process.env.REACT_APP_API_URL}/ads/v1/ads`);
-			const newAds = response.data.data;
-			setAds(newAds);
-		} catch (e) {
-			console.error(e);
-		}
-	};
-
-	useEffect(() => {
-		getAds();
-	}, []);
 
 	const showMoreItems = () => {
 		setCurrentPage((prevValue) => prevValue + 1);
--- frontend/src/screens/Search/Search.js
+++ frontend/src/screens/Search/Search.js
@@ -5,40 +5,69 @@ import SearchBar from "components/composed/SearchBar/SearchBar.js";
 import {SearchStyled} from "./Search.style.js";
 /* import Map from "components/composed/Map/Map"; 
 import CardSearch from 'components/composed/CardSearch/CardSearch.js'; */
+import AdCardListLoadMore from "components/composed/AdCardList/AdCardListLoadMoreBtn.js";
+import axios from "axios";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faSpinner} from "@fortawesome/free-solid-svg-icons";
 
 const Search = () => {
-	const [matchesId, setMatchesId] = useState(null);
+	// const [matchesId, setMatchesId] = useState(null);
+	const [loading, setLoading] = useState(0);
+	const [ads, setAds] = useState([]);
+	const [adType, setAdType] = useState(null);
+	const [adRegion, setAdRegion] = useState(null);
+	const [firstSearch, setFirstSearch] = useState(false);
+
+	const getAds = async () => {
+		try {
+			setLoading(1);
+			let response = null;
+			//if type has been selected by user, send an api request for ads filtered by type
+			if (adType) {
+				response = await axios.get(
+					`${process.env.REACT_APP_API_URL}/ads/v1/ads/type/${adType.label}`
+				);
+				//if type hasn´t been selected by user, send an api request for all ads
+			} else {
+				response = await axios.get(`${process.env.REACT_APP_API_URL}/ads/v1/ads/`);
+			}
+			let filteredAds = response.data.data;
+
+			//if region has been selected, filter ads by region
+			if (adRegion) {
+				filteredAds = filteredAds.filter((ad) => ad.city === adRegion.label);
+			}
+			setAds(filteredAds);
+			setLoading(0);
+			setFirstSearch(true);
+		} catch (e) {
+			console.error(e);
+		}
+	};
+
 	return (
 		<div>
 			<Header isTitleVisible={false} />
-			<SearchBar setMatchesId={setMatchesId} matchesId={matchesId} />
+			<SearchBar
+				// setMatchesId={setMatchesId}
+				// matchesId={matchesId}
+				setAdType={setAdType}
+				setAdRegion={setAdRegion}
+				getAds={getAds}
+			/>
 			<SearchStyled>
 				<div className="search-body">
 					<div className="search-results">
-						<div className="search-results-list">
-							<div className="box"></div>
-							<div className="box"></div>
-							<div className="box"></div>
-							<div className="box"></div>
-							<div className="box"></div>
-							<div className="box"></div>
-							<div className="box"></div>
-							<div className="box"></div>
-							<div className="box"></div>
-							<div className="box"></div>
-							<div className="box"></div>
-							<div className="box"></div>
-							<div className="box"></div>
-							<div className="box"></div>
-							<div className="box"></div>
-							<div className="box"></div>
-						</div>
+						{loading ? (
+							<FontAwesomeIcon icon={faSpinner} className="spinner" />
+						) : ads.length === 0 && firstSearch ? (
+							`There are no ${adType?.label} in ${adRegion?.label}.`
+						) : (
+							<AdCardListLoadMore ads={ads} className="search-results-list" />
+						)}
 					</div>
 					<div className="search-map">
 						<div className="map">map here</div>
-					</div>
-					<div className="search-more-button">
-						<button type="button">cargar mas</button>
 					</div>
 				</div>
 			</SearchStyled>
