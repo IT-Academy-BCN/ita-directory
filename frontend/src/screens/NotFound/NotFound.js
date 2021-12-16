f8da09251f8b291887569b5499767776bf4f93aa
badr <badrhand@gmail.com>
2021-12-16 13:58:42 +0100

    use classes inside styled component in NotFound.styles

--- frontend/src/screens/NotFound/NotFound.js
+++ frontend/src/screens/NotFound/NotFound.js
@@ -1,16 +1,17 @@
 import React from "react";
-import { Container, BackHomeLink, HttpMess, ErrorMess, AskToContact, ContactLink} from "./NotFound.styles"
+import { Container} from "./NotFound.styles"
+import {Link} from "react-router-dom";
 
 
 function NotFound() {
 	return (
 		<Container>
 			
-			<HttpMess>404</HttpMess>
-			<ErrorMess> Ops, parece que la página que estas buscando no existe </ErrorMess>
-			<BackHomeLink to="/"> Vuelve a la página de inicio </BackHomeLink>
-			<AskToContact> Si se trata de un error, ponte en contacto. </AskToContact>
-			<ContactLink to="/login" >Contacto</ContactLink>
+			<h1 className="http__Message">404</h1>
+			<p className="error__Mess"> Ops, parece que la página que estas buscando no existe </p>
+			<Link className="backHome__Link" to="/"> Vuelve a la página de inicio </Link>
+			<p> Si se trata de un error, ponte en contacto. </p>
+			<Link className="contact__Link" to="/login" >Contacto</Link>
 		</Container>
 	);
 }
--- frontend/src/screens/NotFound/NotFound.styles.js
+++ frontend/src/screens/NotFound/NotFound.styles.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import {Link} from "react-router-dom";
 
 export const Container = styled.div`
 	margin: 0;
@@ -13,74 +12,66 @@ export const Container = styled.div`
 	flex-direction: column;
 	justify-content: center;
 	align-items: center;
-`;
-export const HttpMess = styled.h1`
-	font-size: 150px;
-	color: #76adf5;
-	text-shadow: 1px 1px 1px #192791, 2px 2px 1px #192791, 3px 3px 1px #192791, 4px 4px 1px #192791,
-		5px 5px 1px #192791, 6px 6px 1px #192791, 7px 7px 1px #192791, 8px 8px 1px #192791,
-		25px 25px 8px rgba(0, 0, 0, 0.2);
-	@media (max-width: 550px) {
-		font-size: 100px;
-		font-weight: 530;
+
+	.http__Message {
+		font-size: 150px;
+		color: #76adf5;
+		text-shadow: 1px 1px 1px #192791, 2px 2px 1px #192791, 3px 3px 1px #192791,
+			4px 4px 1px #192791, 5px 5px 1px #192791, 6px 6px 1px #192791, 7px 7px 1px #192791,
+			8px 8px 1px #192791, 25px 25px 8px rgba(0, 0, 0, 0.2);
+		@media (max-width: 550px) {
+			font-size: 100px;
+			font-weight: 530;
+		}
 	}
-`;
-export const ErrorMess = styled.p`
-	margin: 2rem 0;
-	font-size: 20px;
-	font-weight: 600;
-	color: #444;
-	@media (max-width: 550px) {
-		font-size: 19px;
-		font-weight: 530;
-		display: flex;
+	.error__Mess {
+		margin: 2rem 0;
+		font-size: 20px;
+		font-weight: 600;
+		color: #444;
+		@media (max-width: 550px) {
+			font-size: 19px;
+			font-weight: 530;
+			display: flex;
+		}
+	}
+	.backHome__Link {
+		display: inline-block;
+		color: #222;
+		text-transform: uppercase;
+		cursor: pointer;
+		font-weight: 600;
+		padding: 0.75rem 1rem 0.6rem;
+		margin-bottom: 1rem;
+		transition: all 0.2s linear;
+		box-shadow: 0 3px 8px rgba(0, 0, 0, 0.3);
+		&:hover {
+			background: #76adf5;
+			color: #fff;
+		}
+		@media (max-width: 1024px) {
+			flex-direction: column;
+		}
+	}
+
+	.contact__Link {
+		display: inline-block;
+		color: #222;
+		text-transform: uppercase;
+		cursor: pointer;
+		font-weight: 600;
+		padding: 0.75rem 1rem 0.6rem;
+		margin: 1rem 0;
+		transition: all 0.2s linear;
+		box-shadow: 0 3px 8px rgba(0, 0, 0, 0.3);
+		&:hover {
+			background: #76adf5;
+			color: #fff;
+		}
+		@media (max-width: 1024px) {
+			flex-direction: column;
+		}
 	}
 `;
 
-export const BackHomeLink = styled(Link)`
-	display: inline-block;
-	color: #222;
-	text-transform: uppercase;
-	cursor: pointer;
-	font-weight: 600;
-	padding: 0.75rem 1rem 0.6rem;
-	transition: all 0.2s linear;
-	box-shadow: 0 3px 8px rgba(0, 0, 0, 0.3);
-	&:hover {
-		background: #76adf5;
-		color: #fff;
-	}
-	@media (max-width: 1024px) {
-		flex-direction: column;
-	}
-`;
-export const AskToContact = styled.p`
-	margin: 2rem auto;
-	font-size: 20px;
-	font-weight: 600;
-	color: #444;
-	width: 450px;
-	text-align: center;
-	@media (max-width: 550px) {
-		font-size: 19px;
-		font-weight: 530;
-	}
-`;
-export const ContactLink = styled(Link)`
-	display: inline-block;
-	/* border: 2px solid #222; */
-	color: #222;
-	text-transform: uppercase;
-	cursor: pointer;
-	font-weight: 600;
-	padding: 0.75rem 1rem 0.6rem;
-	transition: all 0.2s linear;
-	box-shadow: 0 3px 8px rgba(0, 0, 0, 0.3);
-	&:hover {
-		background: #76adf5;
-		color: #fff;
-	}
-	@media (max-width: 1024px) {
-		flex-direction: column;
-	}
-`;
+
