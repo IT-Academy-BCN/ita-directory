3b26d377680195e8e81deb3d2124eb4e9b77c9b7
carme <carmecodina.p@gmail.com>
2022-01-24 12:39:22 +0100

    creo componente PageNotFound

---
+++ frontend/src/screens/PageNotFound/PageNotFound.js
@@ -0,0 +1,13 @@
+//components
+import Body from "components/layout/Body/Body";
+
+const PageNotFound = () => {
+	return (
+		<Body>
+			Ops, parece que la página que estas buscando no existe. Vuelve a la página de inicio. Si
+			se trata de un error, ponte en contacto.
+		</Body>
+	);
+};
+
+export default PageNotFound;
