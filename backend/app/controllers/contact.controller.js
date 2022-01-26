6816853077102cc615dbad9d3a379e6a7e2ee7ed
vicb-rf <61434664+vicb-rf@users.noreply.github.com>
2022-01-26 10:46:51 +0100

    430 send email (#440)
    * Se habilita el endpoit contact
    
    * creacion de controlador para endpoint contact y adicion de noemailer, se agrego los parametros como variables de entorno
    
    * se crea un archivo para el mensaje de respuesta de email
    
    * se agrego validacion para endpoint contact
    
    * actualizacion de informacion swagger
    
    * se agrego env.development
    
    * correccion contact.controller.js

---
+++ backend/app/controllers/contact.controller.js
@@ -0,0 +1,39 @@
+const nodemailer = require("nodemailer");
+const mensaje = require("../utils/messagEmail");
+
+const contactController = async (req, res) => {
+	const {name, email, text} = req.body;
+
+	const transporter = await nodemailer.createTransport({
+		host: process.env.NODEMAILER_HOST,
+		port: 587,
+		secure: false,
+		auth: {
+			user: process.env.NODEMAILER_USER,
+			pass: process.env.NODEMAILER_PASS,
+		},
+	});
+
+	const mailOptions = {
+		from: process.env.NODEMAILER_FROM,
+		to: email,
+		subject: process.env.NODEMAILER_SUBJECT,
+		text: mensaje,
+	};
+
+	await transporter.sendMail(mailOptions, (error, info) => {
+		if (error) {
+			res.status(500).json(error.message);
+		} else {
+			//console.log("email enviado.");
+			res.status(200).json({
+				id: info.messageId,
+				msg: "Email sent",
+			});
+		}
+	});
+};
+
+module.exports = {
+	contactController,
+};
