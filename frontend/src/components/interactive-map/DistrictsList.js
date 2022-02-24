2f2eba920880b7aa8617d5964a8635f941c81aec
Ezequiel <oliverm316@gmail.com>
2022-02-24 18:06:18 +0100

    #482 Map finished

--- frontend/src/components/interactive-map/DistrictsList.js
+++ frontend/src/components/interactive-map/DistrictsList.js
@@ -2,134 +2,110 @@ import React, {useState, useEffect} from "react";
 import {useContext} from "react";
 import {MapContext} from "./store/context";
 import {allDistricts} from "./data/all-districts";
-import "./interactiveMap.css";
+import styled from "styled-components";
+import {MAP_ACTIONS} from "./store/reducer";
+
+const StyledList = styled.div`
+	cursor: pointer;
+	.district {
+		font-size: 15px;
+	}
+
+	.neighborhood {
+		font-size: 12px;
+		text-indent: 1rem;
+	}
+
+	.lit-neighborhood,
+	.lit-district,
+	.district:hover {
+		background-color: #db2c7f;
+		color: #fff;
+	}
+`;
 
 const DistrictsList = () => {
 	const {state, dispatch} = useContext(MapContext);
 	const [leftColumnDistricts, setLeftColumnDistricts] = useState([]);
 	const [rightColumnDistricts, setRightColumnDistricts] = useState([]);
-	const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-
-	const handleWindowSizeChange = () => {
-		setScreenWidth(window.innerWidth);
-	};
-
-	useEffect(() => {
-		window.addEventListener("resize", handleWindowSizeChange);
-		return () => {
-			window.removeEventListener("resize", handleWindowSizeChange);
-		};
-	}, []);
 
 	useEffect(() => {
 		if (allDistricts) {
 			const left = allDistricts.filter(
 				(el) =>
-					el.district === "Ciutat_Vella" ||
-					el.district === "L_Eixample" ||
-					el.district === "Sants_Montjuic" ||
-					el.district === "Les_Corts" ||
-					el.district === "Sarria_Sant_Gervasi" ||
-					el.district === "Gràcia"
+					el.district === "Ciutat-Vella" ||
+					el.district === "L-Eixample" ||
+					el.district === "Sants-Montjuic" ||
+					el.district === "Les-Corts" ||
+					el.district === "Sarria-Sant-Gervasi" ||
+					el.district === "Gracia"
 			);
 
 			const right = allDistricts.filter(
 				(el) =>
-					el.district === "Horta_Guinardó" ||
-					el.district === "Nou_Barris" ||
-					el.district === "Sant_Andreu" ||
-					el.district === "Sant_Martí"
+					el.district === "Horta-Guinardo" ||
+					el.district === "Nou-Barris" ||
+					el.district === "Sant-Andreu" ||
+					el.district === "Sant-Marti"
 			);
 			setLeftColumnDistricts(left);
 			setRightColumnDistricts(right);
 		}
 	}, []);
 
-	const handleMobileDistrictMouseOver = (district) => {
-		const action = {
-			type: "lit-map-district",
-			payload: district,
-		};
-		dispatch(action);
-	};
-
-	const handleDistrictMouseOver = (areas) => {
-		const action = {
-			type: "lit-district",
-			payload: areas,
-		};
-		dispatch(action);
-	};
-
-	const handleAreaMouseOver = (mapId) => {
-		const action = {
-			type: "lit-area",
-			payload: mapId,
-		};
-		dispatch(action);
-	};
-
-	const renderList = (district, areas) => (
-		<ul key={district}>
-			<li
-				className="text-sm cursor-pointer"
-				onMouseOver={() => handleDistrictMouseOver(areas)}
-			>
-				{district}
-			</li>
-			<ul className="flex flex-col">
-				{areas
-					.filter((areas) => Number.isFinite(areas.nr))
-					.map(({nr, id, name}) => (
-						<li
-							key={id}
-							className={
-								id === state[id] && state.title !== "district"
-									? "is-lit text-xs"
-									: "text-xs"
-							}
-							onMouseOver={() => handleAreaMouseOver(id)}
-						>
-							{`${nr}. ${name}`}
-						</li>
-					))}
-			</ul>
-		</ul>
-	);
-
-	const renderMobileList = (district) => (
-		<li
-			key={district}
-			className={
-				state.districtId === district
-					? "is-lit text-xs cursor-pointer"
-					: "text-xs cursor-pointer"
-			}
-			onMouseOver={() => handleMobileDistrictMouseOver(district)}
-			onMouseLeave={() => handleMobileDistrictMouseOver("")}
-		>
-			{district}
-		</li>
-	);
-
-	const isMobileOrTablet = screenWidth <= 820;
-
-	if (isMobileOrTablet) {
-		return (
-			<div className="flex flex-col">
-				<ul className="ml-4">
-					{allDistricts.map(({district}) => renderMobileList(district))}
+	const renderList = (district, neighborhoods) => (
+		<StyledList key={district}>
+			<ul>
+				<li
+					className={state.districtID === district ? "district lit-district" : "district"}
+					onMouseOver={() =>
+						dispatch({
+							type: MAP_ACTIONS.LIT_DISTRICT,
+							payload: district,
+						})
+					}
+				>
+					{district.replace("-", " ")}
+				</li>
+				<ul className="flex flex-col">
+					{neighborhoods
+						.filter((neighborhoods) => Number.isFinite(neighborhoods.nr))
+						.map(({nr, id, name}) => (
+							<li
+								key={id}
+								className={
+									id === state.neighborhoodID
+										? "hidden md:block neighborhood lit-neighborhood"
+										: "hidden md:block neighborhood"
+								}
+								onMouseOver={() =>
+									dispatch({type: MAP_ACTIONS.LIT_NEIGHBORHOOD, payload: id})
+								}
+							>
+								{`${nr}. ${name}`}
+							</li>
+						))}
 				</ul>
-			</div>
-		);
-	}
+			</ul>
+		</StyledList>
+	);
+
 	return (
-		<div className="flex flex-row ml-2">
-			<div className="flex flex-col">
-				{leftColumnDistricts.map(({district, areas}) => renderList(district, areas))}
+		<div className="flex flex-row ml-2 ">
+			<div className="flex flex-col w-80 md:hidden">
+				{allDistricts.map(({district, neighborhoods}) =>
+					renderList(district, neighborhoods)
+				)}
 			</div>
-			<div className="flex flex-col">
-				{rightColumnDistricts.map(({district, areas}) => renderList(district, areas))}
+			<div className="hidden md:flex flex-col w-80">
+				{leftColumnDistricts.map(({district, neighborhoods}) =>
+					renderList(district, neighborhoods)
+				)}
+			</div>
+			<div className="hidden md:flex flex-col w-80">
+				{rightColumnDistricts.map(({district, neighborhoods}) =>
+					renderList(district, neighborhoods)
+				)}
 			</div>
 		</div>
 	);
--- frontend/src/components/interactive-map/store/reducer.js
+++ frontend/src/components/interactive-map/store/reducer.js
@@ -1,29 +1,19 @@
 export const MAP_ACTIONS = {
-	LIT_NEIGHBOURHOOD: "LIT_NEIGHBOURHOOD",
+	LIT_NEIGHBORHOOD: "LIT_NEIGHBORHOOD",
 	LIT_DISTRICT: "LIT_DISTRICT",
-	LIST_MAP_DISTRICT: "LIST_MAP_DISTRICT",
 };
 
 export const reducer = (state, action) => {
 	switch (action.type) {
-		case MAP_ACTIONS.LIT_NEIGHBOURHOOD:
+		case MAP_ACTIONS.LIT_NEIGHBORHOOD:
 			return {
-				[action.payload]: action.payload,
+				neighborhoodID: action.payload,
 			};
-		case "lit-district":
-			const districtIds = {
-				title: "district",
+		case MAP_ACTIONS.LIT_DISTRICT:
+			return {
+				districtID: action.payload,
 			};
-			for (let i = 0; i < action.payload.length; i++) {
-				districtIds[action.payload[i].id] = action.payload[i].id;
-			}
 
-			return districtIds;
-		case "lit-map-district":
-			return {
-				...state,
-				districtId: action.payload,
-			};
 		default:
 			return state;
 	}
