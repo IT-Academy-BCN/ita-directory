77bc5ae46c2a7fb055f24ec300ab03614444f457
Mariano <mariano_farace@hotmail.com>
2022-03-09 02:34:43 +0100

    Improve error handling

--- backend/app/middleware/uploadAdsCSV.js
+++ backend/app/middleware/uploadAdsCSV.js
@@ -1,4 +1,5 @@
 const multer = require("multer");
+const {apiResponse} = require("../utils/utils");
 
 //req.file will be undefined in case of .single()
 
@@ -14,18 +15,34 @@ const csvFilter = function (req, file, cb) {
 	}
 };
 
-const storage = multer.memoryStorage(); //poner a donde va
-
+const storage = multer.memoryStorage();
 const upload = multer({storage, fileFilter: csvFilter}).single("some_csv");
 
 const uploadAdCSV = (req, res, next) => {
 	upload(req, res, function (err) {
+		//Error Handling
 		if (err) {
-			// This is a good practice when you want to handle your errors differently
-
-			return;
+			return res.status(400).json(
+				apiResponse({
+					message: "Error uploading file.",
+					errors: err.message,
+				})
+			);
+		} else if (!req.file) {
+			return res.status(400).json(
+				apiResponse({
+					message: "No file provided for upload.",
+					errors: "No file provided for upload.",
+				})
+			);
+		} else if (err instanceof multer.MulterError) {
+			return res.status(500).json(
+				apiResponse({
+					message: "A Multer error occurred when uploading.",
+					errors: err.message,
+				})
+			);
 		}
-
 		// Everything went fine
 		next();
 	});
@@ -34,9 +51,8 @@ const uploadAdCSV = (req, res, next) => {
 module.exports = {uploadAdCSV};
 //---------------------------------------
 // Tenes problemas cuando subis un .csv , pero que por dentro no lo es! te da un empty buffer, controlar eso! Probablemente controlar los headers es lo que hay que hacer!!!!
-//TODO ver limits en la docu
+//TODO ver limits en la documentacion
 // ver que es esto de fileValidationError!!!!!!!!!!!
-//! uso single o multiple files?
 //TODO Resolver como vas a modelar lo del id que te llegue, con query param???
 
 // a considerar https://stackoverflow.com/questions/45805890/node-multer-memory-storage-how-to-release-memory
