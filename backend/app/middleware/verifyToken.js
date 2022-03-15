94e5c76f30331b903bca03d0aefa2c5d00b74255
Mariano <mariano_farace@hotmail.com>
2022-03-15 06:57:50 +0100

    Refactor to use fn from utils

--- backend/app/middleware/verifyToken.js
+++ backend/app/middleware/verifyToken.js
@@ -1,9 +1,10 @@
 const jwt = require("jsonwebtoken");
-const Hashids = require("hashids");
 const {apiResponse} = require("../utils/utils");
+const {
+	decodeHash
+} = require("../utils/utils");
 
 const authenticateToken = (req, res, next) => {
-	const hashids = new Hashids(process.env.HASH_ID_SECRET, 10);
 	const authHeader = req.headers["authorization"];
 	if (typeof authHeader !== "undefined") {
 		const token = authHeader && authHeader.split(" ")[1];
@@ -11,7 +12,7 @@ const authenticateToken = (req, res, next) => {
 		jwt.verify(token, process.env.JWT_SECRET, (err, authData) => {
 			if (err) return res.status(401).json(apiResponse({message: "Token has expired!"}));
 			const hashedId = authData.sub.user_id;
-			const dehashedId = hashids.decode(hashedId);
+			const dehashedId = decodeHash(hashedId);
 			const userId = dehashedId[0];
 			req.userId = userId;
 			next();
