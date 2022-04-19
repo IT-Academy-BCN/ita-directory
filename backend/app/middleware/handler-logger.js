e870791742954f2866be21557afe652bfbfcb316
xcomi <xaviercomi@gmail.com>
2022-04-19 23:21:23 +0200

    simplify version no ip address and geo ip

--- backend/app/middleware/handler-logger.js
+++ backend/app/middleware/handler-logger.js
@@ -1,43 +1,25 @@
 
-const ipily = require('ipily')
 const logger = require('../../pino-loger/logger');
-const geoip = require('fast-geoip');
 const {detect} = require('detect-browser');
-const ip = require('ip');
 
 async function handlerLogger(req, res, next) {
 
   try {
 
-    const publicIp = await ipily();
-    const ipAddress = ip.address();
     const method = await req.method;
     const url = await req.url;
-    const language = await req.headers["accept-language"];
     const browserCheck = await req.headers["user-agent"];
     const browser = detect(browserCheck);
-    const geo = await geoip.lookup(publicIp);
 
     if (browserCheck.includes('PostmanRuntime')) {
-      logger.info(`route has benn access by:
-                                            ip address: ${ipAddress}
-                                            method: ${method} 
-                                            url: ${url} 
-                                            browser: ${req.headers["user-agent"]}`
+      logger.info(`route access:
+        method: ${method} url: ${url} 
+        browser: ${req.headers["user-agent"]}`
       );
     } else {
-      logger.info(`route has been access by: 
-                                            ip address: ${ipAddress}      
-                                            ip public: ${publicIp}      
-                                            method: ${req.method}         
-                                            url: ${req.url}
-                                            browse: ${browser.name}       
-                                            version: ${browser.version}
-                                            plataform: ${browser.os}      
-                                            language: ${language}
-                                            country: ${geo.country}       
-                                            city: ${geo.city}
-                                            timezone: ${geo.timezone}`
+      logger.info(`route access:
+        method: ${req.method} url: ${req.url}
+        browse: ${browser.name} version: ${browser.version} plataform: ${browser.os}`
       );
     }
   } catch (err) {
