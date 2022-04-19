fc0173978d155cb4d393e92fd5ccce52707210b4
xcomi <xaviercomi@gmail.com>
2022-04-19 23:43:39 +0200

    pino logger final version

--- backend/pino-loger/logger.js
+++ backend/pino-loger/logger.js
@@ -1,15 +1,11 @@
 const pino = require('pino');
-//const path = require('path');
-//const url = require('url');
 const fs = require('fs');
 const PinoPretty = require('pino-pretty');
-const client = require('../app/utils/initRedis');
 
 
 const streams = [
   {stream: fs.createWriteStream(`${__dirname}/server.log`, {flags: "a+"})},
   {stream: PinoPretty({singleLine: false, colorize: true, translateTime: "SYS:dd-mm-yyyy HH:MM:ss"}, process.stdout)},
-  //{stream: client.rpush("LOG:")}
 ]
 
 const logger = pino(
