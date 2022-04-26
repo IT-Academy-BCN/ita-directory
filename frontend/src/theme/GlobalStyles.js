a5e3a14cf6e3861adff14168b7d25d1dae1bf20f
Kevin Mamaqi <kevinmamaqi@gmail.com>
2022-04-26 10:28:38 +0200

    add container

--- frontend/src/theme/GlobalStyles.js
+++ frontend/src/theme/GlobalStyles.js
@@ -1,30 +1,32 @@
-import styled from "styled-components";
-import {Device} from "../theme/mediaQueries";
+import styled from 'styled-components'
+import { device } from './mediaQueries'
 
-export const Container = styled.div`
-	width: 100%;
-	max-width: 85%;
-	margin: 0 auto;
-	display: flex;
-	justify-content: center;
-	flex-direction: ${(props) => (props.column ? "column" : "row")};
+const Container = styled.div`
+  width: 100%;
+  max-width: 85%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  flex-direction: ${(props) => (props.column ? 'column' : 'row')};
 
-	@media ${Device.Tablet} {
-		max-width: 70%;
-	}
+  @media ${device.Tablet} {
+    max-width: 70%;
+  }
 
-	* {
-		transition: all 0.35s ease-in-out;
+  * {
+    transition: all 0.35s ease-in-out;
 
-		*:hover {
-			transition: all 0.15s ease-in-out;
-		}
-	}
+    *:hover {
+      transition: all 0.15s ease-in-out;
+    }
+  }
 
-	& > input:hover,
-	& > textarea:hover,
-	& > button:hover {
-		backdrop-filter: brightness(0.8);
-		border: 1px dotted;
-	}
-`;
+  & > input:hover,
+  & > textarea:hover,
+  & > button:hover {
+    backdrop-filter: brightness(0.8);
+    border: 1px dotted;
+  }
+`
+
+export default Container
--- frontend/src/theme/index.js
+++ frontend/src/theme/index.js
@@ -1,5 +1,6 @@
 import dimensions from './dimensions'
 import font from './font'
 import colors from './colors'
+import { responsiveSizes, device } from './mediaQueries'
 
-export { colors, dimensions, font }
+export { colors, dimensions, font, device, responsiveSizes }
