39d974669b2dd7a7d6ff23dca8fd44e9b5104d97
Kevin Mamaqi <kevinmamaqi@gmail.com>
2022-04-26 10:35:44 +0200

    add eslint + lint-staged in frontend

--- frontend/src/components/layout/Header/Header.jsx
+++ frontend/src/components/layout/Header/Header.jsx
@@ -1,91 +1,85 @@
-import React, {useState} from "react";
-import {Link} from "react-router-dom";
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import PropType from 'prop-types'
+import { HeaderStyled, StyledSubHeader } from './Header.styles'
+import { Container } from '../../../theme'
+import logo from '../../../assets/logos/logo.png'
 
-// Styles
-import {HeaderStyled, StyledSubHeader} from "./Header.styles";
-import {Container} from "../../../theme/GlobalStyles";
-import logo from "../../../assets/logos/logo.png";
+const profilePicture = 'https://randomuser.me/api/portraits/men/21.jpg'
 
-const profilePicture =
-	// "https://sites.google.com/site/ellibrorojoesdla/_/rsrc/1349808591712/personajes/ganda/Gandalf.jpg";
-	"https://randomuser.me/api/portraits/men/21.jpg";
+function Header({
+  title,
+  logoColor,
+  headerColor,
+  fontColor,
+  justifyTitle,
+  isLoggedIn = true,
+  isTitleVisible = true,
+}) {
+  const [dropdownVisible, setDropdownVisible] = useState(false)
 
-const Header = ({
-	isLoggedIn = true,
-	title,
-	logoColor,
-	headerColor,
-	fontColor,
-	justifyTitle,
-	isTitleVisible = true,
-}) => {
-	const [dropdownVisible, setDropdownVisible] = useState(false);
+  const handleClick = () => {
+    setDropdownVisible(!dropdownVisible)
+  }
 
-	const handleClick = () => {
-		setDropdownVisible(!dropdownVisible);
-	};
+  const justifyTitleB = justifyTitle === 'center'
 
-	const justifyTitleB = justifyTitle === "center" ? true : false;
+  return (
+    <HeaderStyled justifyTitle={justifyTitleB} logoColor={logoColor}>
+      <Container>
+        <div className="header__container">
+          <Link className="header__logo-group" to="/ads">
+            <img src={logo} alt="ITAcademy Logo" className="header__logo" />
+            <span className="header__logo-text">_directory</span>
+          </Link>
+          {isLoggedIn ? (
+            <div className="header__profile">
+              <button type="button" className="header__profile-button" onClick={handleClick}>
+                <img className="header__profile-image" src={profilePicture} alt="Profile" />
+                <span className="header__profile-title">Mi perfil</span>
+              </button>
+              {dropdownVisible ? (
+                <div className="header__profile-dropdown">
+                  <ul>
+                    <li>
+                      <Link to="/profile">Editar perfil</Link>
+                    </li>
+                    <li>
+                      <Link to="/my-bills">Mis facturas</Link>
+                    </li>
+                    <li>
+                      <Link to="/user-ads">Mis Anuncios</Link>
+                    </li>
+                    <li>
+                      <Link to="/new-ad">Publicar Anuncio</Link>
+                    </li>
+                    <li>
+                      <Link to="/">Cerrar sesión</Link>
+                    </li>
+                  </ul>
+                </div>
+              ) : null}
+            </div>
+          ) : null}
+        </div>
+      </Container>
+      <StyledSubHeader headerColor={headerColor} fontColor={fontColor} justifyTitle={justifyTitleB}>
+        <Container>
+          <h1>{title}</h1>
+        </Container>
+      </StyledSubHeader>
+    </HeaderStyled>
+  )
+}
 
+Header.propTypes = {
+  title: PropType.string.isRequired,
+  logoColor: PropType.string,
+  headerColor: PropType.string,
+  fontColor: PropType.string,
+  justifyTitle: PropType.string,
+  isLoggedIn: PropType.bool,
+  isTitleVisible: PropType.bool,
+}
 
-	//
-	return (
-		<HeaderStyled justifyTitle={justifyTitleB} logoColor={logoColor}>
-			<Container>
-				<div className="header__container">
-					<Link className="header__logo-group" to="/ads">
-						<img src={logo} alt="ITAcademy Logo" className="header__logo" />
-						<span className="header__logo-text">_directory</span>
-					</Link>
-					{isLoggedIn ? (
-						<div className="header__profile">
-							<button className="header__profile-button" onClick={handleClick}>
-								<img
-									className="header__profile-image"
-									src={profilePicture}
-									alt="Profile"
-								/>
-								<span className="header__profile-title">Mi perfil</span>
-							</button>
-							{dropdownVisible ? (
-								<div className="header__profile-dropdown">
-									<ul>
-										<li>
-											<Link to="/profile">Editar perfil</Link>
-										</li>
-										<li>
-											<Link to="/my-bills">Mis facturas</Link>
-										</li>
-										<li>
-											<Link to="/user-ads">Mis Anuncios</Link>
-										</li>
-										<li>
-											<Link to="/new-ad">Publicar Anuncio</Link>
-										</li>
-										<li>
-											<Link to="/">Cerrar sesión</Link>
-										</li>
-									</ul>
-								</div>
-							) : null}
-						</div>
-					) : null}
-				</div>
-			</Container>
-			{/* {isTitleVisible && ( */}
-			<StyledSubHeader
-				headerColor={headerColor}
-				fontColor={fontColor}
-				justifyTitle={justifyTitleB}
-			>
-				<Container>
-					<h1>{title}</h1>
-				</Container>
-			</StyledSubHeader>
-			{/* )} */}
-		</HeaderStyled>
-	);
-
-};
-
-export default Header;
+export default Header
--- frontend/src/theme/index.js
+++ frontend/src/theme/index.js
@@ -2,5 +2,6 @@ import dimensions from './dimensions'
 import font from './font'
 import colors from './colors'
 import { responsiveSizes, device } from './mediaQueries'
+import { Container, Grid } from './wrappers'
 
-export { colors, dimensions, font, device, responsiveSizes }
+export { colors, dimensions, font, device, responsiveSizes, Container, Grid }
