3ce6aa891549b5769698909aa9a5456732c18ae4
Kevin Mamaqi <kevinmamaqi@gmail.com>
2022-04-27 10:53:55 +0200

    fix server.js environment variable

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,39 @@
-const path = require("path");
-let envFile = process.env.NODE === "development" ? ".env.development" : ".env";
-require("dotenv").config({path: path.join(__dirname, `/./../${envFile}`)});
-const app = require("./app/app.js");
-const socketio = require("socket.io");
-const http = require("http");
-const handlerError = require("./app/middleware/handlerErrors");
-const handlerLogger = require("./app/middleware/handlerLogger");
-const client = require("./app/utils/initRedis");
-const logger = require("./logger.js");
+const path = require('path')
+
+require('dotenv').config({ path: path.join(__dirname, `.env`) })
+const socketio = require('socket.io')
+const http = require('http')
+const app = require('./app/app')
+const handlerError = require('./app/middleware/handlerErrors')
+const handlerLogger = require('./app/middleware/handlerLogger')
+const client = require('./app/utils/initRedis')
+const logger = require('./logger')
 
 // Create IO server
-const server = http.createServer(app);
+const server = http.createServer(app)
 const io = socketio(server, {
-	cors: {
-		origin: process.env.CORS,
-		methods: ["GET", "POST"],
-	},
-});
-require("./app/config/sockets")(io);
+  cors: {
+    origin: process.env.CORS,
+    methods: ['GET', 'POST'],
+  },
+})
+require('./app/config/sockets')(io)
 
-//Back-end logger
-app.use(handlerLogger);
+// Back-end logger
+app.use(handlerLogger)
 
-//No route found handler
-app.use(handlerError.routeFoundHandler);
+// No route found handler
+app.use(handlerError.routeFoundHandler)
 
-//Error handler
-app.use(handlerError.errorHandler);
+// Error handler
+app.use(handlerError.errorHandler)
 
-//Redis connect
-(async () => {
-	await client.connect();
-})();
+// Redis connect
+;(async () => {
+  await client.connect()
+})()
 
 server.listen(process.env.PORT, () => {
-	logger.info(`Server is running on port and working ${process.env.PORT}.`);
-	logger.info(`Visit: http://localhost:${process.env.PORT}/`);
-});
+  logger.info(`Server is running on port and working ${process.env.PORT}.`)
+  logger.info(`Visit: http://localhost:${process.env.PORT}/`)
+})
