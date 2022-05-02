f14cf22632a1ded78edeade033f448d0b44ce4a2
monicab1989 <monika_291@hotmail.com>
2022-05-02 12:21:04 +0200

    Change Button Styled

--- frontend/src/pages/UserFlow/Registration/Registration.styles.js
+++ frontend/src/pages/UserFlow/Registration/Registration.styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import Colors from '../../../theme/Colors'
+import Colors from '../../../theme/colors'
 
 export const Container = styled.div`
   background: #ffffff 0% 0% no-repeat padding-box;
