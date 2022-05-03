4969d99d81231a1135497740d89ae89cbab40bd7
Kevin Mamaqi <kevinmamaqi@gmail.com>
2022-05-03 10:39:56 +0200

    update test

---
+++ e2e/tests/login.spec.js
@@ -0,0 +1,45 @@
+/* eslint-disable import/newline-after-import */
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-restricted-syntax */
+require('dotenv').config()
+const { test, expect } = require('@playwright/test')
+const { chromium } = require('playwright')
+
+const TOKEN_KEYS = ['refresh', 'refreshToken']
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+test.describe('login', () => {
+  let browser, page, context
+  test.beforeAll(async () => {
+    browser = await chromium.launch()
+    context = await browser.newContext()
+    page = await context.newPage()
+    await page.goto(`${process.env.VITE_REACT_APP_API_URL}/login`)
+  })
+
+  test.afterAll(async ({ browser }) => {
+    browser.close()
+  })
+
+  test('login', async () => {
+    const title = page.locator('h1')
+    expect(title).toHaveText('Acceso')
+  })
+
+  test('user can login and sets tokens in localstorage', async () => {
+    await page.locator('[placeholder="Introduce tu email"]').click()
+    await page.locator('[placeholder="Introduce tu email"]').fill(`${process.env.PLAYWRIGHT_MAIL}`)
+    await page.locator('[placeholder="Introduce tu contraseña"]').click()
+    await page
+      .locator('[placeholder="Introduce tu contraseña"]')
+      .fill(`${process.env.PLAYWRIGHT_PASSWORD}`)
+
+    await page.locator('button:has-text("Acceder")').click()
+    await delay(2000)
+
+    let storage = await context.storageState()
+    storage = storage.origins[0].localStorage.map((s) => s.name)
+    expect(storage.every((s) => TOKEN_KEYS.includes(s)))
+  })
+})
