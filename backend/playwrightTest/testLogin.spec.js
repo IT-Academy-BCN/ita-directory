9073c0f5c90056710ffb6004ecdd988f2c2ae1cd
xcomi <xaviercomi@gmail.com>
2022-05-03 06:42:54 +0200

    state.json not working

--- backend/playwrightTest/testLogin.spec.js
+++ backend/playwrightTest/testLogin.spec.js
@@ -22,30 +22,44 @@ const playwright = require('playwright')
 
     await page.locator('[placeholder="Introduce tu email"]').click()
 
-    await page.locator('[placeholder="Introduce tu email"]').fill('test@test.test')
+    await page.locator('[placeholder="Introduce tu email"]').fill('process.env.PLAYWRIGHT_EMAIL')
 
     await page.locator('[placeholder="Introduce tu contraseña"]').click()
 
-    await page.locator('[placeholder="Introduce tu contraseña"]').fill('Test-test')
+    await page
+      .locator('[placeholder="Introduce tu contraseña"]')
+      .fill('process.env.PLAYWRIGHT_PASSWORD')
 
     await page.locator('button:has-text("Acceder")').click()
+
+    await context.storageState({
+      path: 'playwrightTest/state/state.json',
+    })
 
     await page.screenshot({
       path: `playwrightTest/screenshots/image_${browserType.name()}.png`,
       fullPage: true,
     })
+
     await context.close()
     await browser.close()
   }
 })()
 
-test('check the web site designer', async ({ page }) => {
-  const browser = await playwright.chromium.launch()
-  await page.goto('http://localhost:3000')
-  const name = await page.$eval('//*[@id="root"]/div[2]/div[2]/footer/div[1]/div[2]', (designer) =>
-    designer.textContent.trim()
-  )
-  expect(name).toEqual('Diseñado por Kevin Mamaquipara ITAcademy de Barcelona Activa')
+test.describe('Test it-academy-directory login', () => {
+  test('Should not ask for credentials', async ({ page }) => {
+    await page.goto('http://localhost:3000/login')
+    const url = page.url()
+    expect(url).toMatch('http://localhost:3000/login')
+  })
 
-  await browser.close()
+  test('check the web site designer', async ({ page }) => {
+    await playwright.chromium.launch()
+    await page.goto('http://localhost:3000/login')
+    const name = await page.$eval(
+      '//*[@id="root"]/div[2]/div[2]/footer/div[1]/div[2]',
+      (designer) => designer.textContent.trim()
+    )
+    expect(name).toEqual('Diseñado por Kevin Mamaquipara ITAcademy de Barcelona Activa')
+  })
 })
