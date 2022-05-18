fc6a84728f3b102241787b56a708fdf10a250274
Kevin <kevinmamaqi@users.noreply.github.com>
2022-05-18 09:27:47 +0200

    623 fix login errors (#624)
    * fixes
    
    * fix double login

--- frontend/src/pages/UserFlow/Login/Login.jsx
+++ frontend/src/pages/UserFlow/Login/Login.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { useDispatch } from 'react-redux'
 import Body from '../../../components/layout/Body/Body'
-import AsyncButton from '../../../components/atoms/Button'
+import { Button } from '../../../components/atoms'
 import { Container, Form, RedirectStyled } from '../UserFlow.styles'
 import { InputGroup } from '../../../components/molecules'
 import loginSchema from '../../../validation/loginSchema'
@@ -17,9 +16,6 @@ import Urls from '../../../utils/urls'
 function Login() {
   const dispatch = useDispatch()
 
-  const [animated, setAnimated] = useState(false)
-  const [disabled, setIsDisabled] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
   const {
     register,
     handleSubmit,
@@ -27,6 +23,7 @@ function Login() {
   } = useForm({
     resolver: yupResolver(loginSchema),
   })
+
   const loginUser = async (user) => {
     try {
       const response = await axios.post(Urls.login, user)
@@ -69,21 +66,9 @@ function Login() {
 
   const submitForm = (data) => {
     const { email, password } = data
-    setIsDisabled(true)
-    setIsLoading(true)
-    setAnimated(true)
-    setTimeout(() => {
-      setIsDisabled(false)
-      setIsLoading(false)
-      setAnimated(false)
-      loginUser({
-        email,
-        password,
-      })
-      setTimeout(() => {
-        setIsDisabled(false)
-        setIsLoading(false)
-      }, 2000)
+    loginUser({
+      email,
+      password,
     })
   }
 
@@ -98,7 +83,6 @@ function Login() {
             placeholder="Introduce tu email"
             id="emailName"
             name="email"
-            disabled={disabled}
             className="w-full"
             error={errors.email?.message}
             register={register('email')}
@@ -110,20 +94,16 @@ function Login() {
             placeholder="Introduce tu contraseña"
             id="passName"
             name="password"
-            disabled={disabled}
             className="w-full mt-2"
             error={errors.password?.message}
             register={register('password')}
           />
-          <AsyncButton
+          <Button
             text="Acceder"
             loadingText="Accediendo"
             iconPosition="left"
             type="submit"
             className="blue-gradient w-full my-8"
-            isLoading={isLoading}
-            animated={animated}
-            onClick={submitForm}
           />
           <div className="w-full">
             <RedirectStyled>
