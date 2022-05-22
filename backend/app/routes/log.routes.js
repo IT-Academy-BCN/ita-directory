b51eea6c858f3da87bb057e94fb0522cdd34568d
tonikny <dalecanya@gmail.com>
2022-05-22 20:23:47 +0200

    fix Swagger documentation

--- backend/app/routes/log.routes.js
+++ backend/app/routes/log.routes.js
@@ -2,11 +2,17 @@ const router = require('express').Router()
 const logController = require('../controllers/log.controller')
 
 /**
+ * Log data
+ * @typedef {object} log
+ * @property {string} msg.required - The message to log
+ * @property {string} level - The level of serverity ('trace', 'debug', 'info', 'warn', 'error', 'fatal')
+ */
+
+/**
  * POST /log - Allow Frontend to save logs on server
  * @summary Create new log
  * @tags Log
- * @param {string} request.body.msg.required - The message to log
- * @param {string} request.body.level - The level of serverity ('trace', 'debug', 'info', 'warn', 'error', 'fatal')
+ * @param {log} request.body.required
  * @return {object} 200 - Success response - application/json
  * @return {object} 400 - Bad request response - application/json
  * @return {object} 500 - Internal Server Error response - application/json
