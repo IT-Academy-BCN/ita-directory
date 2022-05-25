cf8d98473759874147d2238aea52056f01974841
monicab1989 <monika_291@hotmail.com>
2022-05-25 16:42:04 +0200

    Order the List component

--- frontend/src/components/layout/Header/Header.jsx
+++ frontend/src/components/layout/Header/Header.jsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types'
 import { HeaderStyled, StyledSubHeader } from './Header.styles'
 import { Container } from '../../../theme'
 import logo from '../../../assets/logos/logo.png'
-import { Text, Dropdown } from '../../atoms'
+import { Text, Dropdown, Li, Ul } from '../../atoms'
 
 const profilePicture = 'https://randomuser.me/api/portraits/men/22.jpg'
 
@@ -47,15 +47,15 @@ function Header({ title, logoColor, headerColor, fontColor, justifyTitle, isTitl
             {dropdownVisible ? (
               <Dropdown setDropdownVisible={setDropdownVisible} parentId="dropdownButton">
                 {isLoggedIn ? (
-                  <ul>
+                  <Ul>
                     {children?.map(({ path, text }) => (
-                      <li key={text}>
+                      <Li key={text}>
                         <Link key={text} to={path}>
                           {text}
                         </Link>
-                      </li>
+                      </Li>
                     ))}
-                  </ul>
+                  </Ul>
                 ) : null}
               </Dropdown>
             ) : null}
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,25 @@
+import styled from 'styled-components'
 import Body from '../../components/layout/Body/Body'
-import { HomeContainer } from './Home.styles'
 import { Banner } from '../../components/atoms'
 import { MapOfDistricts } from '../../components/organisms'
+import { Container, device } from '../../theme'
+
+// eslint-disable-next-line import/prefer-default-export
+const HomeContainer = styled(Container)`
+  flex-direction: column;
+  min-height: calc(100vh - 360px);
+
+  @media ${device.Tablet} {
+    min-height: calc(100vh - 300px);
+  }
+
+  a {
+    &:hover {
+      text-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+      color: blue;
+    }
+  }
+`
 
 // const pages = [
 //   { title: 'Ad', route: '/ad' },
