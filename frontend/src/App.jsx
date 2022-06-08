553f8c3ceb09dcd2b81fe8849626b31c9ea86e64
Monica <91555704+monicab1989@users.noreply.github.com>
2022-06-08 10:30:13 +0200

    629 create main menu (#646)
    * Create main menu
    
    * Fix path
    
    * Fix Link
    
    * Create Menu
    
    * Styled Menu
    
    * Fix menu
    
    * Fix paths and Menu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import Home from './pages/Home/Home'
 import Ad from './pages/Ad/Ad'
-import CreateNewAd from './pages/CreateNewAd/CreateNewAd'
-import Login from './pages/UserFlow/Login/Login'
-import Registration from './pages/UserFlow/Registration/Registration'
-import RecoverPassword from './pages/UserFlow/RecoverPassword/RecoverPassword'
-import Profile from './pages/UserFlow/Profile/Profile'
-import AdList from './pages/AdList/AdList'
-import ListaUsuariosAdmins from './pages/ListaUsuariosAdmins/ListaUsuariosAdmins'
-import Dashboard from './pages/Dashboard/Dashboard'
-import MyBills from './pages/MyBills/MyBills'
-import Bill from './pages/MyBills/Bill'
-import UserAds from './pages/UserAds/UserAds'
 import ProtectedRoute from './components/organisms/ProtectedRoute'
+import {
+  Students,
+  Business,
+  Home,
+  Login,
+  Registration,
+  CreateNewAd,
+  RecoverPassword,
+  AdList,
+  UserAds,
+  Profile,
+  Bill,
+  MyBills,
+  ListaUsuariosAdmins,
+  Dashboard,
+} from './pages'
 
 function App() {
   return (
@@ -23,6 +27,8 @@ function App() {
       <Route exact path="/new-ad" component={CreateNewAd} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/register" component={Registration} />
+      <Route exact path="/students" component={Students} />
+      <Route exact path="/business" component={Business} />
       <ProtectedRoute exact path="/profile" component={Profile} />
       <ProtectedRoute exact path="/recover-password" component={RecoverPassword} />
       <ProtectedRoute exact path="/ads" component={AdList} />
--- frontend/src/components/layout/Header/Header.jsx
+++ frontend/src/components/layout/Header/Header.jsx
@@ -2,26 +2,28 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import PropTypes from 'prop-types'
-import { HeaderStyled, StyledSubHeader } from './Header.styles'
+import { ContainerMenu, HeaderStyled, StyledHeaderHome, StyledSubHeader } from './Header.styles'
 import { Container } from '../../../theme'
 import logo from '../../../assets/logos/logo.png'
-import { Text, Dropdown, Li, Ul } from '../../atoms'
+import { Text, Dropdown, Li, Ul, Title } from '../../atoms'
+import { paths } from '../../../utils'
 
 const profilePicture = 'https://randomuser.me/api/portraits/men/22.jpg'
 
-function Header({ title, logoColor, headerColor, fontColor, justifyTitle, isTitleVisible = true }) {
+function Header({ title, logoColor, menu }) {
   const isLoggedIn = useSelector((s) => s.user.isLoggedIn)
 
   const children = [
-    { path: '/profile', text: 'Editar perfil' },
-    { path: '/my-bills', text: 'Mis facturas' },
-    { path: '/user-ads', text: 'Mis Anuncios' },
-    { path: '/new-ad', text: 'Publicar Anuncio' },
-    { path: '/ads', text: 'Anuncios' },
-    { path: '/', text: 'Cerrar sesión' },
+    { path: paths.profile, text: 'Editar perfil' },
+    { path: paths.bills, text: 'Mis facturas' },
+    { path: paths.userAds, text: 'Mis Anuncios' },
+    { path: paths.newAd, text: 'Publicar Anuncio' },
+    { path: paths.ads, text: 'Anuncios' },
+    { path: paths.home, text: 'Cerrar sesión' },
   ]
 
   const [dropdownVisible, setDropdownVisible] = useState(false)
+
   const handleClick = () => {
     setDropdownVisible(!dropdownVisible)
   }
@@ -62,22 +64,28 @@ function Header({ title, logoColor, headerColor, fontColor, justifyTitle, isTitl
           </div>
         </div>
       </Container>
-      <StyledSubHeader headerColor={headerColor} fontColor={fontColor}>
-        <Container>
-          <h1>{title}</h1>
-        </Container>
-      </StyledSubHeader>
+      {menu && (
+        <StyledHeaderHome>
+          <Container>
+            <ContainerMenu>{menu}</ContainerMenu>
+          </Container>
+        </StyledHeaderHome>
+      )}
+      {title && (
+        <StyledSubHeader>
+          <Container>
+            <Title text={title} order={1} />
+          </Container>
+        </StyledSubHeader>
+      )}
     </HeaderStyled>
   )
 }
 
 Header.propTypes = {
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string,
   logoColor: PropTypes.string,
-  headerColor: PropTypes.string,
-  fontColor: PropTypes.string,
-  justifyTitle: PropTypes.string,
-  isTitleVisible: PropTypes.bool,
+  menu: PropTypes.object,
 }
 
 export default Header
