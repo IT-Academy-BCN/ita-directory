bc5819fc7f8d4dd1f51ee1e33c08ba8f2eef1816
tonikny <dalecanya@gmail.com>
2022-06-14 13:54:45 +0200

    change seeds to PascalCase / camelCase

--- backend/prisma/seedFiles/ad_status.js
+++ backend/prisma/seedFiles/ad_status.js
@@ -1,14 +1,14 @@
 // options for ads id_status related to publication status
 
 module.exports = [
-	{
-		name: "un_publish",
-		publish: 0,
-		un_publish: 1
-	},
-	{
-		name: "publish",
-		publish: 1,
-		un_publish: 0
-	}
-];
+  {
+    name: 'un_publish',
+    publish: 0,
+    unPublish: 1,
+  },
+  {
+    name: 'publish',
+    publish: 1,
+    unPublish: 0,
+  },
+]
