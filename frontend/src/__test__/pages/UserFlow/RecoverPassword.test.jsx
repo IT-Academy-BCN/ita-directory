62437f9f64ac4bc6f9eac1190af2cd3c6dfad942
Kevin Mamaqi <kevinmamaqi@gmail.com>
2022-09-28 11:56:08 +0200

    update

--- frontend/src/__test__/pages/UserFlow/RecoverPassword.test.jsx
+++ frontend/src/__test__/pages/UserFlow/RecoverPassword.test.jsx
@@ -1,64 +1,73 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, vi } from 'vitest'
+import userEvent from '@testing-library/user-event'
+import axios from 'axios'
 import configureStore from 'redux-mock-store'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { RecoverPassword } from '../../../pages'
-import userEvent from '@testing-library/user-event';
 
-import {jest} from '@jest/globals'
-import axios from 'axios';
-jest.mock('axios');
+vi.mock('axios')
 
+const initialState = {
+  user: {
+    isLoggedIn: false,
+    value: undefined,
+  },
+  notification: {
+    notifications: {},
+  },
+}
+const mockStore = configureStore()
+const store = mockStore(initialState)
 
 describe('<RecoverPassword>', () => {
-
-  beforeEach(() => {
-
-    const initialState = {
-      user: {
-        isLoggedIn: false,
-        value: undefined,
-      },
-      notification: {
-        notifications: {},
-      },
-    }
-    const mockStore = configureStore()
-    let store=mockStore(initialState)
-
+  it('shows an input with an "Email" placeholder', () => {
     render(
       <Provider store={store}>
-      <BrowserRouter>
-        <RecoverPassword />
-      </BrowserRouter>
-    </Provider>
-    );
-})
-
-  it('shows an input with an "Email" placeholder', () => {
+        <BrowserRouter>
+          <RecoverPassword />
+        </BrowserRouter>
+      </Provider>
+    )
     const input = screen.getByPlaceholderText(/email/i)
     expect(input).toBeInTheDocument()
   })
 
   it('pass valid email to test email input field', async () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <RecoverPassword />
+        </BrowserRouter>
+      </Provider>
+    )
+
     const input = screen.getByPlaceholderText(/email/i)
     const emailFormat = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-    await userEvent.type(input, "test@test.com");
-    expect(input.value).toMatch(emailFormat);
-  });
-
-
- it('should submit form',async () => {
-  // given
-  const submitBtn =  screen.getByRole('button', {name: /enviar/i})
-  const email = { email:"test@test.test"};
-  axios.post.mockResolvedValueOnce(email);
-  // when
- await userEvent.click(submitBtn);
-  // then
-  expect(axios.post).toHaveBeenCalledWith(`${import.meta.env.VITE_API_URL}/recover-password`, email);
+    await userEvent.type(input, 'test@test.com')
+    expect(input.value).toMatch(emailFormat)
   })
 
+  it('should submit form', async () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <RecoverPassword />
+        </BrowserRouter>
+      </Provider>
+    )
+    // given
+    const submitBtn = screen.getByRole('button', { name: /enviar/i })
+    const email = { email: 'test@test.test' }
+    axios.post.mockResolvedValueOnce(email)
+    // when
+    await userEvent.click(submitBtn)
+    // then
+    expect(axios.post).toHaveBeenCalledWith(
+      `${import.meta.env.VITE_API_URL}/recover-password`,
+      email
+    )
+  })
 })
