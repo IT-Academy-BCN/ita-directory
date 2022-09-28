5e793547b335a8d1ee44cd7c714e4c77f95c5d5b
RogerRochoRibas <rogerrochoribas@gmail.com>
2022-09-28 12:10:37 +0200

    Added InputGroup test

---
+++ frontend/src/__test__/molecules/InputGroup.test.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable testing-library/no-debugging-utils */
+/* eslint-disable jest/valid-describe-callback */
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import { describe, it, expect } from 'vitest'
+import InputGroup from '../../components/molecules/InputGroup'
+
+describe('InputGroup', async () => {
+  it('should render the InputGroup', () => {
+    render(
+      <InputGroup
+        label="Email"
+        hiddenLabel
+        type="email"
+        placeholder="Introduce tu email"
+        id="emailName"
+        name="email"
+        className="w-full"
+        error="error text"
+        register="email"
+      />
+    )
+    const inputGroup = screen.getByLabelText('Email')
+
+    expect(inputGroup).toBeInTheDocument()
+  })
+})
