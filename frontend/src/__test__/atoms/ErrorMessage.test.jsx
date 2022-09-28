4ecb70a769ea4099286d2a4b0665925e3719df23
RogerRochoRibas <rogerrochoribas@gmail.com>
2022-09-28 12:06:18 +0200

    Added test for Error Message and InputGroup

---
+++ frontend/src/__test__/atoms/ErrorMessage.test.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable testing-library/no-debugging-utils */
+/* eslint-disable jest/valid-describe-callback */
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import { describe, it, expect } from 'vitest'
+import ErrorMessage from '../../components/atoms/Forms/ErrorMessage'
+
+describe('ErrorMessage', async () => {
+  it('should render the given ErrorMessage', () => {
+    render(<ErrorMessage text="some error" />)
+    const errorMessage = screen.getByText('some error')
+
+    expect(errorMessage).toBeInTheDocument()
+  })
+
+  it('error text should should be Red', () => {
+    render(<ErrorMessage text="some error" />)
+    const errorMessage = screen.getByText('some error')
+
+    expect(errorMessage).toHaveStyle('color:#DC2626')
+  })
+})
