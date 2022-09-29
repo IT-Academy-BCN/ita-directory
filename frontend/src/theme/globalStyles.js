62a1627597aba8ba846d997f2ae3661722a47d08
JORDI.NODEJS <jordi.nodejs@gmail.com>
2022-09-29 13:04:03 +0200

    padding

--- frontend/src/theme/globalStyles.js
+++ frontend/src/theme/globalStyles.js
@@ -2,8 +2,8 @@ import { createGlobalStyle } from 'styled-components'
 import font from './font'
 
 const GlobalStyle = createGlobalStyle`
-
   body {
+    box-sizing: border-box;
     margin: 0;
     padding: 0;
     background: white;
