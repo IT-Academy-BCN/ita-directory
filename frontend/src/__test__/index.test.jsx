a614e04441995e797b103f6574c02d786945ce26
JORDI.NODEJS <jordi.nodejs@gmail.com>
2022-10-04 19:41:14 +0200

    test refactor

--- frontend/src/__test__/index.test.jsx
+++ frontend/src/__test__/index.test.jsx
@@ -2,51 +2,46 @@
 import '@testing-library/jest-dom'
 import { describe, it, expect } from 'vitest'
 
-// const events = require('events')
 import events from 'node:events'
-// const fs = require('fs')
 import fs from 'node:fs'
-// const readline = require('readline')
 import readline from 'node:readline'
 
 // todo: lectura del archivo con node
 // C:\Workspace\IT ACADEMY\Projecte IT\ita-directory\frontend\src\index.jsx
 
 // const filePath = 'C:\\Workspace\\IT ACADEMY\\Projecte IT\\ita-directory\\frontend\\src\\index.jsx'
-const filePath = `${__dirname.split('__')[0]}index.jsx`
+const filePathname = `${__dirname.split('__')[0]}index.jsx`
 
-console.log('filePath -->', filePath)
+const readFile = async (filePath) => {
+  console.log('filePath -->', filePath)
+  let checkedout = false
 
-let lineImportNomalizeCss = 0
+  try {
+    let lineNumber = 0
+    const rl = readline.createInterface({
+      input: fs.createReadStream(filePath),
+    })
 
-try {
-  let lineNumber = 0
-  const rl = readline.createInterface({
-    input: fs.createReadStream(filePath),
-    crlfDelay: Infinity,
-  })
+    rl.on('line', async (line) => {
+      lineNumber += 1
+      console.log(`Line from file ${lineNumber}: ${line}`)
 
-  rl.on('line', (line) => {
-    lineNumber += 1
-    console.log(`Line from file ${lineNumber}: ${line}`)
-    if (lineNumber === 6) lineImportNomalizeCss = line
-  })
+      if (line === `import './modern-normalize.css'`) {
+        checkedout = true
+      }
+    })
 
-  await events.once(rl, 'close')
-
-  console.log('Reading file line by line with readline done.')
-  const used = process.memoryUsage().heapUsed / 1024 / 1024
-  console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`)
-} catch (err) {
-  console.error(err)
+    await events.once(rl, 'close')
+  } catch (err) {
+    console.error(err)
+    return false
+  }
+  return checkedout
 }
-
-console.log('directorio', __filename)
-
 // test
 
 describe('testing normalize', () => {
-  it('log', async () => {
-    expect(lineImportNomalizeCss).toBe(`import './modern-normalize.css'`)
+  it('should be true if css found out', async () => {
+    expect(await readFile(filePathname)).toBe(true)
   })
 })
