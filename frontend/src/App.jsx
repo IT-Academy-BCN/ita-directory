5c9944191f1a7e2d7c39b228bc79a3cf9ed66a9d
JORDI.NODEJS <jordi.nodejs@gmail.com>
2022-10-05 10:19:55 +0200

    redo app an index to inicial state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './store/store'
-import GlobalStyle from './theme/globalStyles'
+import { Route, Switch } from 'react-router-dom'
+
 import Ad from './pages/Ad/Ad'
 import ProtectedRoute from './components/organisms/ProtectedRoute'
 import {
@@ -25,28 +23,23 @@ import { paths } from './utils'
 
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <GlobalStyle />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/ad/:id" component={Ad} />
-          <Route exact path="/new-ad" component={CreateNewAd} />
-          <Route exact path={paths.login} component={Login} />
-          <Route exact path={paths.register} component={Registration} />
-          <Route exact path={paths.recoverPassword} component={RecoverPassword} />
-          <Route exact path="/students" component={Students} />
-          <Route exact path="/business" component={Business} />
-          <ProtectedRoute exact path={paths.profile} component={Profile} />
-          <ProtectedRoute exact path="/ads" component={AdList} />
-          <ProtectedRoute exact path={paths.userAdmin} component={ListaUsuariosAdmins} />
-          <ProtectedRoute exact path="/dashboard" component={Dashboard} />
-          <ProtectedRoute exact path={paths.userAds} component={UserAds} />
-          <ProtectedRoute exact path="/my-bills" component={MyBills} />
-          <ProtectedRoute path="/my-bills/:id" component={Bill} />
-        </Switch>
-      </Router>
-    </Provider>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/ad/:id" component={Ad} />
+      <Route exact path="/new-ad" component={CreateNewAd} />
+      <Route exact path={paths.login} component={Login} />
+      <Route exact path={paths.register} component={Registration} />
+      <Route exact path={paths.recoverPassword} component={RecoverPassword} />
+      <Route exact path="/students" component={Students} />
+      <Route exact path="/business" component={Business} />
+      <ProtectedRoute exact path={paths.profile} component={Profile} />
+      <ProtectedRoute exact path="/ads" component={AdList} />
+      <ProtectedRoute exact path={paths.userAdmin} component={ListaUsuariosAdmins} />
+      <ProtectedRoute exact path="/dashboard" component={Dashboard} />
+      <ProtectedRoute exact path={paths.userAds} component={UserAds} />
+      <ProtectedRoute exact path="/my-bills" component={MyBills} />
+      <ProtectedRoute path="/my-bills/:id" component={Bill} />
+    </Switch>
   )
 }
 
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import axios from 'axios'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
+import GlobalStyle from './theme/globalStyles'
+
+import store from './store/store'
 import './modern-normalize.css'
 import App from './App'
 
@@ -8,7 +13,12 @@ axios.defaults.baseURL = import.meta.env.VITE_API_URL
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <GlobalStyle />
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
