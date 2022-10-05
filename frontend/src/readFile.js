916aee7e9cc00f6235ad112f25f15c99ba4a4456
JORDI.NODEJS <jordi.nodejs@gmail.com>
2022-10-05 13:58:38 +0200

    comments out

--- frontend/src/readFile.js
+++ frontend/src/readFile.js
@@ -1,29 +1,7 @@
-/* eslint-disable prefer-destructuring */
 /* eslint-disable no-console */
-// Import fsPromises
-
-// Node.js program to demonstrate the
-// fs.open() Method
-
 const fs = require('fs')
 
-// const readIt = fs.readFile
-
 const expression = /import '.\/modern-normalize.css'/gi
-
-// const readData = (err, data) => {
-//   // console.log(data)
-//   try {
-//     if (data === expressionCommented) return false
-//     if (data === expression) return true
-//     return false
-//   } catch (e) {
-//     return err
-//   }
-// }
-
-// readIt('./index.jsx', 'utf8', readData)
-// console.log(readIt('./index.jsx', 'utf8', readData))
 
 let check = false
 fs.readFile('./index.jsx', 'utf-8', (err, data) => {
