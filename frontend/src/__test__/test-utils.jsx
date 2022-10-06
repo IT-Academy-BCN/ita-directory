7de5f2dba2be89832b038daff29e6d0077b9deb9
Hiena <aledaldin@gmail.com>
2022-10-06 11:34:25 +0200

    custonRender applied to testing working

--- frontend/src/__test__/test-utils.jsx
+++ frontend/src/__test__/test-utils.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
 import * as ReactRedux from 'react-redux'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 import store from '../store/store'
 
 const { Provider } = ReactRedux
 
+// eslint-disable-next-line react/prop-types
 function StoreProvider({ children }) {
   return (
     <Provider store={store}>
@@ -16,13 +17,13 @@ function StoreProvider({ children }) {
   )
 }
 
-const customRender = (ui, options) => render(StoreProvider)
+const customRender = (ui, options) => render(ui, { wrapper: StoreProvider })
 
 // eslint-disable-next-line import/no-extraneous-dependencies
 export * from '@testing-library/react'
 
 export { customRender as render }
 
-StoreProvider.prototype = {
-  children: PropTypes.node.isRequired,
-}
+// StoreProvider.prototype = {
+//   children: PropTypes.node.isRequired,
+// }
