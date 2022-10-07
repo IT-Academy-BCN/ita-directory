1bf27958d6609357bebb42a7f49554aee6d35791
adaldin <aledaldin@gmail.com>
2022-10-07 08:50:06 +0200

    733 add custom render provider for testing (#734)
    * Changed ProtectedRoute /recover-password to Route component + fixed styles drom form
    
    * changing import to process in .env .VITE_API_URL
    
    * fixed axios post 2nd argument(object)
    
    * erased comments
    
    * testing recovr Password Component
    
    * test (input email exists, calid email format) passed
    
    * test (axios post arguments)failed
    
    * Deleted unwanted comments
    
    * comments
    
    * comments
    
    * refactoring tests and axios test
    
    * update
    
    * axios test
    
    * mocked up axios
    
    * spyOn fixed
    
    * only from spy
    
    * Fixed redux store without redux-mock-store
    
    * customRender from Provider for testing
    
    * propTypes for children
    
    * custonRender applied to testing working
    
    * changing customRender to different Branch
    
    * CustomRender to import in tests working
    
    * Fixed pr changes + StoreProvider.propTypes
    
    * erased esLint comments from line 10
    
    Co-authored-by: Kevin Mamaqi <kevinmamaqi@gmail.com>

---
+++ frontend/src/__test__/test-utils.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { render } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
+import * as ReactRedux from 'react-redux'
+import PropTypes from 'prop-types'
+import store from '../store/store'
+
+const { Provider } = ReactRedux
+
+function StoreProvider({ children }) {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>{children}</BrowserRouter>
+    </Provider>
+  )
+}
+
+const customRender = (ui, options) => render(ui, { wrapper: StoreProvider })
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+export * from '@testing-library/react'
+
+export { customRender as render }
+
+StoreProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+}
