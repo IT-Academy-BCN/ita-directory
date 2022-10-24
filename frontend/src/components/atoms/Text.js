108af275d5f2a6938cfd16b633227cf2b0882b04
JORDI.NODEJS <jordi.nodejs@gmail.com>
2022-10-24 10:39:46 +0200

    Text component fontSize

--- frontend/src/components/atoms/Text.js
+++ frontend/src/components/atoms/Text.js
@@ -5,7 +5,8 @@ import { colors, font } from '../../theme'
 const Text = styled.p.attrs((props) => ({
   children: props.text,
 }))`
-  font-size: ${(props) => props.fontSize || font.base}px;
+  /* font-size: ${(props) => props.fontSize || font.base}px; */
+  font-size: ${(props) => (props.fontSize ? `${props.fontSize}px` : `${font.base}px`)};
   color: ${(props) => props.color || colors.grey};
   ${space}
   ${typography}
