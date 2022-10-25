1c68a24949d5a1031164f572b7898d89697bb684
JORDI.NODEJS <jordi.nodejs@gmail.com>
2022-10-25 12:58:27 +0200

    requested changes

--- frontend/src/components/atoms/Forms/Label.jsx
+++ frontend/src/components/atoms/Forms/Label.jsx
@@ -2,29 +2,9 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import Text from '../Text'
 
-const LabelStyle = styled.span`
-  .hidden {
-    position: absolute;
-    width: 1px;
-    height: 1px;
-    margin: -1px;
-    padding: 0;
-    border: 0;
-    clip: rect(1px 1px 1px 1px); /* IE6, IE7 */
-    clip: rect(1px, 1px, 1px, 1px);
-    overflow: hidden;
-  }
-`
 function Label({ label, htmlFor, isError = false, hiddenLabel }) {
   return (
-    <LabelStyle>
-      <Text
-        as="label"
-        text={label}
-        htmlFor={htmlFor}
-        className={`${hiddenLabel ? 'hidden' : ''}`}
-      />
-    </LabelStyle>
+    <Text as="label" text={label} htmlFor={htmlFor} className={`${hiddenLabel ? 'hidden' : ''}`} />
   )
 }
 
