e357391adeb2e4db545ae08dd14b0133335358d1
JORDI.NODEJS <jordi.nodejs@gmail.com>
2022-10-26 10:25:54 +0200

    fix changes

--- frontend/src/components/atoms/Forms/Label.jsx
+++ frontend/src/components/atoms/Forms/Label.jsx
@@ -1,16 +1,37 @@
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import { colors } from '../../../theme'
 import Text from '../Text'
 
+const LabelStyled = styled(Text)`
+  &.hidden {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+    padding: 0;
+    border: 0;
+    clip: rect(1px 1px 1px 1px); /* IE6, IE7 */
+    clip: rect(1px, 1px, 1px, 1px);
+    overflow: hidden;
+  }
+  ${(props) => props.isError && `color: ${colors.extraDarkRed} `}
+`
 function Label({ label, htmlFor, isError = false, hiddenLabel }) {
   return (
-    <Text as="label" text={label} htmlFor={htmlFor} className={`${hiddenLabel ? 'hidden' : ''}`} />
+    <LabelStyled
+      as="label"
+      text={label}
+      htmlFor={htmlFor}
+      isError
+      className={`${hiddenLabel ? 'hidden' : ''}`}
+    />
   )
 }
 
 Label.propTypes = {
   label: PropTypes.string.isRequired,
-  htmlFor: PropTypes.string,
+  htmlFor: PropTypes.string.isRequired,
   isError: PropTypes.bool,
   hiddenLabel: PropTypes.bool,
 }
