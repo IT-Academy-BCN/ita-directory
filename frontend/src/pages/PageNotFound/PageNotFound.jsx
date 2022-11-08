fec1311f4847bfb6233295158cd8355dfeb772d3
Laura-Ruiz <laaura.26@gmail.com>
2022-11-08 12:14:36 +0100

    created 404 error page

--- frontend/src/pages/PageNotFound/PageNotFound.jsx
+++ frontend/src/pages/PageNotFound/PageNotFound.jsx
@@ -1,18 +1,23 @@
 import { Link } from 'react-router-dom'
-
-// components
+import styled from 'styled-components'
 import Body from '../../components/layout/Body/Body'
 import Button from '../../components/atoms/Button'
 
+const LinkStyled = styled(Link)`
+  text-decoration: none;
+  display: flex;
+  justify-content: center;
+`
+
 function PageNotFound() {
   return (
-    <Body title="404" justifyTitle="center">
-      <div className="h-full m-auto">
-        <p>Ops, parece que la página que estas buscando no existe.</p>
+    <Body title="404">
+      <div>
+        <p>Ops, parece que la página que estás buscando no existe.</p>
         <p>Vuelve a la página de inicio. Si se trata de un error, ponte en contacto.</p>
-        <Link to="/">
-          <Button text="Visit HomePage" />
-        </Link>
+        <LinkStyled to="/">
+          <Button text="Volver a inicio" />
+        </LinkStyled>
       </div>
     </Body>
   )
