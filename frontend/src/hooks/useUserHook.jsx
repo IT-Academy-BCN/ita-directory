f6c72fb934dc4b7307ea149a9f6a89c1532d6f57
Hiena <aledaldin@gmail.com>
2022-11-21 10:35:30 +0100

    Updating useUserHook

--- frontend/src/hooks/useUserHook.jsx
+++ frontend/src/hooks/useUserHook.jsx
@@ -1,27 +1,33 @@
-import { useEffect, useCallback } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 import { login } from '../store/userSlice'
+import store from '../store/store'
 import axiosInstance from '../utils/axiosInstance'
 import urls from '../utils/urls'
 
-export default function useUser() {
-  const dispatch = useDispatch()
-  const user = useSelector((s) => s.user.value)
-  const isLoggedIn = useSelector((s) => s.user.isLoggedIn)
-
-  const getUser = useCallback(async () => {
+const getMe = async () => {
+  try {
     const userData = await axiosInstance.get(urls.user).then((response) => response.data)
+    if (userData) store.dispatch(login(userData))
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error)
+  }
+}
 
-    if (userData) dispatch(login(userData))
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+export default function useUser() {
+  // @ts-ignore
+  const user = useSelector((s) => s.user.value)
+  // @ts-ignore
+  const isLoggedIn = useSelector((s) => s.user.isLoggedIn)
 
   useEffect(() => {
     const token = localStorage.getItem('token')
     if (!isLoggedIn && token) {
-      getUser()
+      getMe()
     }
-  }, [getUser, isLoggedIn])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [getMe, isLoggedIn])
 
   if (user) {
     return user
