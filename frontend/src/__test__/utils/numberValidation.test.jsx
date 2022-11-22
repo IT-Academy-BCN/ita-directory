28a633df64656892322b75c1e20042d6ebd0ec5a
RogerRochoRibas <rogerrochoribas@gmail.com>
2022-11-22 13:48:18 +0100

    fix .only

--- frontend/src/__test__/utils/numberValidation.test.jsx
+++ frontend/src/__test__/utils/numberValidation.test.jsx
@@ -5,7 +5,7 @@ import newAdSchema from '../../validation/createNewAdSchema'
 // Can't test the numberValidation function on its own so we test it through price validation
 const testSchema = newAdSchema.pick({ price: true })
 
-describe.only('numberValidation', () => {
+describe('numberValidation', () => {
   it('Should be successful if positive number without decimals', () => {
     const r = testSchema.safeParse({ price: '5' })
     expect(r.success).toBe(true)
@@ -17,7 +17,7 @@ describe.only('numberValidation', () => {
     expect(r.error.issues[0].message).toBe('Must be a positive number')
   })
 
-  it.only('Should demand a number if given letters', () => {
+  it('Should demand a number if given letters', () => {
     const result = testSchema.safeParse({ price: 'Five' })
     expect(result.success).toBe(false)
     expect(result.error.issues[0].message).toBe('Must insert a number')
