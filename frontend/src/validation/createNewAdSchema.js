a92ef4fe8c50c4a0b16cab82a334dfb6c6154743
RogerRochoRibas <rogerrochoribas@gmail.com>
2022-11-22 13:26:50 +0100

    fixed bug, added numberValidation and tests

--- frontend/src/validation/createNewAdSchema.js
+++ frontend/src/validation/createNewAdSchema.js
@@ -1,4 +1,5 @@
 import { z } from 'zod'
+import numberValidation from '../utils/numberValidation'
 
 const newAdSchema = z.object({
   title: z
@@ -11,46 +12,37 @@ const newAdSchema = z.object({
       invalid_type_error: 'Can only contain letters',
     })
     .min(1, { message: 'This field is required.' }),
-   address: z
+  address: z
     .string({ required_error: 'This field is required' })
     .min(1, { message: 'This field is required.' }),
-  nRooms: z.nullable(
-    z
-      .number({
-        required_error: 'This field is required',
-        invalid_type_error: 'Must insert a number',
-      })
-      .min(1, { message: 'This field is required.' })
-      .int({ message: 'No decimals allowed' })
-      .positive({ message: 'Must be a positive number' })
-      .transform((val) => (isNaN(val) ? undefined : val))
-  ),
+  nRooms: z
+    .string({
+      required_error: 'This field is required',
+    })
+    .transform((val, ctx) => {
+      numberValidation(val, ctx)
+    }),
   price: z
-    .number({
+    .string({
       required_error: 'This field is required',
-      invalid_type_error: 'Must insert a number',
     })
-    .min(1, { message: 'This field is required.' })
-    .positive({ message: 'Must be a positive number' }),
+    .transform((val, ctx) => {
+      numberValidation(val, ctx)
+    }),
   squareMeters: z
-    .number({
+    .string({
       required_error: 'This field is required',
-      invalid_type_error: 'Must insert a number',
     })
-    .min(1, { message: 'This field is required.' })
-    .int({ message: 'No decimals allowed' })
-    .positive({ message: 'Must be a positive number' }),
-  nBathrooms: z.nullable(
-    z
-      .number({
-        required_error: 'This field is required',
-        invalid_type_error: 'Must insert a number',
-      })
-      .min(1, { message: 'This field is required.' })
-      .int({ message: 'No decimals allowed' })
-      .positive({ message: 'Must be a positive number' })
-      .transform((val) => (isNaN(val) ? undefined : val))
-  ),
+    .transform((val, ctx) => {
+      numberValidation(val, ctx)
+    }),
+  nBathrooms: z
+    .string({
+      required_error: 'This field is required',
+    })
+    .transform((val, ctx) => {
+      numberValidation(val, ctx)
+    }),
 })
 
 export default newAdSchema
