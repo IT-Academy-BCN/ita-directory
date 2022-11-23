1731f65fac1139a4a0617ab3d8d4e1dedce6c945
Alex Luque <alexluque29@gmail.com>
2022-11-23 11:54:24 +0100

    Label position fixed

--- frontend/src/pages/CreateNewAd/CreateNewAd.styles.js
+++ frontend/src/pages/CreateNewAd/CreateNewAd.styles.js
@@ -35,7 +35,7 @@ export const Wrapper = styled.div`
     flex-direction: column;
     text-align: start;
     justify-content: start;
-    align-items: start;
+    align-items: baseline;
     margin-bottom: 1rem;
 
     @media ${device.Tablet} {
@@ -86,6 +86,7 @@ export const Wrapper = styled.div`
       display: flex;
       flex-direction: column;
       align-items: start;
+      /* margin: 5px; */
     }
     .style-input-create-new-ad {
       width: 20rem;
