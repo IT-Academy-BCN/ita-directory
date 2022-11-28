9389effbdf3c2bb9634b2188d01f55aaacbdca8d
Kevin Mamaqi <kevinmamaqi@gmail.com>
2022-11-28 11:19:42 +0100

    0-add-deploy-fr-t1

--- scripts/dev.deploy.sh
+++ scripts/dev.deploy.sh
@@ -3,12 +3,15 @@ source utils.sh
 ENV_FILE=/env/.env.dev.frontend
 loadEnv "$ENV_FILE"
 
+echo "Current directory"
+echo "$PWD"
+
 # Copy the .env file to the frontend
 cp ENV_FILE /frontend/.env
 
 # Build the frontend
 cd frontend
-pwd
+echo "$PWD"
 npm install
 npm run build
-pwd
+echo "$PWD"
