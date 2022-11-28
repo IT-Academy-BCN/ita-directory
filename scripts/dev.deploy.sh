ec157a6a40febe53b818a209fa349b6983b22b2d
Kevin Mamaqi <kevinmamaqi@gmail.com>
2022-11-28 11:47:40 +0100

    update

--- scripts/dev.deploy.sh
+++ scripts/dev.deploy.sh
@@ -4,15 +4,14 @@ source utils.sh
 ENV_FILE=/env/.env.dev.frontend
 loadEnv "$ENV_FILE"
 
-echo "Current directory"
-echo "$PWD"
-
 # Copy the .env file to the frontend
 cp $ENV_FILE $(git_root)/frontend/.env
 
 # Build the frontend
 cd $(git_root)/frontend
-echo "$PWD"
 npm install
-npm run build
-echo "$PWD"
+npm run buildc
+
+# Deploy
+rm -rf $PATH_FRONTEND_DEV
+cp $(git_root)/frontend/dist $PATH_FRONTEND_DEV -r
