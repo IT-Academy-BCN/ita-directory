19ba2b782e26e79c5677a5d3a71e83f18b446fd0
Hiena <aledaldin@gmail.com>
2022-11-29 12:25:11 +0100

    erased comments from handler and server mock up

--- frontend/src/mocks/handlers.jsx
+++ frontend/src/mocks/handlers.jsx
@@ -3,16 +3,13 @@ import { urls } from '../utils'
 
 const baseURL = `${import.meta.env.VITE_API_URL}${urls.register}`
 
-// response from Register (preview)
 const mockRegisterRes = [
   {
     message: 'User registered correctly',
   },
 ]
 
-// requested handlers
 const handlers = [
-  // post request     request, response, context
   rest.post(baseURL, (req, res, ctx) => res(ctx.status(200), ctx.json(mockRegisterRes))),
 ]
 
--- frontend/src/mocks/server.jsx
+++ frontend/src/mocks/server.jsx
@@ -1,7 +1,6 @@
 import { setupServer } from 'msw/node'
 import handlers from './handlers'
 
-// This configures a request mocking server with the given request handlers.
 const server = setupServer(...handlers)
 
 export default server
