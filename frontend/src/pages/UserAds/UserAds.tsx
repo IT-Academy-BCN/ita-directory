34d88a8d6238e91942c3707ae2d29d864528adfa
BoccoSantiago <italobocco@gmail.com>
2022-11-29 12:32:47 +0100

    update

--- frontend/src/pages/UserAds/UserAds.tsx
+++ frontend/src/pages/UserAds/UserAds.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import { useHistory } from 'react-router-dom'
 import UserAdsCard from './UserAdsCard/UserAdsCard'
@@ -28,54 +28,27 @@ const AdsStyled = styled(Container)`
   }
 `
 
-type TAdProps = {
-  price: number
-  squareMeters: number
-}
-type TFunctionFilter = (min: number, max: number) => (ad: TAdProps) => boolean
-
-type TFilterParms = {
-  minPrice?: number
-  maxPrice?: number
-  minSize?: number
-  maxSize?: number
-}
-
 function UserAds() {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [filterParams, setFilterParams] = useState<TFilterParms | undefined>()
   const [adList, setAdList] = useState([])
   const [loading, setLoading] = useState(true)
 
   const history = useHistory()
 
-  // Added filters by Kevin
-  const byPrice: TFunctionFilter = (min, max) => (ad) => {
-    if (min == null && max == null) return true
-    return min <= ad.price && ad.price <= max
-  }
-
-  const bySize: TFunctionFilter = (min, max) => (ad) => {
-    if (min == null && max == null) return true
-    return min <= ad.squareMeters && ad.squareMeters <= max
-  }
   const user = useUser()
+  let userId: number = 0
+
+  if (user) {
+    userId = user.id
+  }
 
   useEffect(() => {
     const fetchAds = async () => {
-      const result = await axiosInstance.get(`/ads/user/${user.id}`)
+      const result = await axiosInstance.get(`/ads/user/${userId}`)
       setAdList(result.data.data)
       setLoading(false)
     }
     fetchAds()
-  }, [user])
-
-  const filteredAdList = useMemo(() => {
-    const filteredAds = adList
-      .filter(byPrice(filterParams?.minPrice || 0, filterParams?.maxPrice || Infinity))
-      .filter(bySize(filterParams?.minSize || 0, filterParams?.maxSize || Infinity))
-    return filteredAds
-  }, [filterParams, adList])
+  }, [user, userId])
 
   type TAdCardProps = {
     id: number
@@ -91,7 +64,7 @@ function UserAds() {
     mapLon: string
     key: string
   }
-  const renderList = filteredAdList.map((e: TAdCardProps) => (
+  const renderList = adList.map((e: TAdCardProps) => (
     <UserAdsCard
       id={e.id}
       title={e.title}
