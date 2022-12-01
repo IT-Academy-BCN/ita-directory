e546c8898d535f724da3388382dc8d596014cf7f
Hiena <aledaldin@gmail.com>
2022-12-01 08:29:00 +0100

    fixed msw warning

--- frontend/setupTests.js
+++ frontend/setupTests.js
@@ -2,6 +2,6 @@
 import '@testing-library/jest-dom'
 import server from './src/mocks/server'
 
-beforeAll(() => server.listen())
+beforeAll(() => server.listen({ onUnhandledRequest: 'bypass' }))
 afterEach(() => server.resetHandlers)
 afterAll(() => server.close)
