03cdaa43b4b27f7c0a524ff78a6f801d66bebe20
Sergio Paniagua Sánchez <97700906+Spanioulis@users.noreply.github.com>
2023-02-28 15:51:28 +0100

    feat: migrate Notification component from jsx to tsx

--- frontend/src/components/atoms/Notifications/Notification.tsx
+++ frontend/src/components/atoms/Notifications/Notification.tsx
@@ -1,10 +1,16 @@
-import PropType from 'prop-types'
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import styled, { keyframes } from 'styled-components'
 import { deleteNotification } from '../../../store/notificationSlice'
 import Icon from '../Icon'
 import Text from '../Text'
+
+interface NotificationProps {
+  message: string
+  id: number
+  icon: string
+  colorIcon: string
+}
 
 const InRight = keyframes`
     from {
@@ -26,13 +32,17 @@ const NotificationStyled = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
+
+  ${Text} {
+    margin-left: -15px;
+  }
 `
 const NotificationIconStyled = styled.div`
   display: flex;
   align-items: center;
 `
 
-function Notification({ message, id, icon, colorIcon }) {
+function Notification({ message, id, icon, colorIcon }: NotificationProps) {
   const dispatch = useDispatch()
 
   const closeNotification = () => {
@@ -59,22 +69,15 @@ function Notification({ message, id, icon, colorIcon }) {
         style={{
           position: 'relative',
           background: 'none',
-          bottom: '22px',
-          left: '290px',
+          left: '295px',
           alignSelf: 'flex-start',
+          cursor: 'pointer',
         }}
       />
 
-      <Text text={message} style={{ marginLeft: '-45px' }} />
+      <Text text={message} />
     </NotificationStyled>
   )
-}
-
-Notification.propTypes = {
-  message: PropType.string.isRequired,
-  id: PropType.number.isRequired,
-  icon: PropType.string.isRequired,
-  colorIcon: PropType.string.isRequired,
 }
 
 export default React.memo(Notification)
