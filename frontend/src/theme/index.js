b9f7c76428d50759035c11ef0f0780486ca1a8a0
Sergio Paniagua Sánchez <97700906+Spanioulis@users.noreply.github.com>
2023-03-14 16:44:07 +0100

    [FE] migrate Card component from jsx to ts (#902)

--- frontend/src/theme/index.js
+++ frontend/src/theme/index.js
@@ -3,5 +3,6 @@ import font from './font'
 import colors from './colors'
 import { responsiveSizes, device } from './mediaQueries'
 import { Container, Grid } from './wrappers'
+import boxShadow from './boxShadow'
 
-export { dimensions, imageSize, font, colors, device, responsiveSizes, Container, Grid }
+export { dimensions, imageSize, font, colors, device, responsiveSizes, Container, Grid, boxShadow }
