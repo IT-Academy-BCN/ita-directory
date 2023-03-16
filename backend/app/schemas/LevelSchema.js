d7d6c4a52e656217b3f12c4e522e9968f172852d
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-03-16 12:37:26 +0100

    fixing LevelSchema

--- backend/app/schemas/LevelSchema.js
+++ backend/app/schemas/LevelSchema.js
@@ -1,9 +1,13 @@
 const { z } = require('zod')
+const LevelTypeSchema = require('./LevelTypeSchema')
 
 const LevelSchema = z.object({
   id: z.number().int(),
   name: z.string(),
+  parent: z.lazy(() => LevelSchema).nullish(),
   parentId: z.number().int().nullish(),
+  child: z.array(z.lazy(() => LevelSchema)),
+  levelType: LevelTypeSchema,
   levelTypeId: z.number().int(),
 })
 
