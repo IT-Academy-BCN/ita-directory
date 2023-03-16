d8fcf6b03d96b35a34c5ffd6d5d062b4cfe727bb
Kevin Mamaqi <kevinmamaqi@gmail.com>
2023-03-16 09:57:04 +0100

    update

--- frontend/src/__test__/molecules/Menu.test.tsx
+++ frontend/src/__test__/molecules/Menu.test.tsx
@@ -1,33 +1,34 @@
 import React from 'react'
+import { MemoryRouter } from 'react-router-dom'
 import { render, screen } from '@testing-library/react'
-import { MemoryRouter, useLocation } from 'react-router-dom'
-import userEvent from '@testing-library/user-event'
 import Menu from '../../components/molecules/Menu'
 
-describe('Menu', () => {
-  it('renders useLocation', () => {
-    const route = '/'
-    function LocationDisplay() {
-      const location = useLocation()
-      return <div data-testid="location-display">{location.pathname}</div>
-    }
+// vi.mock('react-router-dom', () => ({
+//   useLocation: vi.fn(() => ({ pathname: '/' })),
+//   Link: ({ children }: { children: React.ReactNode }) => <a>{children}</a>,
+// }))
 
-    render(
-      <MemoryRouter initialEntries={[route]}>
-        <LocationDisplay />
+describe('Menu', () => {
+  //   it('renders correctly', () => {
+  //     render(<Menu data-testid="menu" />)
+
+  //     expect(screen.getByText(/El directorio/)).toBeInTheDocument()
+  //     expect(screen.getByText(/Para alumnos/)).toBeInTheDocument()
+  //     expect(screen.getByText(/Para empresas/)).toBeInTheDocument()
+  //     expect(screen.getByText(/Anuncios/)).toBeInTheDocument()
+  //   })
+
+  it('renders correctly', () => {
+    const { container } = render(
+      <MemoryRouter>
+        <Menu data-testid="menu" />
       </MemoryRouter>
     )
-    expect(screen.getByTestId('location-display')).toHaveTextContent(route)
-  })
 
-  it('renders correctly', async () => {
-    render(<Menu data-testid="menu" />)
-
-    const user = userEvent.setup()
-
-    expect(screen.getByText('/home/i')).toBeInTheDocument()
-
-    await user.click(screen.getByText('/about/i'))
-    expect(screen.getByText('/you are on the about page/i')).toBeInTheDocument()
+    expect(screen.getByText(/El directorio/)).toBeInTheDocument()
+    expect(screen.getByText(/Para alumnos/)).toBeInTheDocument()
+    expect(screen.getByText(/Para empresas/)).toBeInTheDocument()
+    expect(screen.getByText(/Anuncios/)).toBeInTheDocument()
+    console.log('container', container.innerHTML)
   })
 })
